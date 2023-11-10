f7452e21f4f3cc987b9ed9d0a427d06d41d07531
rheyhannh <rheyhannh@student.ub.ac.id>
2023-11-10 13:01:12 +0700

    feat: hide header when scroll mobile

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,17 +1,42 @@
 'use client'
 
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { ContentContext } from './provider/Content';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { LiaTimesSolid } from 'react-icons/lia';
 import styles from './style/header.module.css'
 
 export default function Header() {
-    const { isNavbarActive, setNavbarActive } = useContext(ContentContext);
+    const { isNavbarActive, setNavbarActive, isPhoneContent } = useContext(ContentContext);
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [showHeader, setShowHeader] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.scrollY || window.pageYOffset;
+            const isScrollingDown = currentScrollPos > prevScrollPos;
+            const isScrollingUp = currentScrollPos < prevScrollPos;
+            setPrevScrollPos(currentScrollPos);
+            
+            if (isScrollingDown && window.scrollY >= 360 && isPhoneContent && !isNavbarActive) {
+                setShowHeader(false);
+            }
+
+            if (isScrollingUp) {
+                setShowHeader(true);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [prevScrollPos])
 
     return (
         <>
-            <div className={styles.dashboard}>
+            <div className={`${styles.dashboard} ${!showHeader ? styles.hide : ''}`}>
                 <div className={styles.corner}>
                     <div className={styles.hamburger}>
                         {isNavbarActive ?
