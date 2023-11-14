b88115771ec04feba59a9bdb4eb1b8c950542d0b
rheyhannh <rheyhannh@student.ub.ac.id>
2023-11-15 06:58:50 +0700

    chore: Update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" },
+                    { key: 'Cache-Control', value: 'no-store, must-revalidate' },
+                    { key: 'X-Powered-By', value: 'Next.js' }
+                ]
+            }
+        ]
+    },
+    experimental: {
+        serverActions: true,
+    }
+}
 
 module.exports = nextConfig
