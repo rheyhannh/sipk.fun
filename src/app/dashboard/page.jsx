78a46873b9708b4b951002b399ec7319e0ac2dcc
rheyhannh <rheyhannh@student.ub.ac.id>
2023-11-15 07:09:26 +0700

    fix: Dashboard card

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,81 +1,76 @@
+'use client'
+
 import { Summary } from '@/component/Card'
+import { useMatkul, useUser } from '@/data/core';
+import * as x from '@/data/summary';
 import styles from './home.module.css'
+
+function AcademicCard({ count }) {
+    const { data: user, error: userError, isLoading: userLoading, isValidating: userValidating } = useUser();
+    const { data: matkul, error: matkulError, isLoading: matkulLoading, isValidating: matkulValidating } = useMatkul();
+
+    if (userError || matkulError) {
+        const errorCount = Array.from({ length: count }, (_, index) => (
+            <Summary state='error' key={crypto.randomUUID()} error={matkul} />
+        ));
+        return <>{errorCount}</>;
+    }
+
+    if (userLoading || matkulLoading) {
+        const loadingCount = Array.from({ length: count }, (_, index) => (
+            <Summary state='loading' key={crypto.randomUUID()} />
+        ));
+        return <>{loadingCount}</>;
+    }
+
+    if (userValidating || matkulValidating) {
+        const loadingCount = Array.from({ length: count }, (_, index) => (
+            <Summary state='validating' key={crypto.randomUUID()} />
+        ));
+        return <>{loadingCount}</>;
+    }
+
+    return (
+        <>
+            <Summary
+                state='loaded'
+                color='var(--primary-color)'
+                icon={{ name: 'TbSum', lib: 'tb' }}
+                data={{ value: x.getUserSks(matkul), percentage: x.getUserSksPercentage(user, matkul), lastUpdated: "Terakhir Diupdate" }}
+                title={'SKS'}
+            >
+            </Summary>
+
+            <Summary
+                state='loaded'
+                color='var(--first-color)'
+                icon={{ name: 'TbSum', lib: 'tb' }}
+                data={{ value: x.getUserMatkul(matkul), percentage: x.getUserMatkulPercentage(), lastUpdated: 'Terakhir Diupdate' }}
+                title={'Matkul'}
+            >
+            </Summary>
+
+            <Summary
+                state='loaded'
+                color='var(--second-color-lighter)'
+                icon={{ name: 'TbSum', lib: 'tb' }}
+                data={{ value: x.getUserIpk(matkul), percentage: x.getUserIpkPercentage(user, matkul), lastUpdated: 'Terakhir Diupdate' }}
+                title={'Ipk'}
+            >
+            </Summary>
+        </>
+    )
+}
 
 export default function DashboardPage() {
     return (
-        <>
-            <div className={styles.wrapper}>
-                <div className={styles.card}>
-                    <Summary
-                        state='loaded'
-                        color='var(--primary-color)'
-                        icon={{name: 'TbSum', lib: 'tb'}}
-                        data={{value: 144, percentage: 85, lastUpdated: 'Terakhir Diupdate'}}
-                        title={'SKS'}
-                    >
-                    </Summary>
-                    <Summary
-                        state='loaded'
-                        color='var(--danger-color)'
-                        icon={{name: 'FiBookOpen', lib: 'fi'}}
-                        data={{value: 42, percentage: 32, lastUpdated: 'Terakhir Diupdate'}}
-                        title={'Matakuliah'}
-                    >
-                    </Summary>
-                    <Summary
-                        state='loaded'
-                        color='var(--success-color)'
-                        icon={{name: 'FiBookOpen', lib: 'fi'}}
-                        data={{value: 3.75, percentage: 90, lastUpdated: 'Terakhir Diupdate'}}
-                        title={'IPK'}
-                    >
-                    </Summary>
-                    <Summary
-                        state='loaded'
-                        color='aqua'
-                        icon={{name: 'LiaWeightSolid', lib: 'lia'}}
-                        data={{value: 65, percentage: 55, lastUpdated: 'Terakhir Diupdate'}}
-                        title={'Berat Badan'}
-                    >
-                    </Summary>
-                    <Summary
-                        state='loaded'
-                        color='violet'
-                        icon={{name: 'LiaPeopleCarrySolid', lib: 'lia'}}
-                        data={{value: 175, percentage: 69, lastUpdated: 'Terakhir Diupdate'}}
-                        title={'Tinggi Badan'}
-                    >
-                    </Summary>
-                    <Summary
-                        state='loaded'
-                        color='var(--first-color)'
-                        icon={{name: 'MdPeopleOutline', lib: 'md'}}
-                        data={{value: 24, percentage: 32, lastUpdated: 'Terakhir Diupdate'}}
-                        title={'Umur'}
-                    >
-                    </Summary>
-                    <Summary
-                        state='loaded'
-                        color='gold'
-                        icon={{name: 'GiRank3', lib: 'gi'}}
-                        data={{value: 24, percentage: 83, lastUpdated: 'Terakhir Diupdate'}}
-                        title={'Umur'}
-                    >
-                    </Summary>
-                    <Summary
-                        state='loading'
-                    >
-                    </Summary>
-                    <Summary
-                        state='error'
-                    >
-                    </Summary>
-
-                </div>
-                <div>
-                    <h1>Dashboard Content 2</h1>
-                </div>
+        <div className={styles.wrapper}>
+            <div className={styles.card}>
+                <AcademicCard count={3} />
             </div>
-        </>
+            <div>
+                <h1>Dashboard Content 2</h1>
+            </div>
+        </div>
     )
 }
