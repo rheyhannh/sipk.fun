e92880d5eee200ad916cf17943c015dda1ba520f
rheyhannh <rheyhannh@student.ub.ac.id>
2023-11-15 07:08:21 +0700

    feat: Add ratelimit for api route

---
+++ src/utils/rateLimit.js
@@ -0,0 +1,26 @@
+import { LRUCache } from 'lru-cache';
+
+export default function rateLimit(options) {
+    const tokenCache = new LRUCache({
+        max: options?.uniqueTokenPerInterval || 500,
+        ttl: options?.interval || 60000,
+    });
+
+    return {
+        check: (limit, token) =>
+            new Promise((resolve, reject) => {
+                const tokenCount = tokenCache.get(token) || [0];
+                if (tokenCount[0] === 0) {
+                    tokenCache.set(token, tokenCount);
+                }
+                tokenCount[0] += 1;
+
+                const currentUsage = tokenCount[0];
+                const isRateLimited = currentUsage >= limit;
+
+                return isRateLimited ?
+                    reject() :
+                    resolve(currentUsage);
+            }),
+    };
+}
