e06d2dc58de4de3527ae65f88cc8ce951c6be9ec
rheyhannh <rheyhannh@student.ub.ac.id>
2023-11-15 07:07:16 +0700

    feat: Add and use toaster

---
+++ src/component/loader/Toaster.jsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { toast, Toaster, ToastBar } from 'react-hot-toast';
+import { FaTimes } from 'react-icons/fa';
+import Spinner from './Spinner';
+
+export default function Notification() {
+    return (
+        <Toaster
+            toastOptions={{
+                style: {
+                    background: 'var(--white-color)',
+                    color: 'var(--logo-second-color)'
+                }
+            }}
+            position={'bottom-center'}
+        >
+            {(t) => (
+                <ToastBar toast={t}>
+                    {({ icon, message }) => {
+                        const customColor = { error: '#FF4B4B', success: '#61D345' }
+                        const isLoading = t.type === 'loading' ? true : false;
+                        const isImportant = ['loading', 'error', 'success'].includes(t.type);
+                        return (
+                            <>
+                                {!isLoading && icon}
+                                <span style={
+                                    {
+                                        fontWeight: isImportant ? 700 : 600,
+                                        color: customColor[t.type] ? customColor[t.type] : ''
+                                    }}
+                                >
+                                    {message}
+                                </span>
+                                {!isLoading && (
+                                    <FaTimes
+                                        onClick={() => toast.dismiss(t.id)}
+                                        style={{ cursor: 'pointer', color: 'crimson' }} size={'13px'}
+                                    />
+                                )}
+                                {isLoading && (
+                                    <Spinner
+                                        size={'12px'}
+                                        color={'var(--logo-second-color)'}
+                                    />
+                                )}
+                            </>
+                        )
+                    }}
+                </ToastBar>
+            )}
+        </Toaster>
+    )
+}
