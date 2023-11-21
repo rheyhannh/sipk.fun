f7c66c3587f7e5a3a6f6af74c25913dae517a2f3
rheyhannh <rheyhannh@student.ub.ac.id>
2023-11-21 07:58:58 +0700

    Using league spartan font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,22 +1,22 @@
 import Notification from '@/component/loader/Toaster';
-import { Poppins } from 'next/font/google';
+import { Poppins, League_Spartan } from 'next/font/google';
 import './globals.css'
 export const metadata = {
   title: 'SIPK Application',
   description: 'Elevate your academic journey',
 }
 
-const poppins = Poppins({
+const league_spartan = League_Spartan({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--poppins-font',
+  variable: '--leaguespartan-font',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
-});
+})
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.variable}>
+      <body className={league_spartan.variable}>
         <Notification/>
         {children}
       </body>
