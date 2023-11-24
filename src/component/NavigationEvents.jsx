a16adefb8f8fb92bb8c48de18e2b46580688c489
rheyhannh <rheyhannh@student.ub.ac.id>
2023-11-25 04:09:41 +0700

    fix: Mobile nav event hide navbar

--- src/component/NavigationEvents.jsx
+++ src/component/NavigationEvents.jsx
@@ -5,12 +5,16 @@ import { usePathname, useSearchParams } from 'next/navigation'
 import { DashboardContext } from './provider/Dashboard';
 
 export function NavigationEvents() {
-    const { setActiveLink } = useContext(DashboardContext);
+    const { setActiveLink, setNavbarActive, isRichContent } = useContext(DashboardContext);
     const pathname = usePathname();
     const searchParams = useSearchParams();
 
     useEffect(() => {
         setActiveLink(pathname);
+        if (!isRichContent) {
+            setNavbarActive(false);
+            document.body.classList.remove('disable_scroll');
+        }
     }, [pathname, searchParams])
 
     return null
