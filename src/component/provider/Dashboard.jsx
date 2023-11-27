fd4ac4505c9dbb2e268344458766991d4e2d4180
rheyhannh <rheyhannh@student.ub.ac.id>
2023-11-27 18:29:57 +0700

    feat: Add touch device hook

--- src/component/provider/Dashboard.jsx
+++ src/component/provider/Dashboard.jsx
@@ -19,6 +19,9 @@ export const DashboardProvider = ({ children }) => {
     const [isRichContent, setRichContent] = useState(0);
     const [isPhoneContent, setPhoneContent] = useState(false);
 
+    // Touch Devices
+    const [isTouchDevice, setTouchDevice] = useState(false);
+
     /*
     ========== Use Effect Hook ==========
     */
@@ -26,6 +29,7 @@ export const DashboardProvider = ({ children }) => {
         // Content Init
         const richMediaQuery = window.matchMedia('(min-width: 1280px)');
         const phoneMediaQuery = window.matchMedia('(max-width: 767px)');
+        const touchDevice = 'ontouchstart' in window || navigator.msMaxTouchPoints;
 
         const handleRichMediaQueryChange = (e) => {
             setNavbarActive(e.matches);
@@ -38,13 +42,26 @@ export const DashboardProvider = ({ children }) => {
 
         handleRichMediaQueryChange(richMediaQuery);
         handlePhoneMediaQueryChange(phoneMediaQuery);
+        if (touchDevice) { setTouchDevice(true); }
 
         richMediaQuery.addEventListener('change', handleRichMediaQueryChange);
         phoneMediaQuery.addEventListener('change', handlePhoneMediaQueryChange);
+        window.addEventListener('touchstart', () => {
+            if (!isTouchDevice) {
+                setTouchDevice(true);
+            }
+        })
+        window.addEventListener('mouseover', () => {
+            if (isTouchDevice) {
+                setTouchDevice(false);
+            }
+        })
 
         return () => {
             richMediaQuery.removeEventListener('change', handleRichMediaQueryChange);
             phoneMediaQuery.removeEventListener('change', handlePhoneMediaQueryChange);
+            window.removeEventListener('touchstart', () => { });
+            window.removeEventListener('mouseover', () => { });
         }
     }, [])
 
@@ -55,6 +72,7 @@ export const DashboardProvider = ({ children }) => {
                 activeLink, setActiveLink,
                 isRichContent, setRichContent,
                 isPhoneContent, setPhoneContent,
+                isTouchDevice, setTouchDevice,
             }}
         >
             {children}
