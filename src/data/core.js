a528c31396111051647243edf47565cd50349558
rheyhannh <rheyhannh@student.ub.ac.id>
2023-12-01 21:53:55 +0700

    feat: Update data hooks

--- src/data/core.js
+++ src/data/core.js
@@ -1,32 +1,38 @@
 // ========== COMPONENT DEPEDENCY ========== //
 import useSWR from 'swr'
+import { useCookies } from 'next-client-cookies';
 
 /*
 ============================== CODE START HERE ==============================
 */
-const fetcher = (url) => fetch(url).then((res) => res.json().then((data) => data))
+const fetchDefault = (url) => fetch(url).then((res) => res.json().then((data) => data))
+const fetchWithUserId = (url, id) => fetch(url).then((res) => res.json().then((data) => data))
+
 const swrOptions = {
     refreshInterval: 0,
     revalidateIfStale: true,
     revalidateOnMount: true,
-    revalidateOnFocus: false,
+    revalidateOnFocus: true,
     revalidateOnReconnect: true,
     dedupingInterval: 5000,
     shouldRetryOnError: false,
 }
 
-export function useMatkul() {
-    return useSWR('/api/matkulku', fetcher, swrOptions)
-}
-
-export function useUniversitas() {
-    return useSWR('/api/universitasku', fetcher, swrOptions)
-}
-
 export function useUser() {
-    return useSWR('/api/me', fetcher, swrOptions)
+    const userIdCookie = useCookies().get('s_user_id');
+    return useSWR(['/api/me', , userIdCookie], ([url, id]) => fetchWithUserId(url, id), swrOptions)
+}
+
+export function useMatkul() {
+    const userIdCookie = useCookies().get('s_user_id');
+    return useSWR(['/api/matkulku', userIdCookie], ([url, id]) => fetchWithUserId(url, id), swrOptions)
+}
+
+export function useMatkulHistory() {
+    const userIdCookie = useCookies().get('s_user_id');
+    return useSWR(['/api/matkul-history', userIdCookie], ([url, id]) => fetchWithUserId(url, id), swrOptions)
 }
 
 export function useNotifikasi() {
-    return useSWR('/api/notifikasi', fetcher, swrOptions)
+    return useSWR('/api/notifikasi', fetchDefault, swrOptions)
 }
