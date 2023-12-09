baeb7c67566f0e623169ee8d1113dd5d47e49081
rheyhannh <rheyhannh@student.ub.ac.id>
2023-12-09 22:31:39 +0700

    feat: Add client side utils

--- src/utils/client_side.js
+++ src/utils/client_side.js
@@ -16,3 +16,29 @@ export const unixToDate = (unix, intlLocales = null, intlFormatOptions = null) =
     const date = new Date(unix * 1000);
     return date.toLocaleString(intlLocales || 'id-ID', intlFormatOptions || { dateStyle: 'full' });
 }
+
+// Message or Response Utility
+/**
+ * Method untuk mendapatkan pesan loading lucu/tidak dengan indeks tertentu atau random
+ * @param {boolean} [funWord=false] Boolean untuk menggunakan kata lucu atau tidak. Default value `false`
+ * @param {number} [index=true] Number untuk memilih index kata. Default value `true` yaitu menggunakan index random. Gunakan `number` untuk pilih indeks tertentu (tidak random).
+ * @return {string} Pesan atau teks loading yang tersedia. Jika tidak tersedia default `funWord=true` 'Memproses Keajaiban', default `funWord=false` 'Memproses permintaanmu'
+ */
+export const getLoadingMessage = (funWord = false, index = true) => {
+    const message = {
+        fun: [
+            'Memproses keajaiban',
+            'Kunci adalah sabar',
+            'Membaca pikiranmu'
+        ],
+        default: [
+            'Memproses',
+            'Memproses permintaanmu',
+            'Tunggu sebentar',
+        ]
+    }
+
+    const maximumIndex = funWord ? message.fun.length : message.default.length;
+    const usedIndex = index === true ? Math.floor(Math.random() * maximumIndex) : index === 0 ? 0 : index;
+    return funWord ? message.fun[usedIndex] || 'Memproses Keajaiban' : message.default[usedIndex] || 'Memproses permintaanmu';
+}
