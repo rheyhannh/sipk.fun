ca16b7b41575cd2ae593cfc593487529d222ab7f
rheyhannh <rheyhannh@student.ub.ac.id>
2023-12-15 08:07:53 +0700

    feat: Add api handler for update profil

--- src/app/api/me/route.js
+++ src/app/api/me/route.js
@@ -116,3 +116,111 @@ export async function GET(request) {
         }
     });
 }
+
+export async function POST(request) {
+    const newHeaders = {};
+    const userAccessToken = request.cookies.get(`${process.env.USER_SESSION_COOKIES_NAME}`)?.value;
+    const authorizationHeader = headers().get('Authorization');
+    const authorizationToken = authorizationHeader ? authorizationHeader.split(' ')[1] : null;
+    const cookieStore = cookies();
+
+    if (!userAccessToken || !authorizationHeader || !authorizationToken) {
+        return NextResponse.json({ message: 'Unauthorized - Missing access token' }, {
+            status: 401,
+        })
+    }
+
+    const decryptedSession = await decryptAES(userAccessToken, true);
+    const userId = decryptedSession?.user?.id;
+
+    if (!decryptedSession || !userId) {
+        cookieStore.set({ name: process.env.USER_SESSION_COOKIES_NAME, value: '', ...cookieAuthDeleteOptions })
+        cookieStore.set({ name: 's_user_id', value: '', ...cookieAuthDeleteOptions })
+        cookieStore.set({ name: 's_access_token', value: '', ...cookieAuthDeleteOptions })
+        return NextResponse.json({ message: 'Unauthorized - Invalid access token' }, {
+            status: 401
+        })
+    }
+
+    try {
+        var decoded = validateJWT(authorizationToken, userId);
+        // Log Here, ex: '{TIMESTAMP} decoded.id {METHOD} {ROUTE} {BODY} {PARAMS}'
+    } catch (error) {
+        return NextResponse.json({ message: error.message || 'Unauthorized - Invalid access token' }, {
+            status: 401
+        })
+    }
+
+    try {
+        var currentUsage = await limiter.check(limitRequest, `matkul-${userId}`);
+        // Log Here, ex: '{TIMESTAMP} userId {ROUTE} limit {currentUsage}/{limitRequest}'
+        newHeaders['X-Ratelimit-limit'] = limitRequest;
+        newHeaders['X-Ratelimit-Remaining'] = limitRequest - currentUsage;
+    } catch {
+        // Log Here, ex: '{TIMESTAMP} userId {ROUTE} limited'
+        return NextResponse.json({ message: `Terlalu banyak request, coba lagi dalam 1 menit` }, {
+            status: 429,
+            headers: {
+                'X-Ratelimit-Limit': limitRequest,
+                'X-Ratelimit-Remaining': 0,
+            }
+        })
+    }
+
+    // Check are formData equal to schema using 'Joi'
+    const formData = await request.json();
+    const formDataSchema = Joi.object({
+        fullname: Joi.string().pattern(/^[A-Za-z\s]*$/, 'alpha only').pattern(/^[a-zA-Z]+(\s[a-zA-Z]+)*$/, 'one space each word').min(6).max(50),
+        nickname: Joi.string().min(6).max(20),
+        jurusan: Joi.string().min(6).max(30),
+        sks_target: Joi.number().integer().min(5).max(1000),
+        matkul_target: Joi.number().integer().min(5).max(1000),
+        ipk_target: Joi.number().min(1).max(4)
+    })
+
+    try {
+        await formDataSchema.validateAsync(formData);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: error.message }, { status: 400, headers: newHeaders })
+    }
+
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        {
+            cookies: {
+                get(name) {
+                    const encryptedSession = cookieStore.get(process.env.USER_SESSION_COOKIES_NAME)?.value
+                    if (encryptedSession) {
+                        const decryptedSession = decryptSyncAES(encryptedSession) || 'removeMe';
+                        return decryptedSession;
+                    }
+                    return encryptedSession;
+                },
+                set(name, value, options) {
+                    const encryptedSession = encryptSyncAES(value);
+                    if (encryptedSession) {
+                        cookieStore.set({ name: process.env.USER_SESSION_COOKIES_NAME, value: encryptedSession, ...cookieAuthOptions })
+                    } else {
+                        cookieStore.set({ name: process.env.USER_SESSION_COOKIES_NAME, value, ...cookieAuthOptions })
+                    }
+                },
+                remove(name, options) {
+                    cookieStore.set({ name: process.env.USER_SESSION_COOKIES_NAME, value: '', ...cookieAuthDeleteOptions })
+                    cookieStore.set({ name: 's_user_id', value: '', ...cookieAuthDeleteOptions })
+                    cookieStore.set({ name: 's_access_token', value: '', ...cookieAuthDeleteOptions })
+                },
+            },
+        }
+    )
+
+    var { data: profilBaru, error } = await supabase.from('user').update({ ...formData }).eq('id', userId).select();
+
+    if (error) {
+        console.error(error);
+        return NextResponse.json({ message: `Gagal memperbarui profil` }, { status: 500, headers: newHeaders })
+    }
+
+    return NextResponse.json({ profil: profilBaru[0] }, { status: 200, headers: newHeaders })
+}
