c55332eee47491961c110fb01d5b2ed4e5517538
rheyhannh <rheyhannh@student.ub.ac.id>
2023-12-18 21:24:33 +0700

    feat: Add matakuliah route

---
+++ src/app/dashboard/matakuliah/page.jsx
@@ -0,0 +1,16 @@
+import styles from './matkul.module.css'
+
+export default function ProfilPage() {
+    return (
+        <>
+            <div className={styles.wrapper}>
+                <div>
+                    <h1>Content Profil 1</h1>
+                </div>
+                <div>
+                    <h1>Content Profil 2</h1>
+                </div>
+            </div>
+        </>
+    )
+}
