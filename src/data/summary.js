ef62b87074558fcc3d292932a5a1ffe24800fde1
rheyhannh <rheyhannh@student.ub.ac.id>
2023-12-20 20:47:00 +0700

    feat: Add new calculation method

--- src/data/summary.js
+++ src/data/summary.js
@@ -2,7 +2,7 @@
 ============================== CODE START HERE ==============================
 */
 const getUserIpk = (matkul) => {
-  if (matkul) {
+  if (matkul.length !== 0) {
     const totalSks = getUserSks(matkul);
     const { totalNilaiAkhir } = matkul.reduce((sum, current) => {
       return {
@@ -29,7 +29,7 @@ const getUserIpkPercentage = (user, matkul) => {
 }
 
 const getUserSks = (matkul) => {
-  if (matkul) {
+  if (matkul.length !== 0) {
     const { totalSks } = matkul.reduce((sum, current) => {
       return {
         totalSks: sum.totalSks + current.sks,
@@ -46,7 +46,7 @@ const getUserSksPercentage = (user, matkul) => {
   if (user.length !== 0) {
     const sks = getUserSks(matkul);
     const sksTarget = user[0].sks_target || null;
-    const sksPercentage = sksTarget ? Math.round((sks / sksTarget) * 100) : Math.round((sks / 144) * 100);  
+    const sksPercentage = sksTarget ? Math.round((sks / sksTarget) * 100) : Math.round((sks / 144) * 100);
 
     return sksPercentage > 100 ? 100 : sksPercentage
   }
@@ -74,11 +74,72 @@ const getUserMatkulPercentage = (user, matkul) => {
   return -1
 }
 
+const getAllSemester = (matkul, sort = false) => {
+  if (matkul.length !== 0) {
+    const allSemester = [...new Set(matkul.map(item => item.semester))];
+    return sort ? allSemester.sort((x, y) => x - y) : allSemester;
+  }
+
+  return -1
+}
+
+const getStatsSemester = (matkul, sort = false) => {
+  if (matkul.length !== 0) {
+    const groupBySemester = matkul.reduce((acc, item) => {
+      const key = item.semester;
+      if (!acc[key]) {
+        acc[key] = [];
+      }
+      acc[key].push(item);
+      return acc;
+    }, {});
+
+    const statsSemester = Object.values(groupBySemester).map(group => {
+      const totalNilai = group.reduce((sum, item) => sum + item.nilai.akhir, 0);
+      const totalSks = group.reduce((sum, item) => sum + item.sks, 0);
+      const count = group.length;
+      const semester = group[0].semester;
+
+      return { totalNilai, totalSks, count, semester };
+    });
+
+    return sort ? statsSemester.sort((a, b) => a.semester - b.semester) : statsSemester;
+  }
+
+  return -1
+}
+
+const getStatsByNilai = (matkul, universitas, sort = false) => {
+  if (matkul.length !== 0) {
+    const statsByNilai = Object.values(universitas).map((x, index) => {
+      const keyNilai = Object.keys(universitas);
+      const entries = matkul.filter(item => item.nilai.indeks === keyNilai[index]);
+      const totalSks = entries.reduce((sum, item) => sum + item.sks, 0);
+      const totalMatakuliah = entries.length;
+  
+      return {
+        indeks: keyNilai[index],
+        totalSks,
+        totalMatakuliah,
+        style: x.style,
+        weight: x.weight,
+      };
+    });
+
+    return sort ? statsByNilai.sort((a, b) => b.weight - a.weight) : statsByNilai;
+  }
+
+  return -1;
+}
+
 module.exports = {
   getUserIpk,
   getUserIpkPercentage,
   getUserMatkul,
   getUserMatkulPercentage,
   getUserSks,
-  getUserSksPercentage
+  getUserSksPercentage,
+  getAllSemester,
+  getStatsSemester,
+  getStatsByNilai,
 }
