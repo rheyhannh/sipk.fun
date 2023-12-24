0d48fe6fc28601deb68fc1920e24b57ff77e31e3
rheyhannh <rheyhannh@student.ub.ac.id>
2023-12-25 05:02:23 +0700

    chore: Allow google storage api

--- next.config.js
+++ next.config.js
@@ -20,7 +20,16 @@ const nextConfig = {
     },
     experimental: {
         serverActions: true,
-    }
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'storage.googleapis.com',
+                pathname: '/sipk_assets/**',
+            },
+        ],
+    },
 }
 
 module.exports = withBundleAnalyzer(nextConfig)
