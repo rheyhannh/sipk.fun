27ea6adce3ce8ae34b90c1db7ac05b4b9d2058ab
rheyhannh <rheyhannh@student.ub.ac.id>
2024-01-18 03:43:03 +0700

    feat: Add getSessionTable client utils

--- src/utils/client_side.js
+++ src/utils/client_side.js
@@ -54,3 +54,141 @@ export const getLocalTheme = () => {
     const isDarkTheme = localTheme ? localTheme === 'dark' : false;
     return isDarkTheme ? 'dark' : 'light';
 }
+
+export const getSessionTable = () => {
+    const savedState = sessionStorage.getItem('_table');
+    if (savedState) {
+        try {
+            const allowedKeys = ['nomor', 'matakuliah', 'semester', 'sks', 'nilai', 'diulang', 'target'];
+            const state = JSON.parse(savedState);
+            const validateTab = (tab) => {
+                const allowedTab = [0, 1, 2];
+                if (allowedTab.includes(tab)) {
+                    return tab;
+                }
+                return null;
+            }
+            const validatePageSize = (size) => {
+                const allowedPageSize = [5, 10, 25, 50, 100, -1];
+                if (allowedPageSize.includes(size)) {
+                    return size;
+                }
+                return null;
+            }
+            const validatePageControlPosition = (state) => {
+                const allowedPageControlPosition = [0, 1, 2];
+                if (allowedPageControlPosition.includes(state)) {
+                    return state;
+                }
+                return null;
+            }
+            const validateColumnOrder = (arr) => {
+                const isValid = () => {
+                    if (arr.length !== 7) {
+                        return false;
+                    }
+                    for (const str of allowedKeys) {
+                        if (!arr.includes(str)) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                return isValid ? arr : null;
+            }
+            const validateColumnVisibility = (obj) => {
+                const keys = Object.keys(obj);
+                if (keys.length !== 7 || !keys.every(key => allowedKeys.includes(key))) {
+                    return null;
+                }
+
+                const values = Object.values(obj);
+                if (!values.every(value => typeof value === 'boolean')) {
+                    return null;
+                }
+
+                return obj;
+            }
+            const validateColumnFilters = (arr) => {
+                const validId = ['matakuliah', 'semester', 'sks', 'nilai', 'diulang', 'target'];
+                const isValid = arr.every(obj => {
+                    if (Object.keys(obj).length !== 2 || !obj.hasOwnProperty('id') || !obj.hasOwnProperty('value')) {
+                        return false;
+                    }
+                    if (!validId.includes(obj.id)) {
+                        return false;
+                    }
+                    return true;
+                })
+                return isValid ? arr : null;
+            }
+            const validateColumnSorting = (arr) => {
+                const validId = ['matakuliah', 'semester', 'sks', 'nilai', 'diulang', 'target'];
+                const isValid = arr.every(obj => {
+                    if (Object.keys(obj).length !== 2 || !obj.hasOwnProperty('id') || !obj.hasOwnProperty('desc')) {
+                        return false;
+                    }
+                    if (!validId.includes(obj.id)) {
+                        return false;
+                    }
+                    return true;
+                })
+                return isValid ? arr : null;
+            }
+
+            if (
+                'tab' in state && typeof state.tab === 'number' &&
+                'pageSize' in state && typeof state.pageSize === 'number' &&
+                'pageIndex' in state && typeof state.pageIndex === 'number' &&
+                'pageControlPosition' in state && typeof state.pageControlPosition === 'number' &&
+                'columnOrder' in state && Array.isArray(state.columnOrder) &&
+                'columnVisibility' in state && typeof state.columnVisibility === 'object' && state.columnVisibility !== null && !Array.isArray(state.columnVisibility) &&
+                'columnFilters' in state && Array.isArray(state.columnFilters) &&
+                'columnSorting' in state && Array.isArray(state.columnSorting)
+            ) {
+                const {
+                    tab, pageSize, pageIndex,
+                    pageControlPosition, columnOrder,
+                    columnVisibility, columnFilters,
+                    columnSorting
+                }
+                    = state;
+                return {
+                    tab: validateTab(tab),
+                    pageSize: validatePageSize(pageSize),
+                    pageIndex: pageIndex,
+                    pageControlPosition: validatePageControlPosition(pageControlPosition),
+                    columnOrder: validateColumnOrder(columnOrder),
+                    columnVisibility: validateColumnVisibility(columnVisibility),
+                    columnFilters: validateColumnFilters(columnFilters),
+                    columnSorting: validateColumnSorting(columnSorting)
+                }
+            } else { throw new Error('Invalid table state') }
+        } catch (error) {
+            sessionStorage.removeItem('_table');
+            console.error(error?.message || 'Something went wrong');
+            console.error('Using table settings from preferences');
+            return {
+                tab: null,
+                pageSize: null,
+                pageIndex: null,
+                pageControlPosition: null,
+                columnOrder: null,
+                columnVisibility: null,
+                columnFilters: null,
+                columnSorting: null
+            }
+        }
+    } else {
+        return {
+            tab: null,
+            pageSize: null,
+            pageIndex: null,
+            pageControlPosition: null,
+            columnOrder: null,
+            columnVisibility: null,
+            columnFilters: null,
+            columnSorting: null
+        }
+    }
+}
