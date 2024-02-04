5bc7d2d194b572fe71359316d0afd2466a95a971
rheyhannh <rheyhannh@student.ub.ac.id>
2024-02-04 17:10:44 +0700

    feat: Add progress empty card

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1732,9 +1732,35 @@ export function Progress({ state, user, matkul, penilaian }) {
     }
 
     const EmptyCard = () => {
+        const {
+            setModal,
+            setActive,
+            setData
+        } = useContext(ModalContext);
+
+        const handleTambahModal = () => {
+            if (!penilaian) { return; }
+            setData({ penilaian });
+            setModal('tambahMatkul');
+            setTimeout(() => {
+                setActive(true);
+            }, 50)
+        }
+
         return (
-            <div>
-                Empty Progress Card
+            <div className={`${styles.progress} ${styles.flex}`}>
+                <div className={styles.empty__wrapper}>
+                    <div className={styles.empty__content} onClick={() => { handleTambahModal() }}>
+                        <Image
+                            src={'https://storage.googleapis.com/sipk_assets/tambah_matkul.svg'}
+                            width={100}
+                            height={100}
+                            alt='Tambah Matakuliah'
+                            className={styles.image}
+                        />
+                        <h5>Tambah Matakuliah</h5>
+                    </div>
+                </div>
             </div>
         )
     }
