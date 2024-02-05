a5e427be6a0dc720faad9b427dcee03143d8c343
rheyhannh <rheyhannh@student.ub.ac.id>
2024-02-05 15:23:52 +0700

    feat: Add distribusi error card

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1646,8 +1646,13 @@ export function Distribusi({ state, matkul, penilaian }) {
 
     const ErrorCard = () => {
         return (
-            <div>
-                Error Distribusi Card
+            <div className={`${styles.distribusi} ${styles.flex}`}>
+                <div className={styles.error__wrapper}>
+                    <div className={styles.error__content} onClick={handleRetry}>
+                        <h5>Gagal mengambil data</h5>
+                        <h1>&#x21bb;</h1>
+                    </div>
+                </div>
             </div>
         )
     }
