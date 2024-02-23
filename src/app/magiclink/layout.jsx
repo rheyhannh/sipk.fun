0d20750a997b45088aa0301d58daaa2e5e35d672
rheyhannh <rheyhannh@student.ub.ac.id>
2024-02-23 18:15:38 +0700

    feat: Add magiclink page

---
+++ src/app/magiclink/layout.jsx
@@ -0,0 +1,21 @@
+// ========== COMPONENT DEPEDENCY ========== //
+
+// ========== TAG / METADATA ========== //
+export const metadata = {
+    title: 'SIPK | Magiclink',
+    description: 'Lebih mudah dengan Magiclink SIPK',
+    keywords: 'sipk, sistem indeks prestasi kumulatif, kalkulator ipk, kalkulator nilai, kalkulator kuliah, ipk, indeks prestasi kumulatif, kuliah, sipk website, sipk magiclink, magiclink',
+}
+
+/*
+============================== CODE START HERE ==============================
+*/
+export default function Layout({
+    children,
+}) {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
---
+++ src/app/magiclink/page.jsx
@@ -0,0 +1,14 @@
+// ========== COMPONENT DEPEDENCY ========== //
+
+// ========== STYLE DEPEDENCY ========== //
+
+/*
+============================== CODE START HERE ==============================
+*/
+export default function Page() {
+    return (
+        <div>
+            Magiclink Page
+        </div>
+    )
+}
