8810301aa1e262797feea86168693d173c37f63e
rheyhannh <rheyhannh@student.ub.ac.id>
2024-02-27 00:39:37 +0700

    fix: Magiclink route layout and style

--- src/app/magiclink/page.jsx
+++ src/app/magiclink/page.jsx
@@ -1,14 +1,17 @@
+'use client'
+
 // ========== COMPONENT DEPEDENCY ========== //
+import { Main } from "@/component/Magiclink";
+import { useState } from "react";
 
 // ========== STYLE DEPEDENCY ========== //
+import styles from './page.module.css';
 
 /*
 ============================== CODE START HERE ==============================
 */
 export default function Page() {
     return (
-        <div>
-            Magiclink Page
-        </div>
+        <Main />
     )
 }
