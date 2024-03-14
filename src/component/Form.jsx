e55c609e00eb6a3698a84da8cb46e799cf34a8aa
rheyhannh <rheyhannh@student.ub.ac.id>
2024-03-14 22:37:04 +0700

    docs: Add and use ModalContext docs in Form

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,5 +1,17 @@
 'use client'
 
+// ========== DOCS ========== //
+/**
+ * @typedef ModalContext
+ * @type {object}
+ * @property {(modalType:'default'|'panduanDaftar'|'logout'|'perubahanTerakhirDetail'|'perubahanTerakhirConfirm'|'tambahMatkul'|'profil'|'rating'|'tabelSetting'|'tabelFilter'|'detailMatkul'|'hapusPermanentConfirm') => void} setModal Method untuk set tipe modal.
+ * @property {(modalActive:boolean) => void} setActive Method untuk mengaktifkan atau nonaktifkan modal.
+ * @property {(modalData: {isSuccess?:boolean|true, image?:any|(FaRegCircleCheck|FaRegTimesCircle), title?:string|('Yeaay!'|'Ooops!'), message?:string|('Berhasil memproses permintaanmu'|'Sepertinya ada yang salah saat memproses permintaanmu.'), actionText?:string|'Tutup'}) => void} setData Method untuk set data modal. Setiap tipe modal cenderung memiliki props yang berbeda, sehingga perlu disesuaikan. Jika tipe modal `'default'`, gunakan props yang tertera diatas.
+ * @property {'default'|'panduanDaftar'|'logout'|'perubahanTerakhirDetail'|'perubahanTerakhirConfirm'|'tambahMatkul'|'profil'|'rating'|'tabelSetting'|'tabelFilter'|'detailMatkul'|'hapusPermanentConfirm'} modal Tipe modal yang digunakan.
+ * @property {boolean} active State modal apakah aktif atau nonaktif.
+ * @property {{}} data Data modal yang digunakan.
+ */
+
 // ========== NEXT DEPEDENCY ========== //
 import Image from 'next/image';
 import { useRouter, useSearchParams } from 'next/navigation';
@@ -70,6 +82,8 @@ export function UsersForm() {
         loginMode, setLoginMode,
         isBigContent,
     } = useContext(UsersContext);
+
+    /** @type {ModalContext} */
     const {
         setModal,
         setActive,
