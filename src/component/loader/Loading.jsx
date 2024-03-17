51da6701d5ceaead811b68b5bc062f1ff3f9707a
rheyhannh <rheyhannh@student.ub.ac.id>
2024-03-17 23:00:59 +0700

    docs: Add docs

--- src/component/loader/Loading.jsx
+++ src/component/loader/Loading.jsx
@@ -18,6 +18,14 @@ export function Spinner({ size, color }) {
     )
 }
 
+/**
+ * Render spinning ball untuk feedback loading display dengan atau tanpa backdrop.
+ * @param {{active:boolean, backdrop:'none'|'transparent'|{cssCustomProps}, hideBall?:boolean|false}} props React props object.
+ * @param props.active Aktif atau tidaknya component. Apply `display:flex` saat aktif, apply `display:none` saat nonaktif.
+ * @param props.backdrop Gunakan `'none'` untuk render spinning ball tanpa backdrop, `'transparent'` untuk backdrop transparan atau `{...cssCustomProps}` untuk custom CSS styling.
+ * @param props.hideBall Sembunyikan atau tampilkan spinning ball. Saat disembunyikan, spinning ball tidak dirender bukan dihide dari CSS.
+ * @returns {ReactElement} Element react untuk render spinning ball dengan atau tanpa backdrop.
+ */
 export function Ball({ active, backdrop, hideBall = false }) {
     if (backdrop === 'none') { return hideBall ? null : <span className={styles.ball} /> }
     else if (backdrop === 'transparent') {
