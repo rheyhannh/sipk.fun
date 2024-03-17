a434112901aa397216e6189caafde91e18ad996d
rheyhannh <rheyhannh@student.ub.ac.id>
2024-03-17 19:17:01 +0700

    fix: Update reponse message

--- src/app/api/auth/check/route.js
+++ src/app/api/auth/check/route.js
@@ -105,7 +105,7 @@ export async function GET(request) {
             headers: newHeaders
         })
     } else {
-        return NextResponse.json({ message: 'Unauthorized' }, {
+        return NextResponse.json({ message: 'Unauthorized - Invalid access token' }, {
             status: 401,
             headers: newHeaders
         })
