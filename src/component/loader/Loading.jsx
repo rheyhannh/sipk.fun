bfb3d758987bc67f861022df1103cc3c1764f3d3
rheyhannh <rheyhannh@student.ub.ac.id>
2024-03-17 23:00:44 +0700

    feat: Add props to customize component styling

--- src/component/loader/Loading.jsx
+++ src/component/loader/Loading.jsx
@@ -18,18 +18,25 @@ export function Spinner({ size, color }) {
     )
 }
 
-export function Ball({ active, backdrop, backdropStyle = {}, hideBall = false }) {
-    if (backdrop) {
+export function Ball({ active, backdrop, hideBall = false }) {
+    if (backdrop === 'none') { return hideBall ? null : <span className={styles.ball} /> }
+    else if (backdrop === 'transparent') {
         return (
-            <div className={styles.backdrop} style={{ display: active ? 'flex' : 'none', ...backdropStyle }}>
+            <div className={`${styles.backdrop} ${styles.transparent}`} style={{ display: active ? 'flex' : 'none' }}>
+                {hideBall ? null : <span className={styles.ball} />}
+            </div>
+        )
+    } else if (typeof backdrop === 'object') {
+        return (
+            <div className={styles.backdrop} style={{ display: active ? 'flex' : 'none', ...backdrop }}>
                 {hideBall ? null : <span className={styles.ball} />}
             </div>
         )
     } else {
         return (
-            <>
+            <div className={styles.backdrop} style={{ display: active ? 'flex' : 'none' }}>
                 {hideBall ? null : <span className={styles.ball} />}
-            </>
+            </div>
         )
     }
 }
