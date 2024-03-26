8a35176cfbffe78f2125c6a330e9fc4532db18c0
rheyhannh <rheyhannh@student.ub.ac.id>
2024-03-27 01:43:36 +0700

    fix: Use fallback font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,6 @@ const poppins = Poppins({
   subsets: ['latin'],
   display: 'swap',
   variable: '--poppins-font',
-  adjustFontFallback: false,
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
 });
 
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -29,7 +29,6 @@ const league_spartan = League_Spartan({
     subsets: ['latin'],
     display: 'swap',
     variable: '--leaguespartan-font',
-    adjustFontFallback: false,
     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
 })
 
