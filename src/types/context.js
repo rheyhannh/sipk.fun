0e4bda388e0c660b07746bb73302b10d95feea12
rheyhannh <rheyhannh@student.ub.ac.id>
2024-04-18 09:36:28 +0700

    docs: Update modal context types

--- src/types/context.js
+++ src/types/context.js
@@ -4,9 +4,13 @@
  * @property {(modalType:'default'|'panduanDaftar'|'logout'|'perubahanTerakhirDetail'|'perubahanTerakhirConfirm'|'tambahMatkul'|'profil'|'rating'|'tabelSetting'|'tabelFilter'|'detailMatkul'|'hapusPermanentConfirm'|'akun') => void} setModal Method untuk set tipe modal.
  * @property {(modalActive:boolean) => void} setActive Method untuk mengaktifkan atau nonaktifkan modal.
  * @property {(modalData: {isSuccess?:boolean|true, image?:any|(FaRegCircleCheck|FaRegTimesCircle), title?:string|('Yeaay!'|'Ooops!'), message?:string|('Berhasil memproses permintaanmu'|'Sepertinya ada yang salah saat memproses permintaanmu.'), actionText?:string|'Tutup'}) => void} setData Method untuk set data modal. Setiap tipe modal cenderung memiliki props yang berbeda, sehingga perlu disesuaikan. Jika tipe modal `'default'`, gunakan props yang tertera diatas.
+ * @property {(modalType:'default'|'panduanDaftar'|'logout'|'perubahanTerakhirDetail'|'perubahanTerakhirConfirm'|'tambahMatkul'|'profil'|'rating'|'tabelSetting'|'tabelFilter'|'detailMatkul'|'hapusPermanentConfirm'|'akun') => void} setPrevModal Method untuk set tipe modal sebelumnya.
  * @property {'default'|'panduanDaftar'|'logout'|'perubahanTerakhirDetail'|'perubahanTerakhirConfirm'|'tambahMatkul'|'profil'|'rating'|'tabelSetting'|'tabelFilter'|'detailMatkul'|'hapusPermanentConfirm'} modal Tipe modal yang digunakan.
  * @property {boolean} active State modal apakah aktif atau nonaktif.
  * @property {{}} data Data modal yang digunakan.
+ * @property {'default'|'panduanDaftar'|'logout'|'perubahanTerakhirDetail'|'perubahanTerakhirConfirm'|'tambahMatkul'|'profil'|'rating'|'tabelSetting'|'tabelFilter'|'detailMatkul'|'hapusPermanentConfirm'} prevModal Tipe modal sebelumnya yang digunakan.
+ * @property {() => void} handleModalClose Method untuk menutup atau menonaktifkan modal.
+ * @property {() => void} handleModalPrev Method untuk kembali ke modal sebelumya jika `prevModal` tersedia.
  */
 
 /**
