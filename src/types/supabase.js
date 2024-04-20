981a95d774e55a473f7e91422a558339eab082e3
rheyhannh <rheyhannh@student.ub.ac.id>
2024-04-21 00:10:07 +0700

    docs: Destructuring object types

--- src/types/supabase.js
+++ src/types/supabase.js
@@ -28,10 +28,15 @@
  * @property {string} nama Nama matakuliah
  * @property {number} semester Semester matakuliah
  * @property {number} sks Sks matakuliah
- * @property {{indeks:string, bobot:float, akhir:float}} nilai Nilai matakuliah
+ * @property {object} nilai Nilai matakuliah
+ * @property {string} nilai.indeks Indeks nilai matakuliah
+ * @property {float} nilai.bobot Bobot nilai matakuliah
+ * @property {float} nilai.akhir Nilai akhir matakuliah
  * @property {boolean} dapat_diulang Boolean matakuliah dapat diulang atau tidak
  * @property {string} owned_by Id user pemilik matakuliah
- * @property {{indeks:string, bobot: float}} target_nilai Target nilai matakuliah
+ * @property {object} target_nilai Target nilai matakuliah
+ * @property {string} target_nilai.indeks Indeks target nilai matakuliah
+ * @property {float} target_nilai.bobot Bobot target nilai matakuliah
  * @property {number} created_at Unix timestamp matakuliah dibuat
  * @property {number} updated_at Unix timestamp terakhir matakuliah diperbarui
  * @property {number} deleted_at Unix timestamp matakuliah dihapus
