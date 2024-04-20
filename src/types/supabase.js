b81132aff6d7716b9a347759a321ba4ee30f000f
rheyhannh <rheyhannh@student.ub.ac.id>
2024-04-20 23:50:03 +0700

    docs: Add user data types

--- src/types/supabase.js
+++ src/types/supabase.js
@@ -1,4 +1,26 @@
 /**
+ * @typedef UserData
+ * @property {string} id Id user
+ * @property {string} email Email user
+ * @property {boolean} is_email_confirmed Boolean apakah email sudah dikonfirmasi
+ * @property {Date} email_confirmed_at Tanggal email dikonfirmasi
+ * @property {boolean} is_banned Boolean apakah user diban
+ * @property {Date} banned_until Tanggal berakhirnya ban user
+ * @property {string} roles Roles user
+ * @property {string} fullname Fullname user
+ * @property {string} nickname Nickname user
+ * @property {string} university University user
+ * @property {number} university_id Id university user
+ * @property {string} jurusan Jurusan atau program studi user
+ * @property {number} sks_target Target sks user
+ * @property {number} matkul_target Target matakuliah user
+ * @property {float} ipk_target Target ipk user
+ * @property {Date} created_at Tanggal user daftar
+ * @property {Date} updated_at Tanggal terakhir user memperbarui datanya
+ * @property {{table:{size:number, controlPosition:0|1|2, columnOrder:['nomor'|'matakuliah'|'semester'|'sks'|'nilai'|'diulang'|'target'|'ontarget'], columnVisibility:{nomor:boolean, matakuliah:boolean, semester:boolean, sks:boolean, nilai:boolean, diulang:boolean, target:boolean, ontarget:boolean} }}} preferences Preferences user
+ */
+
+/**
  * @typedef MatkulData
  * @type {object}
  * @property {string} id Id matakuliah
