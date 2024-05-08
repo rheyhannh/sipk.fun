0ffad26a269737c9a325780bebc7a87491ac7fa6
rheyhannh <rheyhannh@student.ub.ac.id>
2024-05-08 13:11:52 +0700

    feat: Update landing wrapper content

--- src/component/landing/Wrapper.jsx
+++ src/component/landing/Wrapper.jsx
@@ -16,12 +16,17 @@ import styles from './style/landing.module.css'
 
 export function Wrapper({ children }) {
     /** @type {ContextTypes.LandingContext} */
-    const { isRichContent, isTouchDevice } = useContext(LandingContext);
+    const { isRichContent, isTouchDevice, isAccessTokenExist } = useContext(LandingContext);
 
     return (
         <div className={styles.wrapper}>
             <h1>isRichContent : {isRichContent ? 'true' : 'false'}</h1>
             <h1>isTouchDevice : {isTouchDevice ? 'true' : 'false'}</h1>
+            <h1>
+                <a href={isAccessTokenExist ? '/dashboard' : '/users'} target={'_blank'}>
+                    {isAccessTokenExist ? 'Dashboard' : 'Login atau Daftar'}
+                </a>
+            </h1>
         </div>
     )
 }
