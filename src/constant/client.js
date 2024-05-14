670a0538f3c32c6fd1d0d9fa035d3e4a61fd9b44
rheyhannh <rheyhannh@student.ub.ac.id>
2024-05-14 08:52:04 +0700

    docs: Update property types

--- src/constant/client.js
+++ src/constant/client.js
@@ -1,12 +1,13 @@
 /**
  * @typedef {Object} NavItem Object yang merepresentasikan navigation item link
  * @property {string} text Display teks item
- * @property {string} href URL path saat item diklik, ex: `'/dashboard'`
+ * @property {string|null} id Section id saat item diklik, ex: `'feature'` (tanpa `'#'`). Property ini dapat bernilai `null`.
+ * @property {string|null} href URL path saat item diklik, ex: `'/dashboard'`. Property ini dapat bernilai `null`.
  * @property {string} icon Nama icon yang digunakan. Lihat {@link https://react-icons.github.io/react-icons/ react-icons}, ex: `'FaRocket', 'IoAdd', etc`
  * @property {string} lib Library icon yang digunakan. Lihat {@link https://react-icons.github.io/react-icons/ react-icons}, ex: `'fa', 'io5', etc`
  */
 
-/** @type {Array<NavItem>} Dashboard navigation item */
+/** @type {Array<NavItem>} Dashboard page navigation item */
 const dashboardNavItem = [
     { text: 'Dashboard', id: null, href: '/dashboard', icon: 'AiOutlineAppstore', lib: 'ai' },
     { text: 'Matakuliah', id: null, href: '/dashboard/matakuliah', icon: 'BsJournalBookmark', lib: 'bs' },
