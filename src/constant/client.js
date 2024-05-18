6ce5b59bec60361aa1322b8b86eb58305a24b07a
rheyhannh <rheyhannh@student.ub.ac.id>
2024-05-18 21:42:58 +0700

    docs: Update property name

--- src/constant/client.js
+++ src/constant/client.js
@@ -1,7 +1,7 @@
 /**
  * @typedef {Object} NavItem Object yang merepresentasikan navigation item link
  * @property {string} text Display teks item
- * @property {string|null} id Section id saat item diklik, ex: `'feature'` (tanpa `'#'`). Property ini dapat bernilai `null`.
+ * @property {string|null} sectionId Section id saat item diklik, ex: `'feature'` (tanpa `'#'`). Property ini dapat bernilai `null`.
  * @property {string|null} href URL path saat item diklik, ex: `'/dashboard'`. Property ini dapat bernilai `null`.
  * @property {string} icon Nama icon yang digunakan. Lihat {@link https://react-icons.github.io/react-icons/ react-icons}, ex: `'FaRocket', 'IoAdd', etc`
  * @property {string} lib Library icon yang digunakan. Lihat {@link https://react-icons.github.io/react-icons/ react-icons}, ex: `'fa', 'io5', etc`
