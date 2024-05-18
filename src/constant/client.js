d382ced5b4d985f1f8bb56fe782560e0e2506750
rheyhannh <rheyhannh@student.ub.ac.id>
2024-05-18 21:44:33 +0700

    docs: Add new property

--- src/constant/client.js
+++ src/constant/client.js
@@ -5,6 +5,7 @@
  * @property {string|null} href URL path saat item diklik, ex: `'/dashboard'`. Property ini dapat bernilai `null`.
  * @property {string} icon Nama icon yang digunakan. Lihat {@link https://react-icons.github.io/react-icons/ react-icons}, ex: `'FaRocket', 'IoAdd', etc`
  * @property {string} lib Library icon yang digunakan. Lihat {@link https://react-icons.github.io/react-icons/ react-icons}, ex: `'fa', 'io5', etc`
+ * @property {Array<NavItem>|null} dropdown List dropdown jika tersedia. Property ini dapat bernilai `null`. 
  */
 
 /** @type {Array<NavItem>} Dashboard page navigation item */
