f59810d736867975d755f0a437b3a49d186d8a5d
rheyhannh <rheyhannh@student.ub.ac.id>
2024-05-18 14:56:02 +0700

    fix: Major update feature section landing page

--- src/component/landing/sections/Feature.jsx
+++ src/component/landing/sections/Feature.jsx
@@ -21,11 +21,65 @@ export function Feature() {
     /** @type {ContextTypes.LandingContext} */
     const { isRichContent, isTouchDevice, isAccessTokenExist } = useContext(LandingContext);
 
+    const targetRef = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: targetRef,
+    })
+
+    const x = useTransform(scrollYProgress, [0, 1], ["0.5%", "-67.5%"]);
+    const getSectionHeight = () => {
+        console.log(`Cards Total: ${cards.length}`);
+        console.log(`Cards Gap Count: ${cards.length - 1}`);
+        const cardsMaximum = 500 * (cards.length - 2);
+        const cardsGapMaximum = 14 * 2.5 * (cards.length - 2);
+        return cardsMaximum + cardsGapMaximum + (14 * 5 * 2);
+    }
+
     return (
-        <section className={`${styles.section} ${styles.feature}`} id={'feature'}>
-            <div className={styles.section__name}>
-                <h1>Feature Card</h1>
+        <section style={{height: `400vh`}} ref={targetRef} className={`${styles.section} ${styles.feature}`} id={'feature'}>
+            <div className={styles.card_container}>
+                <motion.div style={{ x }} className={styles.card_wrapper}>
+                    {cards.map((card, index) => {
+                        return <Card card={card} key={`featureCard-${index}`} />
+                    })}
+                </motion.div>
             </div>
         </section>
     )
 }
+
+const Card = ({ card }) => {
+    return (
+        <div className={styles.card}>
+            <h2>{card.title}</h2>
+            <h3>{card.description}</h3>
+        </div>
+    )
+}
+
+const cards = [
+    {
+        title: 'Feature 1',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, vel.'
+    },
+    {
+        title: 'Feature 2',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, vel.'
+    },
+    {
+        title: 'Feature 3',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, vel.'
+    },
+    {
+        title: 'Feature 4',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, vel.'
+    },
+    {
+        title: 'Feature 5',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, vel.'
+    },
+    {
+        title: 'Feature 6',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, vel.'
+    },
+]
