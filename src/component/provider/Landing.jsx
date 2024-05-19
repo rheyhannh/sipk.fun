cd13ffe5ba6acb1e901a87a72be07f4cdd0be5fc
rheyhannh <rheyhannh@student.ub.ac.id>
2024-05-19 21:19:55 +0700

    chore: Add type deps

--- src/component/provider/Landing.jsx
+++ src/component/provider/Landing.jsx
@@ -1,5 +1,9 @@
 'use client'
 
+// #region TYPE DEPEDENCY
+import * as SupabaseTypes from '@/types/supabase';
+// #endregion
+
 // #region REACT DEPEDENCY
 import { createContext, useEffect, useState } from "react";
 // #endregion
