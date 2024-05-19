7132683e369d99cbf7ed954e7752b98a7a238e2d
rheyhannh <rheyhannh@student.ub.ac.id>
2024-05-19 21:30:49 +0700

    feat: Pass data to context provider

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -64,7 +64,7 @@ export default async function LandingPage() {
   const notifikasiData = await getNotifikasiData();
 
   return (
-    <LandingProvider>
+    <LandingProvider serverData={{ universitas: universitasData, rating: ratingData, notifikasi: notifikasiData }}>
       <Container>
         <Nav data={{ notifikasi: notifikasiData }} />
         <Wrapper>
