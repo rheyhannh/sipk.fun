64bcb8a254312fd379d46ec3af732cfe3fe149a7
rheyhannh <rheyhannh@student.ub.ac.id>
2024-05-20 17:24:57 +0700

    feat: Add reveal component to animate things

--- src/component/landing/sections/Landing.jsx
+++ src/component/landing/sections/Landing.jsx
@@ -29,3 +29,76 @@ export function Landing() {
         </section>
     )
 }
+
+const Reveal = ({ children }) => {
+    const [anim, setAnim] = useState('hide');
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+
+    const mainControls = useAnimation();
+    const sliceControls = useAnimation();
+
+    const handleHideAnimation = () => {
+        mainControls.start('hide');
+        sliceControls.start('hide');
+        setAnim('hide');
+    }
+
+    const handleShowAnimation = () => {
+        mainControls.start('show');
+        sliceControls.start('show');
+        setAnim('show');
+    }
+
+    useEffect(() => {
+        if (isInView) {
+            // Fire the animation
+            mainControls.start("show");
+            sliceControls.start("show");
+            setAnim('show');
+        }
+    }, [isInView])
+
+    return (
+        <div
+            ref={ref}
+            style={{
+                position: 'relative',
+                overflow: 'hidden',
+            }}
+            onClick={() => { anim === 'hide' ? handleShowAnimation() : handleHideAnimation() }}
+        >
+            <motion.div
+                variants={{
+                    hide: { opacity: 0, y: 75 },
+                    show: { opacity: 1, y: 0 },
+                }}
+                initial="hide"
+                animate={mainControls}
+                transition={{ duration: 0.5, delay: 0.25 }}
+            >
+                {children}
+            </motion.div>
+            <motion.div
+                variants={{
+                    hide: { left: 0 },
+                    show: { left: '100%' },
+                }}
+                initial="hide"
+                animate={sliceControls}
+                transition={{ duration: 0.5, ease: 'easeIn' }}
+                style={{
+                    position: 'absolute',
+                    top: 4,
+                    bottom: 4,
+                    left: 0,
+                    right: 0,
+                    background: 'var(--logo-second-color)',
+                    zIndex: 5,
+                }}
+            >
+
+            </motion.div>
+        </div>
+    )
+}
