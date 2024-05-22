6195e54e9a8d4e1b7624432c2987723e865c6ee9
rheyhannh <rheyhannh@student.ub.ac.id>
2024-05-22 15:49:20 +0700

    docs: Add landing page wrapper docs

--- src/component/landing/Wrapper.jsx
+++ src/component/landing/Wrapper.jsx
@@ -4,6 +4,12 @@
 import styles from './style/landing.module.css'
 // #endregion
 
+/**
+ * Render landing page wrapper
+ * @param {{children}} props React props object
+ * @param props.children Component or element children
+ * @returns {ReactElement} Element react untuk render landing page wrapper
+ */
 export function Wrapper({ children }) {
     return (
         <div className={styles.wrapper}>
