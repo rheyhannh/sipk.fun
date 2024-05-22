37ff2cdbee6086aa7f79e0bb7bbb26e238fbd8da
rheyhannh <rheyhannh@student.ub.ac.id>
2024-05-22 14:59:57 +0700

    docs: Add props typedef

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -14,6 +14,32 @@ const league_spartan = League_Spartan({
 })
 // #endregion
 
+/**
+ * @typedef LogoTextProps
+ * @type {object}
+ * @property {JSX.Element|string} as Custom element atau string sebagai container, default: `'h2'`
+ * @property {object} textProps Custom props yang digunakan selain 'className', default: `{}`
+ * @property {object} styles Custom style yang digunakan selain 'fontFamily', default: `{}`
+ */
+
+/**
+ * @typedef LogoImageProps
+ * @type {object}
+ * @property {string} src Source image logo, default: `'/logo.png'`
+ * @property {number} width Width image logo, default: `96`
+ * @property {number} height Height image logo, default: `96`
+ * @property {string} alt Alt image logo, default: `'SIPK Logo'`
+ * @property {object} imageProps Custom props lainnya yang tersedia pada component next image, default: `{}`
+ */
+
+/**
+ * @typedef LogoProps
+ * @type {object}
+ * @property {JSX.Element|string} container Custom element atau string sebagai container, default: `'div'`
+ * @property {object} containerProps Custom container props yang digunakan, default: `{}`
+ * @property {LogoImageProps} image
+ * @property {LogoTextProps} text
+ */
 export function LogoText({
     as: TextTag = 'h2',
     textProps = {},
