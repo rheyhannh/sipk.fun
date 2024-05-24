08bf8930165743d3cf64b514bf469d80caa668b5
rheyhannh <rheyhannh@student.ub.ac.id>
2024-05-24 19:02:29 +0700

    feat: Add content component

--- src/component/landing/sections/Landing.jsx
+++ src/component/landing/sections/Landing.jsx
@@ -52,19 +52,69 @@ export function Landing() {
                         </div>
                     </Reveal>
                 </div>
-                <div className={styles.text}>
-                    <Reveal>
-                        <h1 className={styles.title}>
-                            Lorem ipsum dolor sit amet.
-                        </h1>
-                    </Reveal>
-                    <Reveal>
-                        <p className={styles.description}>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error quibusdam atque repellat nam facere vitae numquam ipsum quae cum placeat.
-                        </p>
-                    </Reveal>
-                </div>
+const Content = () => {
+    const [top, setTop] = useState('0');
+    const [left, setLeft] = useState('0');
+    const [width, setWidth] = useState('0px');
+    const [height, setHeight] = useState('0px');
+
+    useEffect(() => {
+        const adjustPositions = () => {
+            const svg = document.getElementById('main-shape');
+            const path1 = document.getElementById('main-path');
+            const svgRect = svg.getBoundingClientRect();
+
+            // Get the bounding box of the path
+            const bbox1 = path1.getBBox();
+
+            // Calculate the position of the content relative to the SVG container
+            const content1Width = bbox1.width / 2;
+            const content1Height = bbox1.height / 2;
+            const content1X = bbox1.x + bbox1.width / 2 - content1Width / 2;
+            const content1Y = bbox1.y + bbox1.height / 2 - content1Height / 2;
+
+            // Adjust content positions
+            setTop(`${(content1Y / svg.viewBox.baseVal.height) * svgRect.height}px`);
+            setLeft(`${(content1X / svg.viewBox.baseVal.width) * svgRect.width}px`);
+            setWidth(`${(content1Width / svg.viewBox.baseVal.width) * svgRect.width}px`);
+            setHeight(`${(content1Height / svg.viewBox.baseVal.height) * svgRect.height}px`);
+        }
+
+        adjustPositions();
+        window.addEventListener('resize', adjustPositions);
+
+        return () => {
+            window.removeEventListener('resize', adjustPositions);
+        };
+    }, [])
+
+    return (
+        <div
+            className={styles.content}
+            style={{
+                top,
+                left,
+                width,
+                height
+            }}
+        >
+            <div className={styles.text}>
+                <FlyIn duration={0.5} delay={0}>
+                    <h1 className={styles.title}>
+                        Lorem ipsum dolor sit amet.
+                    </h1>
+                </FlyIn>
+
+                <FlyIn duration={0.5} delay={0}>
+                    <p className={styles.description}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, quis.
+                    </p>
+                </FlyIn>
             </div>
+        </div>
+    )
+}
+
 const FlyIn = ({ children, duration = 0.5, delay = 0.25 }) => {
     const [anim, setAnim] = useState('hide');
     const ref = useRef(null);
