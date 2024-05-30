2fc20160c08ac89c0e4d0fcc2375e649488f018c
rheyhannh <rheyhannh@student.ub.ac.id>
2024-05-30 16:03:46 +0700

    feat: Add content component

--- src/component/landing/sections/Introduction.jsx
+++ src/component/landing/sections/Introduction.jsx
@@ -80,3 +80,23 @@ const Wrapper = ({ children }) => {
         </div>
     )
 }
+
+const Content = ({ children }) => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                gap: '1rem',
+                background: 'red',
+                width: '100%',
+                height: '100%',
+            }}
+        >
+            {children}
+        </div>
+    )
+}
+
