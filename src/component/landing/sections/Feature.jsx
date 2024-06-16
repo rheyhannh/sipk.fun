512dc457b50e3c5250afecafdaadfac695485ddd
rheyhannh <rheyhannh@student.ub.ac.id>
2024-06-16 10:18:16 +0700

    feat: Add intro card component

--- src/component/landing/sections/Feature.jsx
+++ src/component/landing/sections/Feature.jsx
@@ -164,6 +164,199 @@ const Card = (
     )
 }
 
+const IntroCard = ({ }) => {
+    const animControls = useAnimation();
+    const [animateState, setAnimateState] = useState(false);
+    const descriptionArray = ["Kamu", "bebas", "tambah", "dan", "hapus", "matakuliah", "kamu", "secara", "dinamis.", "Gausah", "binggung", "IPK", "kamu", "jadi", "berapa,", "biar", "SIPK", "yang", "hitungin", "itu", "semua", "buat kamu."];
+
+    return (
+        <Card>
+            <motion.div
+                style={{
+                    position: 'relative',
+                    overflow: 'hidden',
+                    height: '100%',
+                    width: '100%',
+                    display: 'grid',
+                    gridTemplateColumns: '500px 1fr',
+                    gap: '1rem',
+                    border: '2px solid red',
+                }}
+                initial={'hide'}
+                animate={animControls}
+                whileInView={[
+                    'introCardBox_show',
+                    'introCardDescription_show'
+                ]}
+                transition={{
+                    delayChildren: 0.5,
+                    staggerChildren: 0.2
+                }}
+                onAnimationComplete={
+                    definition => {
+                        console.log(`${definition} selesai`)
+                    }
+                }
+            >
+                <motion.div
+                    onClick={() => { setAnimateState((value) => !value) }}
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        padding: '.5rem',
+                        border: '1px solid yellow'
+                    }}
+                >
+                    <div
+                        style={{
+                            position: 'relative',
+                            height: '200px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            padding: '1rem',
+                            border: '.5px solid purple'
+                        }}
+                    >
+                        <AnimatedTextBox
+                            text='Tambah Matakuliah'
+                            fontSize='1.2rem'
+                            color='var(--landing-copyInverse)'
+                            background='#74ff8d'
+                            enterAnimation='custom'
+                            customEnterAnimation={{
+                                hide: { opacity: 0, rotateY: -25, rotateX: -75, scale: 0.75 },
+                                introCardBox_show: { opacity: 1, rotateY: 0, rotateX: 0, scale: 1, transition: { type: 'spring', damping: 9, stiffness: 100 } },
+                                introCardBox_tambah_unhighlight: { opacity: 1, y: 0, x: 0, scale: 1, transition: { type: 'spring', damping: 8, stiffness: 100, delay: 0 } },
+                                introCardBox_tambah_highlight: { scale: 1.25, transition: { type: 'spring', damping: 8, stiffness: 100, delay: 0 } }
+                            }}
+                            style={{ fontWeight: '600', zIndex: 6, transformOrigin: 'bottom left 1.5rem' }}
+                        />
+
+                        <AnimatedTextBox
+                            text='Hapus Matakuliah'
+                            fontSize='1.35rem'
+                            color='var(--landing-copyInverse)'
+                            background='#ff747d'
+                            enterAnimation='custom'
+                            customEnterAnimation={{
+                                hide: { opacity: 0, y: (-75 / 2), x: (-75 / 2), scale: 0.25 },
+                                introCardBox_show: { opacity: 1, y: 0, x: 0, scale: 1, transition: { type: 'spring', damping: 8, stiffness: 100 } },
+                                introCardBox_hapus_unhighlight: { opacity: 1, y: 0, x: 0, scale: 1, transition: { type: 'spring', damping: 8, stiffness: 100, delay: 0 } },
+                                introCardBox_hapus_highlight: { scale: 1.25, transition: { type: 'spring', damping: 8, stiffness: 100, delay: 0 } }
+                            }}
+                            style={{ fontWeight: '600', zIndex: 5, margin: '.5rem 0 0 1rem' }}
+                        />
+
+                        <AnimatedTextBox
+                            text='Apapun Sesukamu'
+                            fontSize='1.5rem'
+                            color='var(--landing-copyInverse)'
+                            background='#ffd274'
+                            enterAnimation='custom'
+                            customEnterAnimation={{
+                                hide: { opacity: 0, y: (-75 / 3), x: (-75 / 3), scale: 0.5 },
+                                introCardBox_show: { opacity: 1, y: 0, x: 0, scale: 1, transition: { type: 'spring', damping: 7, stiffness: 100 } },
+                                introCardBox_apapun_unhighlight: { opacity: 1, y: 0, x: 0, scale: 1, transition: { type: 'spring', damping: 8, stiffness: 100, delay: 0 } },
+                                introCardBox_apapun_highlight: { scale: 1.25, transition: { type: 'spring', damping: 8, stiffness: 100, delay: 0 } }
+                            }}
+                            style={{ fontWeight: '600', zIndex: 4, margin: '.5rem 0 0 2rem' }}
+                        />
+
+                        <motion.div
+                            style={{
+                                position: 'absolute',
+                                transformOrigin: '200% -200%'
+                            }}
+                            variants={{
+                                hide: { opacity: 0, scale: 1.25, rotateX: -75, rotateY: -25, top: '50%', left: '90%', translateX: '-90%', translateY: '-50%' },
+                                introCardBox_show: { opacity: 1, scale: 1, rotateX: 0, rotateY: 0, transition: { type: 'spring', damping: 10, stiffness: 50 } }
+                            }}
+                        >
+                            <FoldingIcons
+                                contents={[
+                                    { icon: <AiOutlinePlus style={{ display: 'block', verticalAlign: 'middle' }} />, color: 'var(--landing-copyInverse)', backgroundColor: '#74ff8d' },
+                                    { icon: <AiOutlineDelete style={{ display: 'block', verticalAlign: 'middle' }} />, color: 'var(--landing-copyInverse)', backgroundColor: '#ff747d' },
+                                    { icon: <AiOutlineLike style={{ display: 'block', verticalAlign: 'middle' }} />, color: 'var(--landing-copyInverse)', backgroundColor: '#ffd274' },
+                                ]}
+                                contentOptions={{
+                                    fontSize: '3.25rem',
+                                }}
+                                dividerOptions={{
+                                    height: '1.5px'
+                                }}
+                                animationOptions={{
+                                    type: 'stateChanges',
+                                }}
+                                stateChangesOptions={{
+                                    useParentState: true,
+                                    parentStateValue: animateState,
+                                    parentStateSetter: setAnimateState,
+                                    autoUpdateParentState: true,
+                                }}
+                            />
+                        </motion.div>
+                    </div>
+
+                    <motion.div
+                        style={{
+                            overflow: 'hidden',
+                            padding: '.5rem',
+                            zIndex: 7,
+                            border: '1px grey solid'
+                        }}
+                    >
+                        <motion.p
+                            style={{
+                                display: 'flex',
+                                flexWrap: 'wrap'
+                            }}
+                            variants={{ hide: {}, introCardDescription_show: {} }}
+                            transition={{
+                                staggerChildren: 0.075,
+                            }}
+                        >
+                            {descriptionArray.map((item, index) => (
+                                <motion.span
+                                    key={`introCard-description-${index}`}
+                                    variants={{
+                                        hide: getCommonAnimationVariants('scaleFromSmall').hide,
+                                        introCardDescription_show: getCommonAnimationVariants('scaleFromSmall').show
+                                    }}
+                                    style={{
+                                        margin: '0 5px 5px 0',
+                                        fontSize: '1.375rem',
+                                        fontWeight: '500',
+                                        textAlign: 'justify',
+                                        color: 'var(--landing-copy)',
+                                    }}
+                                >
+                                    {item}
+                                </motion.span>
+                            ))}
+                        </motion.p>
+
+                    </motion.div>
+                </motion.div>
+
+                <motion.div
+                    onClick={() => { console.log(animateState) }}
+                    style={{
+                        display: 'grid',
+                        gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
+                        gap: '1rem',
+                        padding: '1rem',
+                        border: '1px solid green',
+                        overflow: 'hidden'
+                    }}
+                >
+
+                </motion.div>
+            </motion.div>
+        </Card >
+    )
+}
 /**
  * 
  * @param {Object} props
