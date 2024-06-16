7237532182eb4ca8b568a75e7a9ebd07c74d788e
rheyhannh <rheyhannh@student.ub.ac.id>
2024-06-16 10:15:48 +0700

    feat: Add utils method

--- src/component/landing/sections/Feature.jsx
+++ src/component/landing/sections/Feature.jsx
@@ -164,6 +164,28 @@ const Card = (
     )
 }
 
+const getCommonAnimationVariants = (
+    type
+) => {
+    const animations = {
+        default: { hide: { opacity: 0 }, show: { opacity: 1 } },
+        flyUp: { hide: { opacity: 0, y: 75 }, show: { opacity: 1, y: 0 } },
+        flyDown: { hide: { opacity: 0, y: -75 }, show: { opacity: 1, y: 0 } },
+        slideLeft: { hide: { opacity: 0, x: 75 }, show: { opacity: 1, x: 0 } },
+        slideRight: { hide: { opacity: 0, x: -75 }, show: { opacity: 1, x: 0 } },
+        throwUp: { hide: { opacity: 0, x: -200, y: 200, rotate: 30 }, show: { opacity: 1, x: 0, y: 0, rotate: 0 } },
+        throwDown: { hide: { opacity: 0, x: -200, y: -200, rotate: -30 }, show: { opacity: 1, x: 0, y: 0, rotate: 0 } },
+        scaleFromSmall: { hide: { opacity: 0, scale: 0.5 }, show: { opacity: 1, scale: 1 } },
+        scaleFromBig: { hide: { opacity: 0, scale: 1.5 }, show: { opacity: 1, scale: 1 } },
+        diagonalUpLeft: { hide: { opacity: 0, y: 75, x: 75 }, show: { opacity: 1, y: 0, x: 0 } },
+        diagonalUpRight: { hide: { opacity: 0, y: 75, x: -75 }, show: { opacity: 1, y: 0, x: 0 } },
+        diagonalDownLeft: { hide: { opacity: 0, y: -75, x: 75 }, show: { opacity: 1, y: 0, x: 0 } },
+        diagonalDownRight: { hide: { opacity: 0, y: -75, x: -75 }, show: { opacity: 1, y: 0, x: 0 } },
+    }
+
+    return animations[type] ?? animations['default'];
+}
+
 const cards = [
     {
         title: 'Feature 1',
