5209f95e260907271a292151f3427148f3ea8d4c
rheyhannh <rheyhannh@student.ub.ac.id>
2024-06-16 13:21:29 +0700

    feat: Add motion global types

---
+++ src/component/motion/types/_global.js
@@ -0,0 +1,6 @@
+/**
+ * @typedef CommonAnimationName
+ * @type {'default' | 'flyUp' | 'flyDown'| 'slideLeft'| 'slideRight'| 'throwUp'| 'throwDown'| 'scaleFromSmall'| 'scaleFromBig' | 'diagonalUpLeft' | 'diagonalUpRight' | 'diagonalDownLeft' | 'diagonalDownRight'}
+ */
+
+export const MotionTypes = {}
