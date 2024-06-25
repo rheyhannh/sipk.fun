fef07d8d45edade2ec65d36ccf1f455be64f9e17
rheyhannh <rheyhannh@student.ub.ac.id>
2024-06-25 15:51:53 +0700

    feat: Use demo large component

--- src/component/landing/cards/TambahHapus.jsx
+++ src/component/landing/cards/TambahHapus.jsx
@@ -111,6 +111,7 @@ const TambahHapus = () => {
                 </Highlight>
 
                 {/* <DemoMedium /> */}
+                <DemoLarge foldingCurrentIndex={foldingCurrentIndex} />
             </Layout>
         </Wrapper >
     )
