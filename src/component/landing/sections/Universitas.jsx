c9dba1e32fdcd639f0cbdec9cd04ddf406c0a478
rheyhannh <rheyhannh@student.ub.ac.id>
2024-07-01 20:53:00 +0700

    feat: Add universitas section carousel component

--- src/component/landing/sections/Universitas.jsx
+++ src/component/landing/sections/Universitas.jsx
@@ -118,6 +118,93 @@ const Details = ({ ...props }) => {
         </div>
     )
 }
+
+const Carousel = ({ ...props }) => {
+    /** @type {ContextTypes.LandingContext} */
+    const { data: { universitas } } = useContext(LandingContext);
+    const [test, setTest] = useState([]);
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    const addTest = () => {
+        const placeAt = test.length > 0 ? Math.floor(Math.random() * test.length) : 0;
+        const updatedTest = [...test];
+        updatedTest.splice(placeAt, 0, crypto.randomUUID());
+        setTest(updatedTest);
+    }
+
+    const deleteTest = () => {
+        if (test.length === 0) { return }
+        const removeAt = Math.floor(Math.random() * test.length);
+        const updatedTest = [...test];
+        updatedTest.splice(removeAt, 1);
+        setTest(updatedTest);
+    }
+
+    return (
+        <div
+            className={universitasStyles.carousel}
+            style={{
+                border: '1px solid pink',
+                position: 'relative'
+            }}
+            {...props}
+        >
+            <div
+                style={{
+                    width: '50px',
+                    height: '25px',
+                    position: 'absolute',
+                    top: '1rem',
+                    left: '50%',
+                    transform: 'translate(-50%, -1rem)',
+                    border: '0.5px solid purple',
+                    display: 'flex',
+                    justifyContent: 'space-evenly'
+                }}
+            >
+                <span
+                    onClick={deleteTest}
+                >
+                    -
+                </span>
+
+                <span
+                    onClick={addTest}
+                >
+                    +
+                </span>
+            </div>
+            <AnimatePresence>
+                {universitas.map((item, index) => (
+                    <motion.div
+                        key={`universitasItem-${item.id}`}
+                        className={universitasStyles.item}
+                        initial={{ scale: 0 }}
+                        animate={{
+                            scale: 1,
+                            boxShadow: selectedItem === index ?
+                                `0 3px 10px ${item.assets.style.color.primary ?? 'rgba(0,0,0,.5)'}`
+                                :
+                                `0 3px 10px rgba(0,0,0,.25)`
+                        }}
+                        exit={{ opacity: 0 }}
+                        onTap={() => setSelectedItem(index)}
+                        layout
+                    >
+                        <Image
+                            src={`/universitas/${item.assets.logo}`}
+                            alt={`Logo ${item.short}`}
+                            width={100}
+                            height={100}
+                            className={item.short === 'TELKOM' ? universitasStyles.telkom : item.short === 'UNPAD' ? universitasStyles.unpad : null}
+                        />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
+        </div>
+    )
+}
+
 // #endregion
 
 export default Universitas;
