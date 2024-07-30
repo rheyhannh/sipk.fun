ccf933c2d44f6f0934941f00a5aa6fb417a2d596
rheyhannh <rheyhannh@student.ub.ac.id>
2024-07-30 18:36:27 +0700

    fix: Declare component as const

--- src/component/loader/ReactIcons.jsx
+++ src/component/loader/ReactIcons.jsx
@@ -6,7 +6,7 @@ import { IconBaseProps } from 'react-icons';
 import loadable from '@loadable/component';
 // #endregion
 
-export function Icon({ name, lib, props }) {
+export const Icon = ({ name, lib, props }) => {
     const ElementIcon = loadable(() => import(`react-icons/${lib}/index.js`), {
         resolveComponent: (el) => el[name]
     });
