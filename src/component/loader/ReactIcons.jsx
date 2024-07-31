22b8835c8972bab69a8a85ab9edea38c0244119e
rheyhannh <rheyhannh@student.ub.ac.id>
2024-07-31 18:17:03 +0700

    docs: Add component docs

--- src/component/loader/ReactIcons.jsx
+++ src/component/loader/ReactIcons.jsx
@@ -6,6 +6,20 @@ import { IconBaseProps } from 'react-icons';
 import loadable from '@loadable/component';
 // #endregion
 
+/**
+ * Component untuk load icon yang tersedia pada {@link https://react-icons.github.io/react-icons/ react-icons}
+ * - Tips : Gunakan saat nama atau library icon yang ingin digunakan bergantung pada resource tertentu seperti API response
+ * @param {Object} props Icon props
+ * @param {string} props.name 
+ * Nama icon yang ingin digunakan 
+ * - Contoh : `'FaRocket'`
+ * @param {string} props.lib
+ * Library icon yang ingin digunakan 
+ * - Contoh : `'fa'`
+ * @param {IconBaseProps} props.props
+ * Props react icons yang ingin digunakan 
+ * @returns {React.ReactElement} Rendered component
+ */
 export const Icon = ({ name, lib, props }) => {
     const ElementIcon = loadable(() => import(`react-icons/${lib}/index.js`), {
         resolveComponent: (el) => el[name]
