f495c4d707573a94da33e18205eab073f2f8e3cb
rheyhannh <rheyhannh@student.ub.ac.id>
2024-08-15 16:03:14 +0700

    docs: Add types module

---
+++ src/types/universitas.js
@@ -0,0 +1,5 @@
+/** Nama universitas yang tersedia dengan format pascal case
+ * @typedef {"Universitas Brawijaya" | "Universitas Diponegoro" | "Universitas Indonesia" | "Institut Teknologi Bandung" | "Universitas Airlangga" | "Institut Pertanian Bogor" | "Institut Teknologi Sepuluh November" | "Universitas Telkom" | "Universitas Padjajaran" | "Universitas Jendral Soedirman"} Names 
+ */
+
+export const UniversitasTypes = {}
