b805958881878b828810a78f58ed7eafb4193bd3
rheyhannh <rheyhannh@student.ub.ac.id>
2024-08-15 11:44:58 +0700

    docs: Add variable type

--- src/app/api/matkul/route.js
+++ src/app/api/matkul/route.js
@@ -117,6 +117,7 @@ export async function PATCH(request) {
 
     // #region Parsing and Handle formData
     try {
+        /** @type {MatkulFormDataServer} */
         var formData = await request.json();
     } catch (error) {
         console.error(error);
