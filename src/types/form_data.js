324e533888c94b91e27767c5ff3fddc9d5d38522
rheyhannh <rheyhannh@student.ub.ac.id>
2024-08-16 12:01:20 +0700

    docs: Update module types

--- src/types/form_data.js
+++ src/types/form_data.js
@@ -3,31 +3,11 @@ import * as Supabase from './supabase';
 // #endregion
 
 /**
- * @typedef {Supabase.MatkulData} MatkulFormDataServer
+ * @typedef {Pick<Supabase.MatkulData, 'nama' | 'semester' | 'sks' | 'dapat_diulang'> & {nilai:Pick<Supabase.MatkulData['nilai'], 'indeks'>} & {target_nilai:Pick<Supabase.MatkulData['target_nilai'], 'indeks'>}} MatkulFormData
  */
 
 /**
- * @typedef {Object} MatkulFormDataClient
- * @property {string} nama 
- * Nama matakuliah dengan kriteria
- * - min_length : `3`
- * - max_length : `50`
- * - Note : Diinput melalui form oleh user `client-side`
- * @property {number} semester 
- * Semester matakuliah dengan kriteria
- * - min : `0`
- * - max : `50`
- * - Note : Diinput melalui form oleh user `client-side`
- * @property {number} sks 
- * Sks matakuliah dengan kriteria
- * - min: `0`
- * - max : `50`
- * - Note : Diinput melalui form oleh user `client-side`
- * @property {Omit<Supabase.MatkulData['nilai'], 'bobot' | 'akhir'} nilai Object yang merepresentasikan nilai matakuliah
- * @property {boolean} dapat_diulang 
- * Boolean matakuliah dapat diulang atau tidak
- * - Note : Diinput melalui form oleh user `client-side`
- * @property {Omit<Supabase.MatkulData['target_nilai'], 'bobot'>} target_nilai Object yang merepresentasikan nilai target matakuliah
+ * @typedef {Pick<Supabase.UserData, 'fullname' | 'nickname' | 'jurusan' | 'sks_target' | 'matkul_target' | 'ipk_target' | 'preferences'>} UserFormData
  */
 
 export const FormDataTypes = {}
