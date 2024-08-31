d3ea6334cb8ef08d6ed286afec7ed3be1a3dafe7
rheyhannh <rheyhannh@student.ub.ac.id>
2024-08-31 23:27:07 +0700

    feat: Add variable module

---
+++ src/constant/api_endpoint.js
@@ -0,0 +1,25 @@
+/**
+ * Object dengan key sebagai nama endpoint dengan value enpointnya. 
+ * ```js
+ * // Contoh
+ * console.log(endpointByKey['fakta']) // '/api/fakta'
+ * console.log(endpointByKey['auth/check']) // '/api/auth/check'
+ * console.log(endpointByKey['rating']) // '/api/rating'
+ * ```
+ */
+export const endpointByKey = {
+    'auth/check': '/api/auth/check',
+    'auth/confirm': '/api/auth/confirm',
+    'fakta': '/api/fakta',
+    'login': '/api/login',
+    'logout': '/api/logout',
+    'magiclink': '/api/magiclink',
+    'matkul': '/api/matkul',
+    'matkul-history': '/api/matkul-history',
+    'me': '/api/me',
+    'notifikasi': '/api/notifikasi',
+    'password': '/api/password',
+    'rating': '/api/rating',
+    'register': '/api/register',
+    'universitas': '/api/universitas',
+}
