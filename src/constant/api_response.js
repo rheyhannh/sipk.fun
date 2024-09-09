494dc349671e9a07c404f4572def5a027eb1a9a4
rheyhannh <rheyhannh@student.ub.ac.id>
2024-09-09 15:27:10 +0700

    docs: Fix type and description

--- src/constant/api_response.js
+++ src/constant/api_response.js
@@ -57,25 +57,51 @@
  * - Contoh : `'Bad Request - Invalid JSON format'`
  * @property {string | null} error.hintUrl 
  * Link atau pathname yang dapat digunakan sebagai call to action untuk user mengetahui lebih lanjut `error` yang terjadi 
- * @property {any | null} error.details
- * Error details dapat berupa object untuk mendeskripsikan field tertentu atau lainnya
+ * @property {Object} _details
+ * Error details untuk mendeskripsikan error lebih detail untuk tujuan `logging`, initial value `undefined`.
+ * Selalu destructure props ini, pastikan props ini hanya tampil pada body response jika request diinisialisasikan oleh `service` atau proses `internal` lainnya.
+ * @property {'error' | 'warning' | 'critical'} _details.severity
+ * Tingkat error yang terjadi
+ * @property {number} _details.stamp
+ * Kapan error terjadi dalam unix timestamp
+ * @property {string} _details.reason
+ * Penyebab error yang terjadi
+ * @property {string | null} _details.stack
+ * Stack error yang terjadi
+ * @property {string} _details.functionDetails
+ * Nama fungsi, letak module dan baris kode penyebab error yang terjadi
+ * - Contoh : `'validateJWT at utils/server_side.js line 27'`
+ * @property {Object<string,any>} _details.functionArgs
+ * Object yang berisikan parameter function penyebab error yang terjadi
+ * @property {Object<string,any>} _details.functionResolvedVariable
+ * Object yang berisikan variable yang diresolve function
+ * @property {Object} _details.request
+ * Object yang berisikan request data `cookies`, `ip`, `headers`
+ * @property {string} _details.request.ip
+ * Request ip
+ * @property {Array<{name:string, value:string, path:string}>} _details.request.cookies
+ * Request cookies
+ * @property {Object<string,any>} _details.request.headers
+ * Request headers
+ * @property {any | null} _details.more
+ * Instance error yang digenerate dari library lain jika tersedia
  */
 
 /** @typedef {Omit<APIResponseBaseProps, 'data'>} APIResponseErrorProps */
-/** @typedef {Omit<APIResponseBaseProps, 'error'>} APIResponseSuccessProps */
+/** @typedef {Omit<APIResponseBaseProps, 'error' | '_details'>} APIResponseSuccessProps */
 
-/** @typedef {Omit<APIResponseBaseProps, 'data' | 'code' | 'headers'>} ClientAPIResponseErrorProps */
-/** @typedef {Omit<APIResponseBaseProps, 'error' | 'code' | 'headers'>} ClientAPIResponseSuccessProps */
+/** @typedef {Omit<APIResponseBaseProps, 'data' | 'code' | 'headers' | '_details'>} ClientAPIResponseErrorProps */
+/** @typedef {Omit<APIResponseBaseProps, 'error' | 'code' | 'headers' | '_details'>} ClientAPIResponseSuccessProps */
 
 /** @typedef {'AUTH_00' | 'AUTH_01' | 'AUTH_02' | 'AUTH_03' | 'AUTH_04'} AuthErrorCodes */
 
 /** 
  * @typedef {Object} AuthErrorResponseType
- * @property {(message?:string, errorHintUrl?:string, errorDetails?:Object<string,any>, customProps?:Object<string,any>) => APIResponseErrorProps} missing_access_token
+ * @property {(message?:string, errorHintUrl?:string, errorDetails?:Omit<APIResponseBaseProps['_details'], 'stamp'>, customProps?:Object<string,any>) => APIResponseErrorProps} missing_access_token
  * Method untuk generate payload response body saat user access token atau cookie `'s_access_token'` tidak tersedia dengan `optional` parameter berikut,
  * - `message` : String untuk override default message yang ditampilkan ke user dengan `toast`
  * - `errorHintUrl` : Link atau pathname yang dapat digunakan sebagai call to action untuk user mengetahui lebih lanjut `error` yang terjadi 
- * - `errorDetails` : Error details dapat berupa object untuk mendeskripsikan field tertentu atau lainnya
+ * - `errorDetails` : Error details untuk mendeskripsikan error lebih detail untuk tujuan `logging`
  * - `customProps` : Object untuk menambah props tertentu selain status, message, code dan error
  * 
  * ```js
@@ -87,16 +113,19 @@
  *          code: 'AUTH_00',
  *          message: 'Unauthorized - Missing access token',
  *          hintUrl: errorHintUrl,
- *          details: errorDetails,
+ *      },
+ *      _details: {
+ *          stamp: Math.floor(Date.now() / 1000),
+ *          ...errorDetails
  *      },
  *      ...customProps
  * }
  * ```
- * @property {(message?:string, errorHintUrl?:string, errorDetails?:Object<string,any>, customProps?:Object<string,any>) => APIResponseErrorProps} invalid_access_token
+ * @property {(message?:string, errorHintUrl?:string, errorDetails?:Omit<APIResponseBaseProps['_details'], 'stamp'>, customProps?:Object<string,any>) => APIResponseErrorProps} invalid_access_token
  * Method untuk generate payload response body saat user access token atau cookie `'s_access_token'` tidak valid dengan `optional` parameter berikut,
  * - `message` : String untuk override default message yang ditampilkan ke user dengan `toast`
  * - `errorHintUrl` : Link atau pathname yang dapat digunakan sebagai call to action untuk user mengetahui lebih lanjut `error` yang terjadi 
- * - `errorDetails` : Error details dapat berupa object untuk mendeskripsikan field tertentu atau lainnya
+ * - `errorDetails` : Error details untuk mendeskripsikan error lebih detail untuk tujuan `logging`
  * - `customProps` : Object untuk menambah props tertentu selain status, message, code dan error
  * 
  * ```js
@@ -108,16 +137,19 @@
  *          code: 'AUTH_01',
  *          message: 'Unauthorized - Invalid access token',
  *          hintUrl: errorHintUrl,
- *          details: errorDetails,
+ *      },
+ *      _details: {
+ *          stamp: Math.floor(Date.now() / 1000),
+ *          ...errorDetails
  *      },
  *      ...customProps
  * }
  * ```
- * @property {(message?:string, errorHintUrl?:string, errorDetails?:Object<string,any>, customProps?:Object<string,any>) => APIResponseErrorProps} expired_access_token
+ * @property {(message?:string, errorHintUrl?:string, errorDetails?:Omit<APIResponseBaseProps['_details'], 'stamp'>, customProps?:Object<string,any>) => APIResponseErrorProps} expired_access_token
  * Method untuk generate payload response body saat user access token atau cookie `'s_access_token'` expired atau kadaluwarsa dengan `optional` parameter berikut,
  * - `message` : String untuk override default message yang ditampilkan ke user dengan `toast`
  * - `errorHintUrl` : Link atau pathname yang dapat digunakan sebagai call to action untuk user mengetahui lebih lanjut `error` yang terjadi 
- * - `errorDetails` : Error details dapat berupa object untuk mendeskripsikan field tertentu atau lainnya
+ * - `errorDetails` : Error details untuk mendeskripsikan error lebih detail untuk tujuan `logging`
  * - `customProps` : Object untuk menambah props tertentu selain status, message, code dan error
  * 
  * ```js
@@ -129,16 +161,19 @@
  *          code: 'AUTH_02',
  *          message: 'Unauthorized - Expired access token',
  *          hintUrl: errorHintUrl,
- *          details: errorDetails,
+ *      },
+ *      _details: {
+ *          stamp: Math.floor(Date.now() / 1000),
+ *          ...errorDetails
  *      },
  *      ...customProps
  * }
  * ```
- * @property {(message?:string, errorHintUrl?:string, errorDetails?:Object<string,any>, customProps?:Object<string,any>) => APIResponseErrorProps} missing_session
+ * @property {(message?:string, errorHintUrl?:string, errorDetails?:Omit<APIResponseBaseProps['_details'], 'stamp'>, customProps?:Object<string,any>) => APIResponseErrorProps} missing_session
  * Method untuk generate payload response body saat session atau cookie `'_Secure-auth.session-token'` tidak tersedia dengan `optional` parameter berikut,
  * - `message` : String untuk override default message yang ditampilkan ke user dengan `toast`
  * - `errorHintUrl` : Link atau pathname yang dapat digunakan sebagai call to action untuk user mengetahui lebih lanjut `error` yang terjadi 
- * - `errorDetails` : Error details dapat berupa object untuk mendeskripsikan field tertentu atau lainnya
+ * - `errorDetails` : Error details untuk mendeskripsikan error lebih detail untuk tujuan `logging`
  * - `customProps` : Object untuk menambah props tertentu selain status, message, code dan error
  * 
  * ```js
@@ -150,16 +185,19 @@
  *          code: 'AUTH_03',
  *          message: 'Unauthorized - Missing session',
  *          hintUrl: errorHintUrl,
- *          details: errorDetails,
+ *      },
+ *      _details: {
+ *          stamp: Math.floor(Date.now() / 1000),
+ *          ...errorDetails
  *      },
  *      ...customProps
  * }
  * ```
- * @property {(message?:string, errorHintUrl?:string, errorDetails?:Object<string,any>, customProps?:Object<string,any>) => APIResponseErrorProps} invalid_session
+ * @property {(message?:string, errorHintUrl?:string, errorDetails?:Omit<APIResponseBaseProps['_details'], 'stamp'>, customProps?:Object<string,any>) => APIResponseErrorProps} invalid_session
  * Method untuk generate payload response body saat session atau cookie `'_Secure-auth.session-token'` tidak valid dengan `optional` parameter berikut,
  * - `message` : String untuk override default message yang ditampilkan ke user dengan `toast`
  * - `errorHintUrl` : Link atau pathname yang dapat digunakan sebagai call to action untuk user mengetahui lebih lanjut `error` yang terjadi 
- * - `errorDetails` : Error details dapat berupa object untuk mendeskripsikan field tertentu atau lainnya
+ * - `errorDetails` : Error details untuk mendeskripsikan error lebih detail untuk tujuan `logging`
  * - `customProps` : Object untuk menambah props tertentu selain status, message, code dan error
  * 
  * ```js
@@ -171,7 +209,10 @@
  *          code: 'AUTH_04',
  *          message: 'Unauthorized - Invalid session',
  *          hintUrl: errorHintUrl,
- *          details: errorDetails,
+ *      },
+ *      _details: {
+ *          stamp: Math.floor(Date.now() / 1000),
+ *          ...errorDetails
  *      },
  *      ...customProps
  * }
@@ -264,11 +305,11 @@ export const AuthErrorResponse = {
 
 /** 
  * @typedef {Object} RatelimitErrorResponseType
- * @property {(message?:string, errorHintUrl?:string, errorDetails?:Object<string,any>, customProps?:Object<string,any>) => APIResponseErrorProps} maximum_usage
+ * @property {(message?:string, errorHintUrl?:string, errorDetails?:Omit<APIResponseBaseProps['_details'], 'stamp'>, customProps?:Object<string,any>) => APIResponseErrorProps} maximum_usage
  * Method untuk generate payload response body saat penggunaan akses atau rate limit `token` mencapai maksimal dengan `optional` parameter berikut,
  * - `message` : String untuk override default message yang ditampilkan ke user dengan `toast`
  * - `errorHintUrl` : Link atau pathname yang dapat digunakan sebagai call to action untuk user mengetahui lebih lanjut `error` yang terjadi 
- * - `errorDetails` : Error details dapat berupa object untuk mendeskripsikan field tertentu atau lainnya
+ * - `errorDetails` : Error details untuk mendeskripsikan error lebih detail untuk tujuan `logging`
  * - `customProps` : Object untuk menambah props tertentu selain status, message, code dan error
  * 
  * ```js
@@ -280,16 +321,19 @@ export const AuthErrorResponse = {
  *          code: 'RL_00',
  *          message: 'Too Many Request - Rate limit exceeded',
  *          hintUrl: errorHintUrl,
- *          details: errorDetails,
+ *      },
+ *      _details: {
+ *          stamp: Math.floor(Date.now() / 1000),
+ *          ...errorDetails
  *      },
  *      ...customProps
  * }
  * ```
- * @property {(message?:string, errorHintUrl?:string, errorDetails?:Object<string,any>, customProps?:Object<string,any>) => APIResponseErrorProps} maximum_token
+ * @property {(message?:string, errorHintUrl?:string, errorDetails?:Omit<APIResponseBaseProps['_details'], 'stamp'>, customProps?:Object<string,any>) => APIResponseErrorProps} maximum_token
  * Method untuk generate payload response body saat jumlah rate limit `token` mencapai maksimal dengan `optional` parameter berikut,
  * - `message` : String untuk override default message yang ditampilkan ke user dengan `toast`
  * - `errorHintUrl` : Link atau pathname yang dapat digunakan sebagai call to action untuk user mengetahui lebih lanjut `error` yang terjadi 
- * - `errorDetails` : Error details dapat berupa object untuk mendeskripsikan field tertentu atau lainnya
+ * - `errorDetails` : Error details untuk mendeskripsikan error lebih detail untuk tujuan `logging`
  * - `customProps` : Object untuk menambah props tertentu selain status, message, code dan error
  * 
  * ```js
@@ -301,7 +345,10 @@ export const AuthErrorResponse = {
  *          code: 'RL_01',
  *          message: 'Service Unavailable - Server is currently busy',
  *          hintUrl: errorHintUrl,
- *          details: errorDetails,
+ *      },
+ *      _details: {
+ *          stamp: Math.floor(Date.now() / 1000),
+ *          ...errorDetails
  *      },
  *      ...customProps
  * }
