d7ed506fabfed058a7e2a2f4d2ed3939c801fa85
rheyhannh <rheyhannh@student.ub.ac.id>
2024-09-30 09:32:58 +0700

    fix: Update next js config

--- next.config.js
+++ next.config.js
@@ -10,18 +10,9 @@ const nextConfig = {
             {
                 source: '/api/:path*',
                 headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" },
                     { key: 'Cache-Control', value: 'no-store, must-revalidate' },
-                    { key: 'X-Powered-By', value: 'Next.js' }
                 ]
             },
-            {
-                source: '/api/fakta',
-                headers: [
-                    { key: 'Cache-Control', value: 'max-age=21600' },
-                ]
-            }
         ]
     },
     experimental: {
