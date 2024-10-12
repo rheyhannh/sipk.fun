7991fc6244cc0502cddcebafe5c1d756ef64b752
rheyhannh <rheyhannh@student.ub.ac.id>
2024-10-12 21:17:46 +0700

    fix: Update api route handler

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,170 +1,162 @@
 // #region TYPE DEPEDENCY
 import * as SupabaseTypes from '@/types/supabase';
 import { RegisterFormData } from '@/types/form_data';
+import { APIResponseErrorProps } from '@/constant/api_response';
 // #endregion
 
 // #region NEXT DEPEDENCY
 import { NextResponse, NextRequest } from 'next/server';
-import { cookies } from 'next/headers';
-// #endregion
-
-// #region SUPABASE DEPEDENCY
-import { createServerClient } from '@supabase/ssr';
 // #endregion
 
 // #region UTIL DEPEDENCY
 import {
-    encryptAES,
-    decryptAES,
     rateLimit,
-    getCookieOptions,
-    getSipkCookies,
-    getIpFromHeaders,
+    getRequestDetails,
 } from '@/utils/server_side';
+import {
+    NotFoundErrorResponse as notFoundError,
+    ServerErrorResponse as serverError,
+} from '@/constant/api_response';
 import Joi from 'joi';
-import isUUID from 'validator/lib/isUUID';
 // #endregion
 
+// #region API HELPER DEPEDENCY
+import {
+    getLogAttributes,
+    checkRateLimit,
+    parseFormData,
+    validateFormData,
+    handleErrorResponse,
+    supabaseServerClient as supabase,
+    supabaseServiceClient as supabaseService,
+} from '@/utils/api_helper';
+// #endregion
+
+const routeMethods = ['POST'];
 const limitRequest = parseInt(process.env.API_REGISTER_REQUEST_LIMIT);
 const limiter = await rateLimit({
     interval: parseInt(process.env.API_REGISTER_TOKEN_INTERVAL_SECONDS) * 1000,
     uniqueTokenPerInterval: parseInt(process.env.API_REGISTER_MAX_TOKEN_PERINTERVAL),
 })
 
-const cookieAuthOptions = await getCookieOptions('auth', 'set');
-const cookieAuthDeleteOptions = await getCookieOptions('auth', 'remove');
-const cookieServiceOptions = await getCookieOptions('service', 'set');
-
 /**
  * Route Handler untuk `POST` `'/api/register'`
  * @param {NextRequest} request
  */
 export async function POST(request) {
-    const newHeaders = {};
-    const { serviceGuestCookie } = await getSipkCookies(request);
+    const responseHeaders = {};
+    const requestLog = await getLogAttributes(request);
+    const ratelimitLog = {};
 
-    // Identifier for Ratelimiting
-    const guestKey = await getIpFromHeaders() ?? serviceGuestCookie ?? 'public';
+    const defaultUserErrorMessage = 'Gagal memproses pendaftaran';
 
-    // #region Checking Ratelimit
     try {
-        var currentUsage = await limiter.check(limitRequest, guestKey);
-        // Log Here, ex: '{TIMESTAMP} userId {ROUTE} limit {currentUsage}/{limitRequest}'
-        newHeaders['X-Ratelimit-limit'] = limitRequest;
-        newHeaders['X-Ratelimit-Remaining'] = limitRequest - currentUsage;
-    } catch {
-        // Log Here, ex: '{TIMESTAMP} userId {ROUTE} limited'
-        return NextResponse.json({ message: `Terlalu banyak request` }, {
-            status: 429,
-            headers: {
-                'X-Ratelimit-Limit': limitRequest,
-                'X-Ratelimit-Remaining': 0,
-            }
+        await checkRateLimit(limiter, limitRequest).then(x => {
+            const { currentUsage, currentTtl, currentSize, rateLimitHeaders } = x;
+            Object.assign(responseHeaders, rateLimitHeaders);
+            Object.assign(ratelimitLog, { currentUsage, currentTtl, currentSize })
         })
-    }
-    // #endregion
 
-    const cookieStore = cookies();
-    if (!serviceGuestCookie || !isUUID(serviceGuestCookie)) {
-        const newId = crypto.randomUUID();
-        cookieStore.set({ name: 's_guest_id', value: newId, ...cookieServiceOptions });
-        return NextResponse.json({ message: 'Unauthorized - Missing guest cookie' }, {
-            status: 401,
-            headers: newHeaders
-        })
-    }
-
-    // #region Parsing and Handle formData
-    try {
         /** @type {RegisterFormData} */
-        var formData = await request.json();
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ message: 'Bad Request - Invalid JSON Format' }, {
-            status: 400,
-            headers: newHeaders
-        })
-    }
-    // #endregion
+        var formData = await parseFormData(request);
 
-    // #region Validating and Handle formData
-    try {
-        const formDataSchema =
-            Joi.object(
+        /** @type {SupabaseTypes._from<SupabaseTypes.UniversitasData} */
+        var { data: universitas, error } = await supabaseService.from('universitas').select('*').order('id', { ascending: true });
+        if (!universitas.length) {
+            throw notFoundError.resource_not_found(
+                defaultUserErrorMessage, undefined,
                 {
-                    email: Joi.string().min(6).max(100).email().required(),
-                    password: Joi.string().min(6).max(50).required(),
-                    fullname: Joi.string().min(6).max(100).required(),
-                    university: Joi.string().required(), // Add validator to make sure university and university_id match (ex: id = 1, university must Universitas Brawijaya)
-                    university_id: Joi.number().min(0).max(parseInt(process.env.DATA_UNIVERSITAS_LENGTH)).required(),
-                    token: process.env.NODE_ENV !== 'production' ? Joi.string() : Joi.string().required(),
+                    severity: 'error',
+                    reason: 'Failed to register user, cant resolve available universitas',
+                    stack: null,
+                    functionDetails: 'supabase.from at POST /api/register line 64',
+                    functionArgs: { from: 'universitas', select: '*', orderColumn: 'id', orderOptions: { ascending: true } },
+                    functionResolvedVariable: { universitas },
+                    request: await getRequestDetails(),
+                    more: error,
                 }
             )
-
-        await formDataSchema.validateAsync(formData);
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ message: error.message || 'Bad Request' }, {
-            status: 400,
-            headers: newHeaders
-        })
-    }
-    // #endregion
-
-    // #region Initiate Supabase Instance
-    const supabase = createServerClient(
-        process.env.SUPABASE_URL,
-        process.env.SUPABASE_ANON_KEY,
-        {
-            cookies: {
-                async get(name) {
-                    const encryptedSession = cookieStore.get(process.env.USER_SESSION_COOKIES_NAME)?.value
-                    if (encryptedSession) {
-                        const decryptedSession = await decryptAES(encryptedSession) || 'removeMe';
-                        return decryptedSession;
-                    }
-                    return encryptedSession;
-                },
-                async set(name, value, options) {
-                    const encryptedSession = await encryptAES(value);
-                    if (encryptedSession) {
-                        cookieStore.set({ name: process.env.USER_SESSION_COOKIES_NAME, value: encryptedSession, ...cookieAuthOptions })
-                    } else {
-                        cookieStore.set({ name: process.env.USER_SESSION_COOKIES_NAME, value, ...cookieAuthOptions })
-                    }
-                },
-                remove(name, options) {
-                    cookieStore.set({ name: process.env.USER_SESSION_COOKIES_NAME, value: '', ...cookieAuthDeleteOptions })
-                    cookieStore.set({ name: 's_user_id', value: '', ...cookieAuthDeleteOptions })
-                    cookieStore.set({ name: 's_access_token', value: '', ...cookieAuthDeleteOptions })
-                },
-            },
         }
-    )
-    // #endregion
-
-    // #region Register User and Handle Response
-    /** @type {SupabaseTypes._auth_signUp} */
-    const { data, error } = await supabase.auth.signUp({
-        email: formData.email,
-        password: formData.password,
-        options: {
-            data: {
-                fullname: formData.fullname,
-                university: formData.university,
-                university_id: formData.university_id,
-            },
-            captchaToken: formData.token,
+        if (error) {
+            throw serverError.interval_server_error(
+                defaultUserErrorMessage, undefined,
+                {
+                    severity: 'error',
+                    reason: 'Failed to regsiter user, cant resolve available universitas',
+                    stack: null,
+                    functionDetails: 'supabase.from at POST /api/register line 64',
+                    functionArgs: { from: 'universitas', select: '*', orderColumn: 'id', orderOptions: { ascending: true } },
+                    functionResolvedVariable: { universitas },
+                    request: await getRequestDetails(),
+                    more: error,
+                }
+            )
         }
-    })
 
-    if (error) {
-        return NextResponse.json({ message: error.message }, {
-            status: 500,
-            headers: newHeaders
+        const formDataSchema = Joi.object({
+            email: Joi.string().min(6).max(100).email().required(),
+            password: Joi.string().min(6).max(50).required(),
+            fullname: Joi.string().min(6).max(100).required(),
+            university_id: Joi.number().min(0).max(universitas.length).required(),
+            token: process.env.NODE_ENV !== 'production' ? Joi.string() : Joi.string().required(),
+        });
+
+        await validateFormData(formData, null, formDataSchema);
+
+        const userUniversitasName = universitas.find(item => item.id === formData.university_id)?.nama;
+
+        if (!userUniversitasName) {
+            throw notFoundError.resource_not_found(
+                defaultUserErrorMessage, undefined,
+                {
+                    severity: 'error',
+                    reason: 'Failed to register user, cant resolve universitas name',
+                    stack: null,
+                    functionDetails: 'POST /api/register line 109',
+                    functionArgs: null,
+                    functionResolvedVariable: null,
+                    request: await getRequestDetails(),
+                    more: null,
+                }
+            )
+        }
+
+        formData.university = userUniversitasName;
+
+        /** @type {SupabaseTypes._auth_signUp} */
+        var { error } = await supabase.auth.signUp({
+            email: formData.email,
+            password: formData.password,
+            options: {
+                data: {
+                    fullname: formData.fullname,
+                    university: formData.university,
+                    university_id: formData.university_id,
+                },
+                captchaToken: formData.token,
+            }
         })
-    }
 
-    return NextResponse.json({ success: true }, { status: 200, headers: newHeaders })
-    // #endregion
+        if (error) {
+            throw serverError.interval_server_error(
+                defaultUserErrorMessage, undefined,
+                {
+                    severity: 'error',
+                    reason: "Failed to register user, supabase error occurred, see details in 'more'",
+                    stack: null,
+                    functionDetails: 'supabase.auth.signUp at POST /api/register line 127',
+                    functionArgs: { formData },
+                    functionResolvedVariable: null,
+                    request: await getRequestDetails(),
+                    more: error,
+                }
+            )
+        }
+
+        return new Response(null, { status: 204, headers: responseHeaders })
+    } catch (/** @type {APIResponseErrorProps} */ error) {
+        const { body, status, headers } = await handleErrorResponse(error, requestLog, ratelimitLog, true);
+        if (headers) { Object.assign(responseHeaders, headers) }
+        return NextResponse.json(body, { status, headers: responseHeaders })
+    }
 }
