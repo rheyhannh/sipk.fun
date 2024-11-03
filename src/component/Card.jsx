dbcd450e088c9fcd99f06092eb45d0d6079657ee
rheyhannh <rheyhannh@student.ub.ac.id>
2024-11-03 08:43:14 +0700

    fix: Prevent component rerender by refactoring

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -133,137 +133,151 @@ import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
  */
 
 /**
- * Card yang menampilkan overall user data seperti total sks, total matakuliah, ipk dan lainnya
+ * Card yang menampilkan overall user data seperti total sks, total matakuliah, ipk dan lainnya.
+ * 
+ * Method ini berfungsi sebagai controller bagaimana card harus dirender berdasarkan `state` berikut,
+ * - `loading` : Card summary saat loading, menampilkan skeleton
+ * - `validating` : Card summary saat validating, menampilkan spinner
+ * - `empty` : Card summary saat konten masih kosong, menampilkan button untuk menambah mata kuliah
+ * - `error` : Card summary saat error atau data tidak dapat diperoleh, menampilkan button untuk retry
+ * - `loaded` : Card summary saat tidak ada masalah
+ * 
+ * Selain state diatas, method ini akan return `null`
  * @param {SummaryProps} props Summary props
- * @returns {React.ReactElement} Rendered component
+ * @returns {React.ReactElement | null} Rendered component
  */
 export function Summary({ state, icon, color, title, data, penilaian }) {
-    const userIdCookie = useCookies().get('s_user_id');
-    const handleRetry = () => {
-        mutate(['/api/matkul', userIdCookie])
-        mutate(['/api/me', userIdCookie])
-    }
+    if (state === 'loading') return <SummarySkeleton />;
+    if (state === 'validating') return <SummaryValidating />;
+    if (state === 'empty') return <SummaryEmpty penilaian={penilaian} />;
+    if (state === 'error') return <SummaryError />;
+    if (state === 'loaded') return <SummaryLoaded icon={icon} color={color} title={title} data={data} />;
 
-    const SkeletonCard = () => (
-        <div className={`${styles.summary} ${styles.skeleton}`}>
-            <SkeletonTheme
-                baseColor="var(--skeleton-base)"
-                highlightColor="var(--skeleton-highlight)"
+    return null;
+}
+
+/**
+ * Card summary dengan state `loading`, menampilkan skeleton dengan layout yang sudah disesuaikan
+ * @returns {React.ReactElement} Rendered component
+ */
+const SummarySkeleton = () => (
+    <div className={`${styles.summary} ${styles.skeleton}`}>
+        <SkeletonTheme
+            baseColor="var(--skeleton-base)"
+            highlightColor="var(--skeleton-highlight)"
+        >
+            <Skeleton borderRadius={'50%'} width={"100%"} height={"100%"} containerClassName={`${styles.summary__icon} ${styles.skeleton}`} />
+        </SkeletonTheme>
+
+        <div className={styles.summary__data}>
+            <div
+                style={{ width: '100%' }}
             >
-                <Skeleton borderRadius={'50%'} width={"100%"} height={"100%"} containerClassName={`${styles.summary__icon} ${styles.skeleton}`} />
-            </SkeletonTheme>
-
-            <div className={styles.summary__data}>
-                <div
-                    style={{ width: '100%' }}
-                >
-                    <h3 className={styles.summary__title}>
-                        <SkeletonTheme
-                            baseColor="var(--skeleton-base)"
-                            highlightColor="var(--skeleton-highlight)"
-                        >
-                            <Skeleton width={"80%"} height={"100%"} />
-                        </SkeletonTheme>
-                    </h3>
-                    <h1>
-                        <SkeletonTheme
-                            baseColor="var(--skeleton-base)"
-                            highlightColor="var(--skeleton-highlight)"
-                        >
-                            <Skeleton width={"60%"} height={"100%"} />
-                        </SkeletonTheme>
-                    </h1>
-                </div>
-                <SkeletonTheme
-                    baseColor="var(--skeleton-base)"
-                    highlightColor="var(--skeleton-highlight)"
-                >
-                    <Skeleton borderRadius={'50%'} containerClassName={`${styles.summary__progress} ${styles.skeleton}`} width={"100%"} height={"100%"} />
-                </SkeletonTheme>
-            </div>
-            <div>
-                <small>
+                <h3 className={styles.summary__title}>
+                    <SkeletonTheme
+                        baseColor="var(--skeleton-base)"
+                        highlightColor="var(--skeleton-highlight)"
+                    >
+                        <Skeleton width={"80%"} height={"100%"} />
+                    </SkeletonTheme>
+                </h3>
+                <h1>
                     <SkeletonTheme
                         baseColor="var(--skeleton-base)"
                         highlightColor="var(--skeleton-highlight)"
                     >
                         <Skeleton width={"60%"} height={"100%"} />
                     </SkeletonTheme>
-                </small>
+                </h1>
             </div>
-
+            <SkeletonTheme
+                baseColor="var(--skeleton-base)"
+                highlightColor="var(--skeleton-highlight)"
+            >
+                <Skeleton borderRadius={'50%'} containerClassName={`${styles.summary__progress} ${styles.skeleton}`} width={"100%"} height={"100%"} />
+            </SkeletonTheme>
         </div>
-    )
+        <div>
+            <small>
+                <SkeletonTheme
+                    baseColor="var(--skeleton-base)"
+                    highlightColor="var(--skeleton-highlight)"
+                >
+                    <Skeleton width={"60%"} height={"100%"} />
+                </SkeletonTheme>
+            </small>
+        </div>
 
-    const LoadedCard = () => {
-        const [dashArray, setDashArray] = React.useState('0 999');
+    </div>
+)
 
-        const getCircleDraw = (radius, percentage) => {
-            const roundCircum = 2 * radius * Math.PI;
-            const roundDraw = percentage * roundCircum / 100;
-            return `${roundDraw.toFixed(2)} 999`;
-        }
-
-        React.useEffect(() => {
-            setDashArray(getCircleDraw(35, data.percentage))
-        }, [data])
-
-        return (
-            <div className={`${styles.summary}`}>
-                <div style={{ background: color ? color : 'var(--first-color)' }} className={styles.summary__icon}>
-                    {icon.name && icon.lib ?
-                        <Icon name={icon.name} lib={icon.lib} props={{ size: '24px' }} />
-                        : null
-                    }
-                </div>
-
-                <div className={styles.summary__data}>
-                    <div>
-                        <h3 className={styles.summary__title}>{title}</h3>
-                        <CountUp
-                            start={0}
-                            duration={2.5}
-                            decimals={Number.isInteger(data.value) ? 0 : 2}
-                            end={data.value}
-                            delay={0}
-                        >
-                            {({ countUpRef }) => (
-                                <h1
-                                    ref={countUpRef} />
-                            )}
-                        </CountUp>
-                    </div>
-                    <div className={styles.summary__progress}>
-                        <svg
-                            className={styles.summary__progress_circle}
-                            style={{
-                                stroke: color ? color : 'var(--first-color)',
-                                strokeDasharray: dashArray
-                            }}
-                        >
-                            <circle cx='50%' cy='50%' r='35'></circle>
-                        </svg>
-                        <CountUp
-                            start={0}
-                            end={data.percentage}
-                            duration={2.5}
-                            delay={0}
-                            suffix={'%'}
-                        >
-                            {({ countUpRef }) => (
-                                <span
-                                    style={{ color: color ? color : 'var(--first-color)' }}
-                                    ref={countUpRef} />
-                            )}
-                        </CountUp>
-                    </div>
-                </div>
-                <small>{data.keterangan}</small>
+/**
+ * Card summary dengan state `validating`, menampilkan spinner dengan layout yang sudah disesuaikan
+ * @returns {React.ReactElement} Rendered component
+ */
+const SummaryValidating = () => (
+    <div className={styles.summary}>
+        <div className={styles.validating__wrapper}>
+            <div className={styles.validating__content}>
+                <Spinner size={'30px'} color={'var(--logo-second-color)'} />
             </div>
-        )
+        </div>
+    </div>
+)
+
+/**
+ * Card summary dengan state `empty`, menampilkan button untuk menambah mata kuliah
+ * @param {Pick<SummaryProps,'penilaian'>} props SummaryEmpty props
+ * @returns {React.ReactElement} Rendered component
+ */
+const SummaryEmpty = ({ penilaian }) => {
+    /** @type {ContextTypes.ModalContext} */
+    const {
+        setModal,
+        setActive,
+        setData
+    } = React.useContext(ModalContext);
+
+    const handleTambahModal = () => {
+        if (!penilaian) { return; }
+        setData({ penilaian });
+        setModal('tambahMatkul');
+        setTimeout(() => {
+            setActive(true);
+        }, 50)
     }
 
-    const ErrorCard = () => (
+    return (
+        <div className={styles.summary}>
+            <div className={styles.empty__wrapper}>
+                <div className={styles.empty__content} onClick={handleTambahModal}>
+                    <Image
+                        src={'/tambah_matkul.svg'}
+                        width={100}
+                        height={100}
+                        alt='Tambah Matakuliah'
+                        className={styles.image}
+                    />
+                    <h5>Tambah Matakuliah</h5>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+/**
+ * Card summary dengan state `error`, menampilkan button untuk retry fetch user data
+ * @returns {React.ReactElement} Rendered component
+ */
+const SummaryError = () => {
+    const userIdCookie = useCookies().get('s_user_id');
+
+    const handleRetry = () => {
+        mutate(['/api/matkul', userIdCookie])
+        mutate(['/api/me', userIdCookie])
+    }
+
+    return (
         <div className={styles.summary}>
             <div className={styles.error__wrapper}>
                 <div className={styles.error__content} onClick={handleRetry}>
@@ -273,58 +287,83 @@ export function Summary({ state, icon, color, title, data, penilaian }) {
             </div>
         </div>
     )
+}
 
-    const ValidatingCard = () => (
-        <div className={styles.summary}>
-            <div className={styles.validating__wrapper}>
-                <div className={styles.validating__content}>
-                    <Spinner size={'30px'} color={'var(--logo-second-color)'} />
-                </div>
-            </div>
-        </div>
-    )
+/**
+ * Card summary dengan state `loaded`
+ * @param {Omit<SummaryProps,'state'|'penilaian'>} props SummaryLoaded props
+ * @returns {React.ReactElement} Rendered component
+ */
+const SummaryLoaded = ({ icon, color, title, data }) => {
+    const [dashArray, setDashArray] = React.useState('0 999');
 
-    const EmptyCard = () => {
-        /** @type {ContextTypes.ModalContext} */
-        const {
-            setModal,
-            setActive,
-            setData
-        } = React.useContext(ModalContext);
-
-        const handleTambahModal = () => {
-            if (!penilaian) { return; }
-            setData({ penilaian });
-            setModal('tambahMatkul');
-            setTimeout(() => {
-                setActive(true);
-            }, 50)
-        }
-
-        return (
-            <div className={styles.summary}>
-                <div className={styles.empty__wrapper}>
-                    <div className={styles.empty__content} onClick={() => { handleTambahModal() }}>
-                        <Image
-                            src={'/tambah_matkul.svg'}
-                            width={100}
-                            height={100}
-                            alt='Tambah Matakuliah'
-                            className={styles.image}
-                        />
-                        <h5>Tambah Matakuliah</h5>
-                    </div>
-                </div>
-            </div>
-        )
+    const getCircleDraw = (radius, percentage) => {
+        const roundCircum = 2 * radius * Math.PI;
+        const roundDraw = percentage * roundCircum / 100;
+        return `${roundDraw.toFixed(2)} 999`;
     }
 
-    if (state === 'loading') { return (<SkeletonCard />) }
-    else if (state === 'loaded') { return (<LoadedCard />) }
-    else if (state === 'error') { return (<ErrorCard />) }
-    else if (state === 'validating') { return (<ValidatingCard />) }
-    else if (state === 'empty') { return (<EmptyCard />) }
-    else { return 'Unidentified Card State' }
+    React.useEffect(() => {
+        if (data) {
+            setDashArray(getCircleDraw(35, data.percentage));
+        }
+    }, [data])
+
+    return (
+        <div className={`${styles.summary}`}>
+            <div style={{ background: color ? color : 'var(--first-color)' }} className={styles.summary__icon}>
+                {icon.name && icon.lib ?
+                    <Icon name={icon.name} lib={icon.lib} props={{ size: '24px' }} />
+                    : null
+                }
+            </div>
+
+            <div className={styles.summary__data}>
+                <div>
+                    <h3 className={styles.summary__title}>{title}</h3>
+                    <CountUp
+                        start={0}
+                        duration={2.5}
+                        decimals={Number.isInteger(data.value) ? 0 : 2}
+                        end={data.value}
+                        delay={0}
+                        preserveValue={true}
+                    >
+                        {({ countUpRef }) => (
+                            <h1
+                                ref={countUpRef} />
+                        )}
+                    </CountUp>
+                </div>
+                <div className={styles.summary__progress}>
+                    <svg
+                        className={styles.summary__progress_circle}
+                        style={{
+                            stroke: color ? color : 'var(--first-color)',
+                            strokeDasharray: dashArray
+                        }}
+                    >
+                        <circle cx='50%' cy='50%' r='35'></circle>
+                    </svg>
+                    <CountUp
+                        start={0}
+                        end={data.percentage}
+                        duration={2.5}
+                        delay={0}
+                        suffix={'%'}
+                        preserveValue={true}
+                    >
+                        {({ countUpRef }) => (
+                            <span
+                                style={{ color: color ? color : 'var(--first-color)' }}
+                                ref={countUpRef} />
+                        )}
+                    </CountUp>
+                </div>
+            </div>
+            <small>{data.keterangan}</small>
+        </div>
+    )
 }
 
 /**
