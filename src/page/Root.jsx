4c12d34a56d575c64ac56e77f37f67559396aed0
rheyhannh <rheyhannh@student.ub.ac.id>
2024-11-05 14:19:07 +0700

    feat: Add footer components

--- src/page/Root.jsx
+++ src/page/Root.jsx
@@ -2137,3 +2137,108 @@ const MulaiSekarang = () => {
         </div>
     )
 }
+
+const footerShorcuts = [
+    { text: 'About', target: '#price' },
+    { text: 'Why Us', target: '#about' },
+    { text: 'University', target: '#support' },
+    { text: 'Features', target: '#features' },
+    { text: 'Testimonies', target: '#contacts' }
+]
+
+const footerSocials = [
+    { icon: <FaLinkedin size={'100%'} />, target: 'https://www.linkedin.com/' },
+    { icon: <FaTiktok size={'100%'} />, target: 'https://www.tiktok.com/' },
+    { icon: <FaTelegram size={'100%'} />, target: 'https://web.telegram.org/' },
+]
+
+const footerSublinks = [
+    { text: 'Guides', target: 'https://www.linkedin.com/' },
+    { text: 'Changelog', target: 'https://www.linkedin.com/' }
+]
+
+const footerDelayAnims = [0.125, 0.25, 0.375];
+
+const getFooterTransition = (isText = true, delayIndex = 0, isLine = false) => ({
+    initial: {
+        opacity: isText ? 0 : 1,
+        scale: isText ? 1 : 0,
+        y: isText ? delayIndex === 2 ? 50 : 75 : 0,
+    },
+    variants: {
+        show: {
+            opacity: 1,
+            scale: 1,
+            y: 0,
+        }
+    },
+    transition: {
+        duration: 1.25,
+        bounce: (isLine || isText) ? 0 : 0.25,
+        type: 'spring',
+        delay: footerDelayAnims[delayIndex]
+    }
+})
+
+const FooterOutter = ({ children }) => (
+    <div className={styles.footer_outter}>
+        {children}
+    </div>
+)
+
+const FooterWrapper = ({ children }) => (
+    <div className={styles.footer_wrapper}>
+        {children}
+    </div>
+)
+
+const Footer = () => (
+    <footer className={styles.footer}>
+        <motion.div className={styles.primary} whileInView={'show'}>
+            <motion.div className={styles.brand} {...getFooterTransition(false, 0)}
+            >
+                <LogoImage src={'/logo_fill_contrast.png'} width={60} height={60} />
+            </motion.div>
+
+            <div className={styles.shorcut}>
+                {footerShorcuts.map((item, index) => (
+                    <motion.a key={index} href={'https://chatgpt.com/'} target={'_blank'} {...getFooterTransition(true, 0)}>
+                        {item.text}
+                    </motion.a>
+                ))}
+            </div>
+
+            <div className={styles.socials}>
+                {footerSocials.map((item, index) => (
+                    <motion.a key={index} className={styles.box} href={item.target} target={'_blank'} {...getFooterTransition(false, 0)}>
+                        {item.icon}
+                    </motion.a>
+                ))}
+            </div>
+        </motion.div>
+
+        <motion.div className={styles.line} whileInView={'show'} {...getFooterTransition(false, 1, true)} />
+
+        <motion.div className={styles.secondary} whileInView={'show'} >
+            <motion.span initial={{ y: 75, opacity: 0 }} {...getFooterTransition(true, 2)}>
+                2024 All Rights Reserved.
+            </motion.span>
+            <div className={styles.sublinks}>
+                {footerSublinks.map((item, index) => (
+                    <motion.a key={index} href={item.target} target={'_blank'}{...getFooterTransition(true, 2)}>
+                        {item.text}
+                    </motion.a>
+                ))}
+            </div>
+        </motion.div>
+    </footer>
+)
+
+const MainFooter = () => (
+    <FooterOutter>
+        <FooterWrapper>
+            <Footer />
+        </FooterWrapper>
+    </FooterOutter>
+)
+
