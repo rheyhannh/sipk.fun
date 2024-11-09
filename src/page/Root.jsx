09fb72ff80a365eb89962150b3488119295dea4a
rheyhannh <rheyhannh@student.ub.ac.id>
2024-11-09 20:35:38 +0700

    feat: Add component

--- src/page/Root.jsx
+++ src/page/Root.jsx
@@ -2067,6 +2067,98 @@ const CaraPakai = ({ contents = ['x', 'y', 'z'], useAutoplay = true, autoplayOpt
     )
 }
 
+/**
+ * Props yang digunakan component `TextFitContainer`
+ * @typedef {Object} TextFitContainerProps
+ * @property {React.RefObject<HTMLElement>} containerRef
+ * Ref dari container yang digunakan, props ini dibutuhkan untuk perhitungan ukuran teks. Jika tidak tersedia, maka ukuran teks tidak akan berubah
+ * @property {keyof motion} as
+ * Tipe element yang digunakan dan tersedia pada component `motion`
+ * - Default : `'span'`
+ */
+
+/**
+ * Component untuk membuat teks dengan ukuran yang fit terhadap sebuah container.
+ * Pastikan ref dari element container dipass melalui props `containerRef`, karna jika tidak maka ukuran teks tidak akan berubah.
+ * 
+ * Perhitungan dan perubahan ukuran teks ditrigger saat,
+ * - Component mount
+ * - Perubahan width pada container 
+ * - Perubahan width dan height pada viewport
+ * 
+ * Style dapat diatur melalui props `style` walaupun secara default menggunakan `className` yang menerapkan style berikut,
+ * ```js
+ * { margin: '0 auto',  whiteSpace: 'nowrap', textAlign: 'center' }
+ * ```
+ * @param {Omit<HTMLMotionProps<any>, 'className'> & TextFitContainerProps} props TextFitContainer props
+ * @returns {React.ReactElement} Rendered component
+ */
+const TextFitContainer = ({ containerRef, as = 'span', children, ...props }) => {
+    const { width, height } = useWindowSize();
+    const textRef = React.useRef(null);
+    const TextElement = motion[as] ?? motion['span'];
+
+    const resizeText = () => {
+        if (!containerRef) return;
+
+        const container = containerRef.current;
+        const text = textRef.current;
+
+        if (!container || !text) return;
+
+        const containerWidth = container.offsetWidth;
+        let min = 1;
+        let max = 2500;
+
+        while (min <= max) {
+            const mid = Math.floor((min + max) / 2);
+            text.style.fontSize = mid + "px";
+
+            if (text.offsetWidth <= containerWidth) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
+
+        text.style.fontSize = max + "px";
+    }
+
+    React.useEffect(() => {
+        if (containerRef?.current) {
+            const resizeObserver = new ResizeObserver((entries) => {
+                for (let entry of entries) {
+                    if (entry.contentRect.width) {
+                        resizeText();
+                    }
+                }
+            })
+
+            resizeObserver.observe(containerRef.current);
+
+            return () => {
+                resizeObserver.unobserve(containerRef.current);
+            }
+        }
+    }, [containerRef, resizeText]);
+
+    React.useEffect(() => {
+        resizeText();
+    }, [width, height]);
+
+    return (
+        <TextElement
+            style={{
+                margin: "0 auto",
+                whiteSpace: "nowrap",
+                textAlign: "center",
+            }}
+            ref={textRef}
+        >
+            {children}
+        </TextElement>
+    )
+}
 const MulaiSekarang = () => {
     const title = 'Mulai Sekarang';
     const description = 'Akses semua fitur secara gratis! Daftar sekarang atau login jika sudah punya akun. Jangan lewatkan,'
