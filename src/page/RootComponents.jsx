07aea77a2494a58e5bbca2a9bb8745411023768c
rheyhannh <rheyhannh@student.ub.ac.id>
2024-11-19 15:45:39 +0700

    refactor: Scrolling carousel component

--- src/page/RootComponents.jsx
+++ src/page/RootComponents.jsx
@@ -11,6 +11,7 @@ import * as React from 'react';
 // #region HOOKS DEPEDENCY
 import useWindowSize from '@/hooks/useWindowSize';
 import { useTimeout } from 'ahooks';
+import useMeasure from 'react-use-measure'
 // #endregion
 
 // #region STYLE DEPEDENCY
@@ -18,7 +19,7 @@ import styles from './style/root.module.css';
 // #endregion
 
 // #region COMPONENT DEPEDENCY
-import { motion, useScroll, useTransform } from 'framer-motion';
+import { motion, useMotionValue, useScroll, useVelocity, useSpring, useTransform, useAnimationFrame } from 'framer-motion';
 // #endregion
 
 export const Container = ({ children }) => (
@@ -200,5 +201,145 @@ export const TextFitContainer = ({ containerRef, as = 'span', minSize = 1, maxSi
         >
             {children}
         </TextElement>
+    )
+}
+
+/**
+ * Props yang digunakan component `ScrollingCarousel`
+ * @typedef {Object} ScrollingCarouselProps
+ * @property {number} [speed]
+ * Angka positif `> 0` sebagai kecepatan scroll container
+ * - Default : `50`
+ * @property {'left' | 'right'} [initialDirection]
+ * Arah scroll container saat pertama kali mount
+ * - Default : `'left'`
+ * @property {boolean} [useHoverEffect]
+ * Boolean untuk menggunakan efek hover atau tidak, pengaturan tersedia pada props `hoverOffset`
+ * - Default : `true`
+ * @property {number} [hoverOffset]
+ * Angka positif `>= 0` sebagai offset saat container dihover. Props ini berfungsi sebagai pembagi kecepatan scroll container,
+ * sehingga semakin besar maka semakin pelan scroll container saat dihover.
+ * 
+ * - Default : `2`
+ * - Tips : Gunakan `0` untuk memberhentikan scoll container
+ * @property {'none' | 'bounce' | 'speed' | 'reverse'} [scrollEffectType]
+ * Tipe efek yang digunakan saat terjadi mouse scroll dengan keterangan berikut,
+ * - `none` : Tidak ada efek yang terjadi
+ * - `bounce` : Tidak merubah arah, menambah kecepatan scroll container dengan efek bounce saat mouse scroll berlawanan dengan arah scroll container
+ * - `speed` : Tidak merubah arah, menambah kecepatan scroll container tanpa bounce
+ * - `reverse` : Merubah arah scroll container dengan saat mouse scroll berlawanan dengan arah scroll container
+ * 
+ * Default menggunakan `none`
+ * @property {number} [contentGap]
+ * Gap content yang digunakan dalam `px`, props ini diperlukan untuk melakukan perhitungan width dari scroll container
+ * - Default : `28`
+ * @property {number} [contentRenderOffset]
+ * Props ini menyatakan bilangan bulat `> 0` berapa kali content harus dirender. Semakin besar, maka semakin panjang width dari scroll container.
+ * 
+ * Umumnya kita ingin width scroll container selalu lebih besar dari viewport agar scroll container selalu berisikan content dan tidak kosong saat animasi berjalan
+ * - Default : `3`
+ * @property {Omit<React.HTMLProps<HTMLDivElement>,'ref'>} containerProps
+ * Agar component dapat berjalan sesuai, jangan merubah atribut `display` dan `flex-direction` pada props style
+ * @property {Omit<HTMLMotionProps<'div'>, 'onHoverStart' | 'onHoverEnd'>} wrapperProps
+ * Agar component dapat berjalan sesuai, jangan merubah atribut `display`, `gap`, `width` dan `x` pada props style
+ * dan props `onHoverStart` dan `onHoverEnd`
+ */
+
+/**
+ * Component Description
+ * @param {ScrollingCarouselProps} props ScrollingCarousel props
+ * @returns {React.ReactElement} Rendered component
+ */
+export const ScrollingCarousel = ({
+    speed = 50,
+    initialDirection = 'left',
+    useHoverEffect = true,
+    hoverOffset = 2,
+    scrollEffectType = 'none',
+    contentGap = 28,
+    contentRenderOffset = 3,
+    containerProps = {},
+    wrapperProps = {},
+    children
+}) => {
+    const { style: _x, ...containerPropsFiltered } = containerProps;
+    const { style: _y, ...wrapperPropsFiltered } = wrapperProps;
+
+    const COUNT_CHILDREN = contentRenderOffset;
+
+    const speedAbs = Math.abs(speed);
+    const baseSpeed = initialDirection === 'right' ? speedAbs : (speedAbs * -1);
+    const baseX = useMotionValue(0);
+    const { scrollY } = useScroll();
+    const scrollVelocity = useVelocity(scrollY);
+    const smoothVelocity = useSpring(scrollVelocity, { damping: 50, stiffness: 400 })
+
+    const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], { clamp: false });
+    const decelerationFactor = useMotionValue(1);
+
+    const x = useTransform(baseX, (v) => v);
+    const directionFactor = React.useRef(1);
+
+    let [containerRef, { width: containerWidth }] = useMeasure();
+    let [wrapperRef, { width: wrapperWidth }] = useMeasure();
+    const { width: viewportWidth, height: viewportHeight } = useWindowSize();
+
+    useAnimationFrame((t, delta) => {
+        let moveBy = directionFactor.current * baseSpeed * (delta / 1000);
+
+        if (scrollEffectType === 'reverse') {
+            if (velocityFactor.get() < 0) {
+                directionFactor.current = -1;
+            } else if (velocityFactor.get() > 0) {
+                directionFactor.current = 1;
+            }
+            moveBy += (directionFactor.current * moveBy * velocityFactor.get());
+        } else if (scrollEffectType === 'bounce') {
+            moveBy += (moveBy * velocityFactor.get());
+        } else if (scrollEffectType === 'speed') {
+            moveBy += (moveBy * Math.abs(velocityFactor.get()));
+        }
+
+        if (decelerationFactor.get() === 0) {
+            moveBy = 0;
+        } else {
+            moveBy = moveBy / decelerationFactor.get();
+        }
+
+        if (baseX.get() > viewportWidth) {
+            baseX.set(0);
+        } else if (baseX.get() < viewportWidth * -1) {
+            baseX.set(0);
+        } else {
+            baseX.set(baseX.get() + moveBy);
+        }
+    })
+
+    return (
+        <div
+            ref={containerRef}
+            style={{
+                display: 'flex',
+                flexDirection: initialDirection === 'right' ? 'row-reverse' : 'row',
+                ...containerProps?.style
+            }}
+            {...containerPropsFiltered}
+        >
+            <motion.div
+                ref={wrapperRef}
+                style={{ display: 'flex', gap: contentGap, width: 'max-content', x, ...wrapperProps?.style }}
+                onHoverStart={() => {
+                    if (useHoverEffect) {
+                        decelerationFactor.set(Math.abs(hoverOffset))
+                    }
+                }}
+                onHoverEnd={() => { decelerationFactor.set(1) }}
+                {...wrapperPropsFiltered}
+            >
+                {Array.from({ length: COUNT_CHILDREN }, (_, index) => (
+                    <React.Fragment key={index}>{children}</React.Fragment>
+                ))}
+            </motion.div>
+        </div>
     )
 }
--- src/page/RootTestimoniSection.jsx
+++ src/page/RootTestimoniSection.jsx
@@ -15,133 +15,14 @@ import styles from './style/root.module.css';
 // #endregion
 
 // #region COMPONENT DEPEDENCY
-import { motion, useMotionValue, useScroll, useVelocity, useSpring, useTransform, useAnimationFrame } from 'framer-motion';
-import { TextFitContainer } from './RootComponents';
+import { motion } from 'framer-motion';
+import { ScrollingCarousel } from './RootComponents';
 import HighlightText from '@/component/motion/HighlightText';
-import useMeasure from 'react-use-measure'
 // #endregion
 
 // #region ICON DEPEDENCY
 import { AiFillStar } from 'react-icons/ai';
 // #endregion
-
-/**
- * Props yang digunakan component `ScrollingCarousel`
- * @typedef {Object} ScrollingCarouselProps
- * @property {number} [speed]
- * Angka positif `> 0` sebagai kecepatan scroll container
- * @property {'left' | 'right'} [initialDirection]
- * Arah scroll container saat pertama kali mount
- * - Default : `'left'`
- * @property {boolean} [useHoverEffect]
- * Boolean untuk menggunakan efek hover atau tidak, pengaturan tersedia pada props `hoverOffset`
- * @property {number} [hoverOffset]
- * Angka positif `>= 0` sebagai offset saat container dihover. Props ini berfungsi sebagai pembagi kecepatan scroll container,
- * sehingga semakin besar maka semakin pelan scroll container saat dihover.
- * 
- * - Default : `2`
- * - Tips : Gunakan `0` untuk memberhentikan scoll container
- * @property {'none' | 'bounce' | 'speed' | 'reverse'} [scrollEffectType]
- * Tipe efek yang digunakan saat terjadi mouse scroll dengan keterangan berikut,
- * - `none` : Tidak ada efek yang terjadi
- * - `bounce` : Tidak merubah arah, menambah kecepatan scroll container dengan efek bounce saat mouse scroll berlawanan dengan arah scroll container
- * - `speed` : Tidak merubah arah, menambah kecepatan scroll container tanpa bounce
- * - `reverse` : Merubah arah scroll container dengan saat mouse scroll berlawanan dengan arah scroll container
- * 
- * Default menggunakan `none`
- * @property {number} [contentGap]
- * Gap content yang digunakan dalam `px`, props ini diperlukan untuk melakukan perhitungan width dari scroll container
- * - Default : `28`
- * @property {number} [contentRenderOffset]
- * Props ini menyatakan bilangan bulat `> 0` berapa kali content harus dirender. Semakin besar, maka semakin panjang width dari scroll container.
- * 
- * Umumnya kita ingin width scroll container selalu lebih besar dari viewport agar scroll container selalu berisikan content dan tidak kosong saat animasi berjalan
- * - Default : `3`
- */
-
-/**
- * Component Description
- * @param {React.HTMLAttributes & ScrollingCarouselProps} props ScrollingCarousel props
- * @returns {React.ReactElement} Rendered component
- */
-const ScrollingCarousel = ({
-    speed = 50,
-    initialDirection = 'left',
-    useHoverEffect = true,
-    hoverOffset = 2,
-    scrollEffectType = 'none',
-    contentGap = 28,
-    contentRenderOffset = 3,
-    children
-}) => {
-    const COUNT_CHILDREN = contentRenderOffset;
-
-    const speedAbs = Math.abs(speed);
-    const baseSpeed = initialDirection === 'right' ? speedAbs : (speedAbs * -1);
-    const baseX = useMotionValue(0);
-    const { scrollY } = useScroll();
-    const scrollVelocity = useVelocity(scrollY);
-    const smoothVelocity = useSpring(scrollVelocity, { damping: 50, stiffness: 400 })
-
-    const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], { clamp: false });
-    const decelerationFactor = useMotionValue(1);
-
-    const x = useTransform(baseX, (v) => v);
-    const directionFactor = React.useRef(1);
-
-    let [containerRef, { width: containerWidth }] = useMeasure();
-    let [wrapperRef, { width: wrapperWidth }] = useMeasure();
-
-    useAnimationFrame((t, delta) => {
-        let moveBy = directionFactor.current * baseSpeed * (delta / 1000);
-
-        if (scrollEffectType === 'reverse') {
-            if (velocityFactor.get() < 0) {
-                directionFactor.current = -1;
-            } else if (velocityFactor.get() > 0) {
-                directionFactor.current = 1;
-            }
-            moveBy += (directionFactor.current * moveBy * velocityFactor.get());
-        } else if (scrollEffectType === 'bounce') {
-            moveBy += (moveBy * velocityFactor.get());
-        } else if (scrollEffectType === 'speed') {
-            moveBy += (moveBy * Math.abs(velocityFactor.get()));
-        }
-
-        if (decelerationFactor.get() === 0) {
-            moveBy = 0;
-        } else {
-            moveBy = moveBy / decelerationFactor.get();
-        }
-
-        if (baseX.get() > (((wrapperWidth - (COUNT_CHILDREN - 1) * contentGap) / COUNT_CHILDREN) + contentGap)) {
-            baseX.set(0);
-        } else if (baseX.get() < (-((wrapperWidth - (COUNT_CHILDREN - 1) * contentGap) / COUNT_CHILDREN) - contentGap)) {
-            baseX.set(0);
-        } else {
-            baseX.set(baseX.get() + moveBy);
-        }
-    })
-
-    return (
-        <div ref={containerRef} style={{ display: 'flex', flexDirection: initialDirection === 'right' ? 'row-reverse' : 'row' }}>
-            <motion.div
-                ref={wrapperRef}
-                style={{ display: 'flex', gap: contentGap, width: 'max-content', x }}
-                onHoverStart={() => {
-                    if (useHoverEffect) {
-                        decelerationFactor.set(Math.abs(hoverOffset))
-                    }
-                }}
-                onHoverEnd={() => { decelerationFactor.set(1) }}
-            >
-                {Array.from({ length: COUNT_CHILDREN }, (_, index) => (
-                    <React.Fragment key={index}>{children}</React.Fragment>
-                ))}
-            </motion.div>
-        </div>
-    )
-}
 
 const Testimoni = ({ rating }) => {
     const stars = [3, 3, 5, 4, 4, 5];
