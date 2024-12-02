6ac621b3876d35b6f81a32d1bcca112aa787a58b
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-03 06:57:58 +0700

    feat: Handle navbar on small devices

--- src/page/Root.jsx
+++ src/page/Root.jsx
@@ -12,6 +12,7 @@ import * as React from 'react';
 import { RootProvider } from '@/component/provider/Root';
 import { Container } from './RootComponents';
 import Header from './RootHeader';
+import Navbar from './RootNavbar';
 import Hero from './RootHero';
 import KenapaSipk from './RootKenapaSipkSection';
 import Universitas from './RootUniversitasSection';
@@ -38,6 +39,7 @@ export default function Root({ universitas, rating, notifikasi }) {
     return (
         <RootProvider>
             <Header />
+            <Navbar/>
             <Container>
                 <Hero />
                 <Universitas universitas={universitas} />
--- src/page/RootHeader.jsx
+++ src/page/RootHeader.jsx
@@ -15,12 +15,11 @@ import * as React from 'react';
 // #region HOOKS DEPEDENCY
 import useWindowSize from '@/hooks/useWindowSize';
 import { useLocalTheme } from '@/data/core';
-import { useTimeout } from 'ahooks';
 // #endregion
 
 // #region COMPONENT DEPEDENCY
 import { mutate } from 'swr';
-import { motion, useScroll, useMotionValueEvent } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { LogoImage } from '@/component/Main';
 import Link from '@/component/Link';
 import { animateScroll as scroll, scroller } from 'react-scroll';
@@ -97,12 +96,10 @@ const ButtonCTA = React.forwardRef(({ text = 'Lorem', onClick, href, ...props },
 ))
 
 const Header = () => {
-    const [showNavbar, setShowNavbar] = React.useState(false);
     /** @type {React.MutableRefObject<HTMLDivElement>} */
     const navRef = React.useRef(null);
     const { width: windowWidth, height: windowHeight } = useWindowSize();
     const { data: theme } = useLocalTheme();
-    const { scrollY } = useScroll();
 
     const handleChangeTheme = (newTheme) => {
         if (theme === newTheme) { return }
@@ -110,105 +107,96 @@ const Header = () => {
         mutate('localUserTheme');
     }
 
-    useTimeout(() => {
-        if (scrollY.get() > windowHeight) {
-            setShowNavbar(true);
-        }
-    }, 100)
-
-    useMotionValueEvent(scrollY, 'change', (x) => {
-        if (x > windowHeight) {
-            if (!showNavbar) setShowNavbar(true);
-        } else {
-            if (showNavbar) setShowNavbar(false);
-        }
-    });
-
     return (
         <div className={styles.header_outter}>
             <header id={'header'} className={styles.header}>
-                <nav className={styles.navbar}>
-                    <motion.div
-                        ref={navRef}
-                        initial={{ y: -150, opacity: 0 }}
-                        transition={{ duration: 0.5, bounce: 0.2 }}
-                        animate={showNavbar ? { y: 0, opacity: 1 } : { y: -150, opacity: 0 }}
-                        className={styles.nav}
-                        onKeyDown={(event) => {
-                            if (event.key === 'Tab') {
-                                if (!event.shiftKey) {
-                                    if (navRef.current && navRef.current.lastChild === document.activeElement) {
-                                        const sections = document.getElementsByTagName('section');
-                                        // TODOS should scroll to first section not second
-                                        scroller.scrollTo(sections[1].id, { offset: -75, smooth: true, duration: (x) => Math.abs(x) > 1500 ? 0 : 500 });
-                                    }
-                                }
-                            }
-                        }}
-                    >
-                        <div className={styles.logo} onClick={() => { scroll.scrollToTop({ smooth: false, duration: 1 }) }}>
-                            <LogoImage src={'/logo_fill.png'} width={24} height={24} />
-                        </div>
-
-                        {headerShorcuts.map((item, index) => (
-                            <Link
-                                key={index}
-                                item={{ href: item.href, elementId: item.elementId }}
-                                // TODOS offset still static '-75', when comes to mobile or any viewport less than 1280
-                                // offset should be corrected
-                                scrollOptions={{ offset: -75, ...item?.scrollOptions }}
-                                routingOptions={{ ...item?.routingOptions }}
-                                className={styles.link}
-                                {...(item?.isOpenNewTab || false ? { target: '_blank' } : {})}
-                                tabIndex={0}
-                                onKeyDown={(event) => {
-                                    if (event.key === 'Enter' && item?.elementId) scroller.scrollTo(item.elementId, { offset: -75 });
-                                }}
-                                onClickCapture={(event) => {
-                                    event.target.blur();
-                                    if (item?.elementId) {
-                                        const target = document.getElementById(item.elementId);
-                                        if (target) target.focus();
-                                    }
-                                }}
-                            >
-                                {item.text}
-                                {item?.isOpenNewTab && (
-                                    <FiArrowUpRight className={styles.external} />
-                                )}
-                            </Link>
-                        ))}
-
-                        <NextLink href={'/users?action=daftar'} scroll={false} passHref legacyBehavior>
-                            <ButtonCTA id={'navbar-cta'} text={'Mulai Sekarang'} />
-                        </NextLink>
-
-                        <div
-                            id={'navbar-theme'}
-                            className={styles.theme}
-                            role={'button'}
-                            tabIndex={0}
-                            onKeyDown={(event) => {
-                                if (event.key === 'Enter') {
-                                    handleChangeTheme(theme === 'dark' ? 'light' : 'dark')
-                                }
-                            }}
-                            onClick={(event) => {
-                                event.currentTarget.blur();
-                                handleChangeTheme(theme === 'dark' ? 'light' : 'dark')
-                            }}
-                        >
+                <AnimatePresence>
+                    {(windowWidth >= 768) && (
+                        <nav className={styles.navbar}>
                             <motion.div
-                                className={styles.inner}
-                                initial={false}
-                                animate={theme === 'dark' ? { y: -37.5 } : { y: 7.5 }}
+                                ref={navRef}
+                                className={styles.nav}
+                                transition={{ duration: 0.5, bounce: 0.2 }}
+                                initial={{ y: -125 }}
+                                animate={{ y: 0 }}
+                                exit={{ y: -125 }}
+                                onKeyDown={(event) => {
+                                    if (event.key === 'Tab') {
+                                        if (!event.shiftKey) {
+                                            if (navRef.current && navRef.current.lastChild === document.activeElement) {
+                                                const sections = document.getElementsByTagName('section');
+                                                // TODOS should scroll to first section not second
+                                                scroller.scrollTo(sections[1].id, { offset: -75, smooth: true, duration: (x) => Math.abs(x) > 1500 ? 0 : 500 });
+                                            }
+                                        }
+                                    }
+                                }}
                             >
-                                <FiMoon />
-                                <FiSun />
+                                <div className={styles.logo} onClick={() => { scroll.scrollToTop({ smooth: false, duration: 1 }) }}>
+                                    <LogoImage src={'/logo_fill.png'} width={24} height={24} />
+                                </div>
+
+                                {headerShorcuts.map((item, index) => (
+                                    <Link
+                                        key={index}
+                                        item={{ href: item.href, elementId: item.elementId }}
+                                        // TODOS offset still static '-75', when comes to mobile or any viewport less than 1280
+                                        // offset should be corrected
+                                        scrollOptions={{ offset: -75, ...item?.scrollOptions }}
+                                        routingOptions={{ ...item?.routingOptions }}
+                                        className={styles.link}
+                                        {...(item?.isOpenNewTab || false ? { target: '_blank' } : {})}
+                                        tabIndex={0}
+                                        onKeyDown={(event) => {
+                                            if (event.key === 'Enter' && item?.elementId) scroller.scrollTo(item.elementId, { offset: -75 });
+                                        }}
+                                        onClickCapture={(event) => {
+                                            event.target.blur();
+                                            if (item?.elementId) {
+                                                const target = document.getElementById(item.elementId);
+                                                if (target) target.focus();
+                                            }
+                                        }}
+                                    >
+                                        {item.text}
+                                        {item?.isOpenNewTab && (
+                                            <FiArrowUpRight className={styles.external} />
+                                        )}
+                                    </Link>
+                                ))}
+
+                                <NextLink href={'/users?action=daftar'} scroll={false} passHref legacyBehavior>
+                                    <ButtonCTA id={'navbar-cta'} text={'Mulai Sekarang'} />
+                                </NextLink>
+
+                                <div
+                                    id={'navbar-theme'}
+                                    className={styles.theme}
+                                    role={'button'}
+                                    tabIndex={0}
+                                    onKeyDown={(event) => {
+                                        if (event.key === 'Enter') {
+                                            handleChangeTheme(theme === 'dark' ? 'light' : 'dark')
+                                        }
+                                    }}
+                                    onClick={(event) => {
+                                        event.currentTarget.blur();
+                                        handleChangeTheme(theme === 'dark' ? 'light' : 'dark')
+                                    }}
+                                >
+                                    <motion.div
+                                        className={styles.inner}
+                                        initial={false}
+                                        animate={theme === 'dark' ? { y: -37.5 } : { y: 7.5 }}
+                                    >
+                                        <FiMoon />
+                                        <FiSun />
+                                    </motion.div>
+                                </div>
                             </motion.div>
-                        </div>
-                    </motion.div>
-                </nav>
+                        </nav>
+                    )}
+                </AnimatePresence>
             </header>
         </div>
     )
