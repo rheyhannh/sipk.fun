bbbd8202fd03998c23a8f73d7368c576229d958b
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-07 23:46:16 +0700

    docs: Add types

--- src/types/universitas.js
+++ src/types/universitas.js
@@ -2,4 +2,8 @@
  * @typedef {"Universitas Brawijaya" | "Universitas Diponegoro" | "Universitas Indonesia" | "Institut Teknologi Bandung" | "Universitas Airlangga" | "Institut Pertanian Bogor" | "Institut Teknologi Sepuluh November" | "Universitas Telkom" | "Universitas Padjajaran" | "Universitas Jendral Soedirman"} Names 
  */
 
+/** Singkatan universitas yang tersedia dengan format uppercase
+ * @typedef {"UB" | "UNDIP" | "UI" | "ITB" | "UNAIR" | "IPB" | "ITS" | "TELKOM" | "UNPAD" | "UNSOED"} Short 
+ */
+
 export const UniversitasTypes = {}
