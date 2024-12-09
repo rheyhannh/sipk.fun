51c0c011d18eeb68d24ff746b9c4ce487b34c292
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-09 15:09:17 +0700

    fix: Mount animation in small devices

--- src/page/RootFooter.jsx
+++ src/page/RootFooter.jsx
@@ -14,13 +14,17 @@ import {
 import * as React from 'react';
 // #endregion
 
+// #region HOOKS DEPEDENCY
+import useWindowSize from '@/hooks/useWindowSize';
+// #endregion
+
 // #region STYLE DEPEDENCY
 import styles from './style/root.module.css';
 // #endregion
 
 // #region COMPONENT DEPEDENCY
 import { motion } from 'framer-motion';
-import { LogoImage } from '@/component/Main';
+import { LogoSipkFillContrast } from '@/loader/StaticImages';
 import Link from '@/component/Link';
 import { animateScroll as scroll, scroller } from 'react-scroll';
 // #endregion
@@ -102,7 +106,7 @@ const footerSublinks = [
 
 const footerDelayAnims = [0.125, 0.25, 0.375];
 
-const getFooterTransition = (isText = true, delayIndex = 0, isLine = false, smallDevices = true) => ({
+const getFooterTransition = (isText = true, delayIndex = 0, isLine = false, smallDevices = false) => ({
     initial: {
         opacity: isText ? 0 : 1,
         scale: isText ? 1 : 0,
@@ -135,151 +139,137 @@ const FooterWrapper = ({ children }) => (
     </div>
 )
 
-const Footers = () => {
-    const [smallDevices, setSmallDevices] = React.useState(false);
+const Footers = ({ viewportWidth }) => (
+    <footer id={'footer'} className={styles.footer}>
+        <motion.div
+            className={styles.primary}
+            whileInView={'show'}
+            viewport={{ once: GLOBAL_VIEWPORT_ONCE }}
+        >
+            <div className={styles.brand_wrap}>
+                <motion.div className={styles.brand} onClick={() => { scroll.scrollToTop({ smooth: false, duration: 1 }) }} {...getFooterTransition(false, 0, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
+                    <LogoSipkFillContrast priority={true} />
+                </motion.div>
+            </div>
 
-    React.useEffect(() => {
-        const smallDevicesQuery = window.matchMedia(`(max-width: ${FOOTER_SMALL_DEVICES}px)`);
-        setSmallDevices(smallDevicesQuery.matches);
+            <div id={'footer-shorcuts'} className={styles.shorcut}>
+                {footerShorcuts.map((item, index) => (
+                    <motion.div key={index} {...getFooterTransition(true, 0, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
+                        <Link
+                            key={index}
+                            item={{ href: item.href, elementId: item.elementId }}
+                            // TODOS offset still static '-75', when comes to mobile or any viewport less than 1280
+                            // offset should be corrected
+                            scrollOptions={{ offset: -75, ...item?.scrollOptions }}
+                            routingOptions={{ ...item?.routingOptions }}
+                            {...(item?.isOpenNewTab || false ? { target: '_blank' } : {})}
+                            tabIndex={0}
+                            onKeyDown={(event) => {
+                                if (event.key === 'Enter' && item?.elementId) {
+                                    const target = document.getElementById(item.elementId);
+                                    scroller.scrollTo(item.elementId, { offset: -75 })
+                                    if (target) target.focus();
+                                }
+                            }}
+                            onClickCapture={(event) => {
+                                event.target.blur();
+                                if (item?.elementId) {
+                                    const target = document.getElementById(item.elementId);
+                                    if (target) target.focus();
+                                }
+                            }}
+                        >
+                            {item.text}
+                            {item?.isOpenNewTab && (
+                                <FiArrowUpRight className={styles.external} />
+                            )}
+                        </Link>
+                    </motion.div>
+                ))}
+            </div>
 
-        smallDevicesQuery.addEventListener('change', handleSmallDevicesQuery);
+            <div id={'footer-socials'} className={styles.socials}>
+                {footerSocials.map((item, index) => (
+                    <motion.a
+                        key={index}
+                        className={styles.box}
+                        href={item.target}
+                        target={'_blank'}
+                        tabIndex={0}
+                        onClickCapture={(event) => { event.currentTarget.blur() }}
+                        {...getFooterTransition(false, 0, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
+                        {item.icon}
+                    </motion.a>
+                ))}
+            </div>
+        </motion.div>
 
-        return () => {
-            smallDevicesQuery.removeEventListener('change', handleSmallDevicesQuery);
-        }
-    }, [])
+        <motion.div
+            className={styles.line}
+            whileInView={'show'}
+            viewport={{ once: GLOBAL_VIEWPORT_ONCE }}
+            {...getFooterTransition(false, 1, true, viewportWidth <= FOOTER_SMALL_DEVICES)}
+        />
 
-    /** @param {MediaQueryListEvent} event */
-    const handleSmallDevicesQuery = (event) => {
-        setSmallDevices(event.matches);
-    }
+        <motion.div
+            className={styles.secondary}
+            whileInView={'show'}
+            viewport={{ once: GLOBAL_VIEWPORT_ONCE }}
+        >
+            <motion.span initial={{ y: 75, opacity: 0 }} {...getFooterTransition(true, 2, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
+                2024 All Rights Reserved.
+            </motion.span>
+            <div id={'footer-sublinks'} className={styles.sublinks}>
+                {footerSublinks.map((item, index) => (
+                    <motion.div key={index} {...getFooterTransition(true, 2, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
+                        <Link
+                            key={index}
+                            item={{ href: item.href, elementId: item.elementId }}
+                            // TODOS offset still static '-75', when comes to mobile or any viewport less than 1280
+                            // offset should be corrected
+                            scrollOptions={{ offset: -75, ...item?.scrollOptions }}
+                            routingOptions={{ ...item?.routingOptions }}
+                            {...(item?.isOpenNewTab || false ? { target: '_blank' } : {})}
+                            tabIndex={0}
+                            onKeyDown={(event) => {
+                                if (event.key === 'Enter' && item?.elementId) {
+                                    const target = document.getElementById(item.elementId);
+                                    scroller.scrollTo(item.elementId, { offset: -75 })
+                                    if (target) target.focus();
+                                }
+                            }}
+                            onClickCapture={(event) => {
+                                event.target.blur();
+                                if (item?.elementId) {
+                                    const target = document.getElementById(item.elementId);
+                                    if (target) target.focus();
+                                }
+                            }}
+                        >
+                            {item.text}
+                            {item?.isOpenNewTab && (
+                                <FiArrowUpRight className={styles.external} />
+                            )}
+                        </Link>
+                    </motion.div>
+                ))}
+            </div>
+        </motion.div>
+    </footer>
+)
+
+const Footer = () => {
+    const { width: viewportWidth } = useWindowSize();
+
+    if (typeof viewportWidth === 'undefined') return null;
 
     return (
-        <footer id={'footer'} className={styles.footer}>
-            <motion.div
-                className={styles.primary}
-                whileInView={'show'}
-                viewport={{ once: GLOBAL_VIEWPORT_ONCE }}
-            >
-                <div className={styles.brand_wrap}>
-                    <motion.div className={styles.brand} onClick={() => { scroll.scrollToTop({ smooth: false, duration: 1 }) }} {...getFooterTransition(false, 0, false, smallDevices)}>
-                        <LogoImage src={'/logo_fill_contrast.png'} width={250} height={250} />
-                    </motion.div>
-                </div>
-
-                <div id={'footer-shorcuts'} className={styles.shorcut}>
-                    {footerShorcuts.map((item, index) => (
-                        <motion.div key={index} {...getFooterTransition(true, 0, false, smallDevices)}>
-                            <Link
-                                key={index}
-                                item={{ href: item.href, elementId: item.elementId }}
-                                // TODOS offset still static '-75', when comes to mobile or any viewport less than 1280
-                                // offset should be corrected
-                                scrollOptions={{ offset: -75, ...item?.scrollOptions }}
-                                routingOptions={{ ...item?.routingOptions }}
-                                {...(item?.isOpenNewTab || false ? { target: '_blank' } : {})}
-                                tabIndex={0}
-                                onKeyDown={(event) => {
-                                    if (event.key === 'Enter' && item?.elementId) {
-                                        const target = document.getElementById(item.elementId);
-                                        scroller.scrollTo(item.elementId, { offset: -75 })
-                                        if (target) target.focus();
-                                    }
-                                }}
-                                onClickCapture={(event) => {
-                                    event.target.blur();
-                                    if (item?.elementId) {
-                                        const target = document.getElementById(item.elementId);
-                                        if (target) target.focus();
-                                    }
-                                }}
-                            >
-                                {item.text}
-                                {item?.isOpenNewTab && (
-                                    <FiArrowUpRight className={styles.external} />
-                                )}
-                            </Link>
-                        </motion.div>
-                    ))}
-                </div>
-
-                <div id={'footer-socials'} className={styles.socials}>
-                    {footerSocials.map((item, index) => (
-                        <motion.a
-                            key={index}
-                            className={styles.box}
-                            href={item.target}
-                            target={'_blank'}
-                            tabIndex={0}
-                            onClickCapture={(event) => { event.currentTarget.blur() }}
-                            {...getFooterTransition(false, 0, false, smallDevices)}>
-                            {item.icon}
-                        </motion.a>
-                    ))}
-                </div>
-            </motion.div>
-
-            <motion.div
-                className={styles.line}
-                whileInView={'show'}
-                viewport={{ once: GLOBAL_VIEWPORT_ONCE }}
-                {...getFooterTransition(false, 1, true, smallDevices)}
-            />
-
-            <motion.div
-                className={styles.secondary}
-                whileInView={'show'}
-                viewport={{ once: GLOBAL_VIEWPORT_ONCE }}
-            >
-                <motion.span initial={{ y: 75, opacity: 0 }} {...getFooterTransition(true, 2, false, smallDevices)}>
-                    2024 All Rights Reserved.
-                </motion.span>
-                <div id={'footer-sublinks'} className={styles.sublinks}>
-                    {footerSublinks.map((item, index) => (
-                        <motion.div key={index} {...getFooterTransition(true, 2, false, smallDevices)}>
-                            <Link
-                                key={index}
-                                item={{ href: item.href, elementId: item.elementId }}
-                                // TODOS offset still static '-75', when comes to mobile or any viewport less than 1280
-                                // offset should be corrected
-                                scrollOptions={{ offset: -75, ...item?.scrollOptions }}
-                                routingOptions={{ ...item?.routingOptions }}
-                                {...(item?.isOpenNewTab || false ? { target: '_blank' } : {})}
-                                tabIndex={0}
-                                onKeyDown={(event) => {
-                                    if (event.key === 'Enter' && item?.elementId) {
-                                        const target = document.getElementById(item.elementId);
-                                        scroller.scrollTo(item.elementId, { offset: -75 })
-                                        if (target) target.focus();
-                                    }
-                                }}
-                                onClickCapture={(event) => {
-                                    event.target.blur();
-                                    if (item?.elementId) {
-                                        const target = document.getElementById(item.elementId);
-                                        if (target) target.focus();
-                                    }
-                                }}
-                            >
-                                {item.text}
-                                {item?.isOpenNewTab && (
-                                    <FiArrowUpRight className={styles.external} />
-                                )}
-                            </Link>
-                        </motion.div>
-                    ))}
-                </div>
-            </motion.div>
-        </footer>
-    )
+        <FooterOutter>
+            <FooterWrapper>
+                <Footers viewportWidth={viewportWidth} />
+            </FooterWrapper>
+        </FooterOutter>
+    );
 }
-
-const Footer = () => (
-    <FooterOutter>
-        <FooterWrapper>
-            <Footers />
-        </FooterWrapper>
-    </FooterOutter>
-)
 
 export default Footer;
