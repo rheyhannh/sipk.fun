4f5e6eff5bc128f4da7040685b1956c520944da6
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-15 14:18:51 +0700

    style: Prettier code

--- src/component/loader/Loading.jsx
+++ src/component/loader/Loading.jsx
@@ -1,18 +1,28 @@
-// ========== STYLE DEPEDENCY ========== //
-import styles from '../style/loading.module.css'
+// #region REACT DEPEDENCY
+import * as React from 'react';
+// #endregion
 
-/*
-============================== CODE START HERE ==============================
-*/
+// #region STYLE DEPEDENCY
+import styles from '../style/loading.module.css';
+// #endregion
 
 /**
- * Render spinner untuk feedback loading.
- * @param {{size?:string|'24px', color?:string|'var(--infoDark-color)'}} props React props object.
- * @param props.size Ukuran spinner.
- * @param props.color Warna spinner.
- * @returns {ReactElement} Element react untuk render spinner.
+ * Props yang digunakan component `Spinner`
+ * @typedef {Object} SpinnerProps
+ * @property {React.CSSProperties['width'] | React.CSSProperties['height']} [size]
+ * Ukuran spinner yang digunakan untuk width dan height element pada attribute `style`
+ * - Default : `'24px'`
+ * @property {React.CSSProperties['borderColor']} color
+ * Warna spinner yang digunakan untuk border color element pada attribute `style`
+ * - Default : `'var(--infoDark-color)'`
  */
-export function Spinner({ size, color }) {
+
+/**
+ * Render spinner untuk feedback loading dengan `className` tertentu.
+ * @param {Omit<React.HTMLAttributes<HTMLSpanElement>, 'className'> & SpinnerProps} props Spinner props
+ * @returns {React.ReactElement<Omit<React.HTMLAttributes<HTMLSpanElement>, 'className'> & SpinnerProps, HTMLSpanElement>} Rendered component
+ */
+export function Spinner({ size, color, ...props }) {
     return (
         <span
             style={{
@@ -21,42 +31,51 @@ export function Spinner({ size, color }) {
                 borderTop: color ? `3px solid ${color}` : '3px solid var(--infoDark-color)'
             }}
             className={styles.spinner}
+            {...props}
         >
         </span>
     )
 }
 
 /**
- * Render spinning ball untuk feedback loading display dengan atau tanpa backdrop.
- * @param {{active:boolean, backdrop:'none'|'transparent'|{cssCustomProps}, hideBall?:boolean|false}} props React props object.
- * @param props.active Aktif atau tidaknya component. Apply `display:flex` saat aktif, apply `display:none` saat nonaktif.
- * @param props.backdrop Gunakan `'none'` untuk render spinning ball tanpa backdrop, `'transparent'` untuk backdrop transparan atau `{...cssCustomProps}` untuk custom CSS styling.
- * @param props.hideBall Sembunyikan atau tampilkan spinning ball. Saat disembunyikan, spinning ball tidak dirender bukan dihide dari CSS.
- * @returns {ReactElement} Element react untuk render spinning ball dengan atau tanpa backdrop.
+ * Props yang digunakan component `Ball`
+ * @typedef {Object} BallProps
+ * @property {boolean} active
+ * Boolean sebagai aktif atau tidaknya element
+ * @property {'none' | 'transparent' | React.CSSProperties} backdrop
+ * Tipe backdrop atau overlay yang digunakan dengan penjelasan berikut,
+ * - `'none'` : Tanpa menggunakan overlay, hanya spinning ball
+ * - `'transparent'` : Untuk overlay yang transparan
+ * - `{}` : Atau sebuah object yang berisikan style yang digunakan
+ * @property {boolean} hideBall
+ * Sembunyikan spinning ball, saat truthy ini membuat element spinning ball tidak dirender
+ * - Default : `false`
  */
-export function Ball({ active, backdrop, hideBall = false }) {
+
+/**
+ * Render spinning ball untuk feedback loading dengan `className` dan `style` tertentu.
+ * @param {Omit<React.HTMLAttributes<HTMLDivElement>, 'className' | 'style'> & BallProps} props Ball props
+ * @returns {React.ReactElement<Omit<React.HTMLAttributes<HTMLDivElement>, 'className' | 'style'> & BallProps, HTMLDivElement>} Rendered component
+ */
+export function Ball({ active, backdrop, hideBall = false, ...props }) {
     if (backdrop === 'none') { return hideBall ? null : <span className={styles.ball} /> }
     else if (backdrop === 'transparent') {
         return (
-            <div className={`${styles.backdrop} ${styles.transparent}`} style={{ display: active ? 'flex' : 'none' }}>
+            <div className={`${styles.backdrop} ${styles.transparent}`} style={{ display: active ? 'flex' : 'none' }} {...props}>
                 {hideBall ? null : <span className={styles.ball} />}
             </div>
         )
     } else if (typeof backdrop === 'object') {
         return (
-            <div className={styles.backdrop} style={{ display: active ? 'flex' : 'none', ...backdrop }}>
+            <div className={styles.backdrop} style={{ display: active ? 'flex' : 'none', ...backdrop }} {...props}>
                 {hideBall ? null : <span className={styles.ball} />}
             </div>
         )
     } else {
         return (
-            <div className={styles.backdrop} style={{ display: active ? 'flex' : 'none' }}>
+            <div className={styles.backdrop} style={{ display: active ? 'flex' : 'none' }} {...props}>
                 {hideBall ? null : <span className={styles.ball} />}
             </div>
         )
     }
 }
-
-/*
-============================== CODE END HERE ==============================
-*/
