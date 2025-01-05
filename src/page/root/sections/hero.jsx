4f1a24f625c0cde54b6bcf3a593c81a787595830
rheyhannh <rheyhannh@student.ub.ac.id>
2025-01-05 23:12:01 +0700

    refactor: Hero section components

--- src/page/root/sections/hero.jsx
+++ src/page/root/sections/hero.jsx
@@ -54,12 +54,35 @@ const resolveTitleProps = (text) => ({
     }
 })
 
+const HERO_SECTION_ID = 'home';
+
+const HERO_BANNER_ID = 'hero-cta-banner';
+
 const HERO_TITLE_DELAY_OFFSET = 0.175;
 const HERO_TITLE_PARAGRAPH = [
     ['Perencanaan', 'Dinamis'],
     ['untuk', 'IPK', 'yang', 'Lebih', 'Baik']
 ]
+const HERO_TITLE_PROPS = /** @type {import('@root_page/sections/hero').TitleProps} */ ({
+    id: 'hero-title',
+    label: 'SIPK Introduction'
+});
 const HERO_TITLE_STAGGERED = shuffleArray(HERO_TITLE_PARAGRAPH.flat().map((_, index) => index * HERO_TITLE_DELAY_OFFSET));
+
+const HERO_DESCRIPTION_TEXT = 'SIPK memungkinkan kalian menyimpan seluruh matakuliah dengan SKS dan nilai yang kalian tentukan sendiri untuk memvisualisasikan perolehan IPK secara lebih fleksibel dan proaktif.'
+
+const HERO_BUTTONS = /** @type {Array<import('@root_page/sections/hero').ButtonItemProps>} */ ([
+    {
+        id: 'hero-cta-main',
+        text: 'Mulai Sekarang',
+        href: '/users?action=daftar&utm_source=slp'
+    },
+    {
+        id: 'hero-cta-secondary',
+        text: 'Pelajari Lebih Lanjut',
+        href: '#universitas'
+    }
+])
 
 const HERO_CUSTOM_CHAR_VARIANTS = {
 
@@ -138,176 +161,29 @@ const Hero = ({ notifikasi }) => {
         /** @type {HTMLElement} */
         (null)
     )
-    const [alreadyInView, setAlreadyInView] = React.useState(false);
 
     return (
         <motion.section
-            id={'home'}
+            id={HERO_SECTION_ID}
             ref={sectionRef}
             className={`${styles.section} ${styles.hero}`}
             whileInView={'inView'}
             viewport={{ once: true }}
         >
             <ContainerWrapper style={{ position: 'relative', zIndex: 2 }}>
-                <VisibilityWrapper>
-                    <motion.div
-                        className={styles.banner}
-                        initial={{ opacity: 0, y: -40 }}
-                        variants={{
-                            inView: { opacity: 1, y: 0, transition: { duration: 2.25, delay: 0.375, type: 'spring', bounce: 0.3 } }
-                        }}
-                    >
-                        <Link
-                            href={'https://chatgpt.com/c/67713a78-705c-800a-88b8-1573aef74f32'}
-                            passHref
-                            legacyBehavior
-                        >
-                            <ButtonTranslateForwarded id={'hero-cta-banner'}
-                                textColor={{
-                                    unhover: 'var(--dark-color)',
-                                    hover: 'var(--alwaysWhite-color)'
-                                }}
-                                bgColor={{
-                                    unhover: 'var(--body-color)',
-                                    hover: 'var(--users-btn-alt)'
-                                }}
-                                borderColor={{
-                                    unhover: 'var(--users-btn-alt)',
-                                    hover: 'var(--users-btn-alt)'
-                                }}
-                                style={{
-                                    borderRadius: '0.5rem',
-                                    gap: '0.6rem',
-                                    padding: '0.35rem 0.7rem',
-                                }}
-                                shape='oval'
-                                animationDirection='top_left'
-                            >
-                                <span>Cek fitur baru yang menarik di SIPK!</span>
-                                <span className={styles.icon}>
-                                    <FiArrowUpRight />
-                                </span>
-                            </ButtonTranslateForwarded>
-                        </Link>
-                    </motion.div>
-                </VisibilityWrapper>
-
-                <VisibilityWrapper
-                    as='h1'
-                    id={'hero-title'}
-                    className={styles.title}
-                    onAnimationComplete={(x) => {
-                        if (typeof x === 'string') {
-                            if (x === 'inView') setAlreadyInView(true);
-                        }
-                    }}
-                    aria-label={'SIPK Introduction'}
-                >
-                    {HERO_TITLE_PARAGRAPH.flat().map((text, index) => (
-                        <HighlightText
-                            key={index}
-                            text={text}
-                            {...resolveTitleProps(text)}
-
-                        />
-                    ))}
-                </VisibilityWrapper>
-
-                <VisibilityWrapper
-                    id={'hero-description'}
-                    className={styles.title_description}
-                >
-                    <HighlightText
-                        useHook={false}
-                        preset={'wavingFlyIn'}
-                        presetOptions={{
-                            makeVariant: true,
-                            variantName: 'inView',
-                            wordStagger: 'random'
-                        }}
-                        adjustWavingFlyIn={{
-                            baseDelay: 0.5,
-                            stagger: 0.03,
-                            duration: 1.75
-                        }}
-                        text={'SIPK memungkinkan kalian menyimpan seluruh matakuliah dengan SKS dan nilai yang kalian tentukan sendiri untuk memvisualisasikan perolehan IPK secara lebih fleksibel dan proaktif.'}
-                    />
-                </VisibilityWrapper>
-
-                <VisibilityWrapper>
-                    <motion.div
-                        className={styles.buttons}
-                        initial={{ opacity: 0, y: 40 }}
-                        variants={{ inView: { opacity: 1, y: 0, transition: { duration: 2.25, delay: 0.75, type: 'spring', bounce: 0.3 } } }}
-                    >
-                        <Link
-                            href={'/users?action=daftar&utm_source=slp'}
-                            passHref
-                            legacyBehavior
-                        >
-                            <ButtonSimpleForwarded id={'hero-cta-main'}
-                                bgColor={{
-                                    unhover: 'var(--users-btn-alt)',
-                                    hover: 'var(--users-btn-hov)',
-                                }}
-                                textColor={{
-                                    unhover: 'var(--white-color)',
-                                    hover: 'var(--white-color)',
-                                }}
-                                style={{
-                                    border: 'none',
-                                    borderRadius: '0.75rem',
-                                    padding: '0.7rem 0.9rem',
-                                    gap: '0.25rem',
-                                    fontWeight: 600,
-                                }}
-                            >
-                                <span>
-                                    Mulai Sekarang
-                                </span>
-                                <span className={styles.icon}>
-                                    <FiArrowRight />
-                                </span>
-                            </ButtonSimpleForwarded>
-                        </Link>
-
-                        <ButtonSimple id={'hero-cta-secondary'}
-                            bgColor={{
-                                unhover: 'var(--white-color)',
-                                hover: 'var(--accordion-bg-color)',
-                            }}
-                            textColor={{
-                                unhover: 'var(--dark-color)',
-                                hover: 'var(--dark-color)',
-                            }}
-                            borderColor={{
-                                unhover: 'var(--accordion-bg2-color)',
-                                hover: 'var(--accordion-bg2-color)',
-                            }}
-                            style={{
-                                borderRadius: '0.75rem',
-                                padding: '0.7rem 0.9rem',
-                                gap: '0.25rem'
-                            }}
-                            onClick={(event) => {
-                                scroller.scrollTo('universitas', { smooth: true, offset: -75 })
-                                event.currentTarget.blur();
-                            }}
-                        >
-                            <span>
-                                Lihat Detailnya
-                            </span>
-                            <span className={styles.icon}>
-                                <FiArrowDown />
-                            </span>
-                        </ButtonSimple>
-                    </motion.div>
-                </VisibilityWrapper>
+                <Banner
+                    id={HERO_BANNER_ID}
+                    title='Cek fitur baru yang menarik di SIPK!'
+                    href={notifikasi[3].href}
+                />
+                <Title {...HERO_TITLE_PROPS} />
+                <Description text={HERO_DESCRIPTION_TEXT} />
+                <Buttons item={HERO_BUTTONS} />
             </ContainerWrapper>
 
-            <div className={styles.mockup_container} >
-                <div className={styles.mockup_perspective}>
-                    <div className={styles.mockup_3d}>
+            <MockupContainer>
+                <MockupPerspective>
+                    <Mockup3D>
                         <DashboardMockup
                             user={{
                                 fullname: 'Reyhan Naufal Hayyan',
@@ -325,10 +201,217 @@ const Hero = ({ notifikasi }) => {
                                 { item: MatkulDummies[4], color: MatkulDummiesNilaiColorPreset[MatkulDummies[4].nilai] },
                             ]}
                         />
-                    </div>
-                </div>
-            </div>
+                    </Mockup3D>
+                </MockupPerspective>
+            </MockupContainer>
         </motion.section>
+    )
+}
+
+function Banner({
+    id = 'banner',
+    title = 'Lorem ipsum dolor',
+    href = 'https://www.google.com/'
+}) {
+    return (
+        <VisibilityWrapper>
+            <motion.div
+                className={styles.banner}
+                initial={{ opacity: 0, y: -40 }}
+                variants={{
+                    inView: { opacity: 1, y: 0, transition: { duration: 2.25, delay: 0.375, type: 'spring', bounce: 0.3 } }
+                }}
+            >
+                <Link
+                    href={href}
+                    passHref
+                    legacyBehavior
+                >
+                    <ButtonTranslateForwarded id={id}
+                        textColor={{
+                            unhover: 'var(--dark-color)',
+                            hover: 'var(--alwaysWhite-color)'
+                        }}
+                        bgColor={{
+                            unhover: 'var(--body-color)',
+                            hover: 'var(--users-btn-alt)'
+                        }}
+                        borderColor={{
+                            unhover: 'var(--users-btn-alt)',
+                            hover: 'var(--users-btn-alt)'
+                        }}
+                        style={{
+                            borderRadius: '0.5rem',
+                            gap: '0.6rem',
+                            padding: '0.35rem 0.7rem',
+                        }}
+                        shape='oval'
+                        animationDirection='top_left'
+                    >
+                        <span>
+                            {title}
+                        </span>
+                        <span className={styles.icon}>
+                            <FiArrowUpRight />
+                        </span>
+                    </ButtonTranslateForwarded>
+                </Link>
+            </motion.div>
+        </VisibilityWrapper>
+    )
+}
+
+function Title({
+    id = 'title',
+    label = 'Awesome Title'
+}) {
+    return (
+        <VisibilityWrapper
+            as='h1'
+            id={id}
+            className={styles.title}
+            aria-label={label}
+        >
+            {HERO_TITLE_PARAGRAPH.flat().map((text, index) => (
+                <HighlightText
+                    key={index}
+                    text={text}
+                    {...resolveTitleProps(text)}
+                />
+            ))}
+        </VisibilityWrapper>
+    )
+}
+
+function Description({ text }) {
+    return (
+        <VisibilityWrapper
+            id={'hero-description'}
+            className={styles.title_description}
+        >
+            <HighlightText
+                useHook={false}
+                preset={'wavingFlyIn'}
+                presetOptions={{
+                    makeVariant: true,
+                    variantName: 'inView',
+                    wordStagger: 'random'
+                }}
+                adjustWavingFlyIn={{
+                    baseDelay: 0.5,
+                    stagger: 0.03,
+                    duration: 1.75
+                }}
+                text={text}
+            />
+        </VisibilityWrapper>
+    )
+}
+
+function Buttons({
+    item = [
+        { id: 'button', text: 'Click Me', href: 'https://www.google.com/' },
+        { id: 'button', text: 'Click Me', href: 'https://www.google.com/' },
+    ]
+}) {
+    return (
+        <VisibilityWrapper>
+            <motion.div
+                className={styles.buttons}
+                initial={{ opacity: 0, y: 40 }}
+                variants={{ inView: { opacity: 1, y: 0, transition: { duration: 2.25, delay: 0.75, type: 'spring', bounce: 0.3 } } }}
+            >
+                <Link
+                    href={item[0]?.href || 'https://www.google.com/'}
+                    passHref
+                    legacyBehavior
+                >
+                    <ButtonSimpleForwarded id={item[0]?.id || 'button'}
+                        bgColor={{
+                            unhover: 'var(--users-btn-alt)',
+                            hover: 'var(--users-btn-hov)',
+                        }}
+                        textColor={{
+                            unhover: 'var(--white-color)',
+                            hover: 'var(--white-color)',
+                        }}
+                        style={{
+                            border: 'none',
+                            borderRadius: '0.75rem',
+                            padding: '0.7rem 0.9rem',
+                            gap: '0.25rem',
+                            fontWeight: 600,
+                        }}
+                    >
+                        <span>
+                            {item[0]?.text || 'Click Me'}
+                        </span>
+                        <span className={styles.icon}>
+                            <FiArrowRight />
+                        </span>
+                    </ButtonSimpleForwarded>
+                </Link>
+
+                <ButtonSimple id={item[1]?.id || 'button'}
+                    bgColor={{
+                        unhover: 'var(--white-color)',
+                        hover: 'var(--accordion-bg-color)',
+                    }}
+                    textColor={{
+                        unhover: 'var(--dark-color)',
+                        hover: 'var(--dark-color)',
+                    }}
+                    borderColor={{
+                        unhover: 'var(--accordion-bg2-color)',
+                        hover: 'var(--accordion-bg2-color)',
+                    }}
+                    style={{
+                        borderRadius: '0.75rem',
+                        padding: '0.7rem 0.9rem',
+                        gap: '0.25rem'
+                    }}
+                    onClick={(event) => {
+                        const target = item[1]?.href || 'https://www.google.com/';
+                        if (target.startsWith('#')) {
+                            const id = target.split('#')[1];
+                            scroller.scrollTo(id, { smooth: true, offset: -75 })
+                        }
+                        event.currentTarget.blur();
+                    }}
+                >
+                    <span>
+                        {item[1]?.text || 'Click Me'}
+                    </span>
+                    <span className={styles.icon}>
+                        <FiArrowDown />
+                    </span>
+                </ButtonSimple>
+            </motion.div>
+        </VisibilityWrapper>
+    )
+}
+
+function MockupContainer({ children }) {
+    return (
+        <div className={styles.mockup_container} >
+            {children}
+        </div>
+    )
+}
+
+function MockupPerspective({ children }) {
+    return (
+        <div className={styles.mockup_perspective}>
+            {children}
+        </div>
+    )
+}
+
+function Mockup3D({ children }) {
+    return (
+        <div className={styles.mockup_3d}>
+            {children}
+        </div>
     )
 }
 
