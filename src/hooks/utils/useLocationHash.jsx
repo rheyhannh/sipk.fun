ca292407e109d87d2bd1b34e53b5d8c9344dee6b
rheyhannh <rheyhannh@student.ub.ac.id>
2025-01-17 04:35:05 +0700

    docs: Add hook docs

--- src/hooks/utils/useLocationHash.jsx
+++ src/hooks/utils/useLocationHash.jsx
@@ -1,3 +1,39 @@
+'use client'
+
+// #region NEXT DEPEDENCY
+import { useParams } from 'next/navigation';
+// #endregion
+
+// #region REACT DEPEDENCY
+import * as React from 'react';
+// #endregion
+
+const getHash = () => typeof window !== 'undefined' ? window.location.hash : '';
+
+/**
+ * Hook custom untuk mendeteksi perubahan hash pada URL dengan `window.location.hash`. 
+ * 
+ * Perlu diingat hook hanya ditrigger saat navigasi dilakukan melalui client-side seperti menggunakan component `Link` 
+ * atau hook `useRouter`, jika mengganti hash pada browser URL secara langsung hook ini tidak akan bekerja.
+ * 
+ * @returns {string} Location hash pada URL beserta tanda pagar `#`
+ * @example
+ * ```jsx
+ * const MyComponent = () => {
+ *      const locationHash = useLocationHash();
+ *      
+ *      // Scroll to hash when hash changes
+ *      React.useEffect(() => { 
+ *          const element = document.getElementById(locationHash.replace('#', ''));  
+ *          if (element) { 
+ *              element.scrollIntoView({ behavior: 'smooth', block: 'center' }); 
+ *          } 
+ *          console.log(locationHash) // e.g.'#myelement'
+ *      }, [locationHash]);
+ *      ...
+ * }
+ * ```
+ */
 const useLocationHash = () => {
     const [isClient, setIsClient] = React.useState(false);
     const [hash, setHash] = React.useState(getHash());
