483e924ac7089ddfcdd31a5d3ecf48b4f74463bf
rheyhannh <rheyhannh@student.ub.ac.id>
2025-01-19 01:29:51 +0700

    feat: Add custom not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import { LogoSipkFill } from '@/loader/StaticImages';
+import styles from '@/app/not_found.module.css';
+
+export const metadata = /** @type {import('next').Metadata} */ ({
+    title: 'Not Found | SIPK',
+  })
+
+export default function NotFound() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <div className={styles.image}>
+                    <LogoSipkFill priority={true} quality={100} />
+                </div>
+                <h1 className={styles.title}>
+                    404 <span>|</span> Not Found
+                </h1>
+                <p className={styles.description}>
+                    Kadang kita melangkah terlalu jauh, tapi setiap perjalanan membawa peluang untuk
+                </p>
+                <Link
+                    className={styles.link}
+                    href="/"
+                >
+                    Kembali ke Rumah
+                </Link>
+            </div>
+        </div>
+    )
+}
