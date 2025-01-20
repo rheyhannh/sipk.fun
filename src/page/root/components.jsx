bf571595e99558321193a8325f63176fdd0d4319
rheyhannh <rheyhannh@student.ub.ac.id>
2025-01-20 19:13:42 +0700

    docs: Update component jsdoc types and description

--- src/page/root/components.jsx
+++ src/page/root/components.jsx
@@ -92,13 +92,9 @@ export const VisibilityWrapper = ({ as: Tag = 'div', children, ...props }) => {
  * eksternal atau `arrowTopRight`
  * 
  * - Default : `false`
- * @property {import('next/link').LinkProps['scroll']} [scroll]
- * Opsi `scroll` pada component Link, disini kita merubah default valuenya.
- * 
- * - Default : `false`
  * @property {[string, Parameters<Element['scrollIntoView']>[0], Parameters<Element['scrollIntoView']>[0]] | Parameters<Element['scrollIntoView']>[0]} [scrollRules]
  * 
- * Untuk melakukan scroll ke element terkait, kita menggunakan [scrollIntoView](https://developer.mozilla.org/docs/Web/API/Element/scrollIntoView).
+ * Saat element id tersedia pada current page, kita override default behaviour Next.js scroll dengan menggunakan [scrollIntoView](https://developer.mozilla.org/docs/Web/API/Element/scrollIntoView).
  * Props ini dapat berupa object [scrollIntoViewOptions](https://developer.mozilla.org/docs/Web/API/Element/scrollIntoView) atau array dengan length `3` yang berisikan sebuah rules untuk melakukan scroll.
  * - Index `0` merupakan rule query untuk {@link https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia matchMedia}, contoh `(max-width: 1023px)`
  * - Index `1` merupakan opsi [scrollIntoViewOptions](https://developer.mozilla.org/docs/Web/API/Element/scrollIntoView) yang digunakan saat rule terpenuhi
@@ -124,7 +120,7 @@ export const VisibilityWrapper = ({ as: Tag = 'div', children, ...props }) => {
  * Component sebagai element anchor untuk melakukan navigasi dan scroll ke element tertentu
  * dengan melakukan navigasi secara client-side menggunakan component `Link` dan melakukan scroll menggunakan [scrollIntoView](https://developer.mozilla.org/docs/Web/API/Element/scrollIntoView). 
  * Component ini menggunakan atribut `onClick`, gunakan `onClickCapture` untuk menambahkan handler.
- * @param {React.AnchorHTMLAttributes<HTMLAnchorElement> & Omit<import('next/link').LinkProps, 'scroll'> & LinkHashProps} props LinkHash props
+ * @param {React.AnchorHTMLAttributes<HTMLAnchorElement> & import('next/link').LinkProps & LinkHashProps} props LinkHash props
  * @returns {React.ReactElement} Rendered component
  */
 export const LinkHash = ({
