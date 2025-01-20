5b43083a5ef032507c2fc0368dcb5491c448189c
rheyhannh <rheyhannh@student.ub.ac.id>
2025-01-20 19:17:33 +0700

    fix: Handle navigation in footer use link hash component

--- src/page/root/sections/footer.jsx
+++ src/page/root/sections/footer.jsx
@@ -1,9 +1,5 @@
 'use client'
 
-// #region TYPE DEPEDENCY
-import { NavItem } from '@/constant/client';
-// #endregion
-
 // #region CONFIG DEPEDENCY
 import {
     GLOBAL_VIEWPORT_ONCE,
@@ -29,13 +25,12 @@ import styles from '@root_page/root.module.css';
 // #region COMPONENT DEPEDENCY
 import { motion } from 'framer-motion';
 import { LogoSipkFillContrast } from '@/loader/StaticImages';
-import Link from '@/component/Link';
-import { animateScroll as scroll, scroller } from 'react-scroll';
+import { LinkHash } from '../components';
+import { animateScroll as scroll } from 'react-scroll';
 // #endregion
 
 // #region ICON DEPEDENCY
 import { FaLinkedin, FaTiktok } from 'react-icons/fa';
-import { FiArrowUpRight } from 'react-icons/fi';
 // #endregion
 
 /**
@@ -44,77 +39,43 @@ import { FiArrowUpRight } from 'react-icons/fi';
  */
 const FOOTER_SMALL_DEVICES = 524;
 
-/**
- * Array yang berisikan link yang digunakan pada footer. Gunakan props `elementId` untuk scroll ke section atau element tertentu,
- * gunakan `href` untuk routing ke url tertentu. 
- * 
- * Saat url dibuka pada tab baru, pastikan pass `true` pada props `isOpenNewTab` untuk
- * menambahkan icon dan target `_blank` pada element anchor.
- * 
- * Untuk opsi lanjutan dapat diatur pada props `routingOptions` untuk opsi routing dan `scrollOptions` untuk opsi scroll.
- * @type {Array<Omit<NavItem, 'icon' | 'iconName' | 'iconLib' | 'dropdown'> & {isOpenNewTab:boolean}>}
- */
-const footerShorcuts = [
+/** Array yang berisikan link yang digunakan pada footer. */
+const footerShorcuts = /** @type {Array<Parameters<typeof LinkHash>[0]>} */ ([
     {
-        text: 'Universitas',
-        elementId: null,
         href: '/#universitas',
-        routingOptions: {
-            scroll: false,
-            onClick: (event) => {
-                const element = document.getElementById('universitas');
-                if (element) {
-                    event.preventDefault();
-                    element.scrollIntoView({ behavior: 'smooth', block: 'center' });
-                }
-            }
-        }
+        text: 'Universitas',
+        hashId: 'universitas',
+        scrollRules: ['(max-width: 1023px)', { behaviour: 'smooth', block: 'start' }, { behaviour: 'smooth', block: 'center' }]
     },
     {
-        text: 'Fitur',
-        elementId: null,
         href: '/#kenapa_sipk',
-        routingOptions: {
-            scroll: false,
-            onClick: (event) => {
-                const element = document.getElementById('kenapa_sipk');
-                if (element) {
-                    event.preventDefault();
-                    element.scrollIntoView({ behavior: 'smooth', block: 'center' });
-                }
-            }
-        }
+        text: 'Fitur',
+        hashId: 'kenapa_sipk',
+        scrollRules: ['(max-width: 1079px)', { behaviour: 'smooth', block: 'start' }, { behaviour: 'smooth', block: 'center' }],
     },
     {
-        text: 'FAQ',
-        elementId: null,
         href: '/faq',
+        text: 'FAQ',
+        hashId: null,
+        scrollRules: null,
     },
-]
+])
 
 const footerSocials = [
     { icon: <FaLinkedin size={'100%'} />, target: 'https://www.linkedin.com/company/sipk-app' },
     { icon: <FaTiktok size={'100%'} />, target: 'https://www.tiktok.com/@sipk.official' },
 ]
 
-/**
- * Array yang berisikan link yang digunakan pada footer sublinks. Gunakan props `elementId` untuk scroll ke section atau element tertentu,
- * gunakan `href` untuk routing ke url tertentu. 
- * 
- * Saat url dibuka pada tab baru, pastikan pass `true` pada props `isOpenNewTab` untuk
- * menambahkan icon dan target `_blank` pada element anchor.
- * 
- * Untuk opsi lanjutan dapat diatur pada props `routingOptions` untuk opsi routing dan `scrollOptions` untuk opsi scroll.
- * @type {Array<Omit<NavItem, 'icon' | 'iconName' | 'iconLib' | 'dropdown'> & {isOpenNewTab:boolean}>}
- */
-const footerSublinks = [
+/** Array yang berisikan link yang digunakan pada footer sublinks */
+const footerSublinks = /** @type {Array<Parameters<typeof LinkHash>[0]>} */ ([
     {
-        text: 'Analitik',
-        elementId: null,
         href: 'https://dashboard.simpleanalytics.com/sipk.fun',
+        text: 'Analitik',
+        hashId: null,
+        scrollRules: null,
         isOpenNewTab: true,
     }
-]
+])
 
 const footerDelayAnims = [0.125, 0.25, 0.375];
 
@@ -175,111 +136,56 @@ const Footers = ({ viewportWidth }) => {
                     </motion.div>
                 </div>
 
-            <div id={'footer-shorcuts'} className={styles.shorcut}>
-                {footerShorcuts.map((item, index) => (
-                    <motion.div key={index} {...getFooterTransition(true, 0, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
-                        <Link
+                <div id={'footer-shorcuts'} className={styles.shorcut}>
+                    {footerShorcuts.map((props, index) => (
+                        <motion.div key={index} {...getFooterTransition(true, 0, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
+                            <LinkHash key={index} {...props} />
+                        </motion.div>
+                    ))}
+                </div>
+
+                <div id={'footer-socials'} className={styles.socials}>
+                    {footerSocials.map((item, index) => (
+                        <motion.a
                             key={index}
-                            item={{ href: item.href, elementId: item.elementId }}
-                            // TODOS offset still static '-75', when comes to mobile or any viewport less than 1280
-                            // offset should be corrected
-                            scrollOptions={{ offset: -75, ...item?.scrollOptions }}
-                            routingOptions={{ ...item?.routingOptions }}
-                            {...(item?.isOpenNewTab || false ? { target: '_blank' } : {})}
+                            className={styles.box}
+                            href={item.target}
+                            target={'_blank'}
                             tabIndex={0}
-                            onKeyDown={(event) => {
-                                if (event.key === 'Enter' && item?.elementId) {
-                                    const target = document.getElementById(item.elementId);
-                                    scroller.scrollTo(item.elementId, { offset: -75 })
-                                    if (target) target.focus();
-                                }
-                            }}
-                            onClickCapture={(event) => {
-                                event.target.blur();
-                                if (item?.elementId) {
-                                    const target = document.getElementById(item.elementId);
-                                    if (target) target.focus();
-                                }
-                            }}
-                        >
-                            {item.text}
-                            {item?.isOpenNewTab && (
-                                <FiArrowUpRight className={styles.external} />
-                            )}
-                        </Link>
-                    </motion.div>
-                ))}
-            </div>
+                            onClickCapture={(event) => { event.currentTarget.blur() }}
+                            {...getFooterTransition(false, 0, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
+                            {item.icon}
+                        </motion.a>
+                    ))}
+                </div>
+            </motion.div>
 
-            <div id={'footer-socials'} className={styles.socials}>
-                {footerSocials.map((item, index) => (
-                    <motion.a
-                        key={index}
-                        className={styles.box}
-                        href={item.target}
-                        target={'_blank'}
-                        tabIndex={0}
-                        onClickCapture={(event) => { event.currentTarget.blur() }}
-                        {...getFooterTransition(false, 0, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
-                        {item.icon}
-                    </motion.a>
-                ))}
-            </div>
-        </motion.div>
+            <motion.div
+                className={styles.line}
+                whileInView={'show'}
+                viewport={{ once: GLOBAL_VIEWPORT_ONCE }}
+                {...getFooterTransition(false, 1, true, viewportWidth <= FOOTER_SMALL_DEVICES)}
+            />
 
-        <motion.div
-            className={styles.line}
-            whileInView={'show'}
-            viewport={{ once: GLOBAL_VIEWPORT_ONCE }}
-            {...getFooterTransition(false, 1, true, viewportWidth <= FOOTER_SMALL_DEVICES)}
-        />
-
-        <motion.div
-            className={styles.secondary}
-            whileInView={'show'}
-            viewport={{ once: GLOBAL_VIEWPORT_ONCE }}
-        >
-            <motion.span initial={{ y: 75, opacity: 0 }} {...getFooterTransition(true, 2, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
-                2024 All Rights Reserved.
-            </motion.span>
-            <div id={'footer-sublinks'} className={styles.sublinks}>
-                {footerSublinks.map((item, index) => (
-                    <motion.div key={index} {...getFooterTransition(true, 2, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
-                        <Link
-                            key={index}
-                            item={{ href: item.href, elementId: item.elementId }}
-                            // TODOS offset still static '-75', when comes to mobile or any viewport less than 1280
-                            // offset should be corrected
-                            scrollOptions={{ offset: -75, ...item?.scrollOptions }}
-                            routingOptions={{ ...item?.routingOptions }}
-                            {...(item?.isOpenNewTab || false ? { target: '_blank' } : {})}
-                            tabIndex={0}
-                            onKeyDown={(event) => {
-                                if (event.key === 'Enter' && item?.elementId) {
-                                    const target = document.getElementById(item.elementId);
-                                    scroller.scrollTo(item.elementId, { offset: -75 })
-                                    if (target) target.focus();
-                                }
-                            }}
-                            onClickCapture={(event) => {
-                                event.target.blur();
-                                if (item?.elementId) {
-                                    const target = document.getElementById(item.elementId);
-                                    if (target) target.focus();
-                                }
-                            }}
-                        >
-                            {item.text}
-                            {item?.isOpenNewTab && (
-                                <FiArrowUpRight className={styles.external} />
-                            )}
-                        </Link>
-                    </motion.div>
-                ))}
-            </div>
-        </motion.div>
-    </footer>
-)
+            <motion.div
+                className={styles.secondary}
+                whileInView={'show'}
+                viewport={{ once: GLOBAL_VIEWPORT_ONCE }}
+            >
+                <motion.span initial={{ y: 75, opacity: 0 }} {...getFooterTransition(true, 2, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
+                    2025 All Rights Reserved.
+                </motion.span>
+                <div id={'footer-sublinks'} className={styles.sublinks}>
+                    {footerSublinks.map((props, index) => (
+                        <motion.div key={index} {...getFooterTransition(true, 2, false, viewportWidth <= FOOTER_SMALL_DEVICES)}>
+                            <LinkHash key={index} {...props} />
+                        </motion.div>
+                    ))}
+                </div>
+            </motion.div>
+        </footer>
+    )
+}
 
 const Footer = () => {
     const { width: viewportWidth } = useWindowSize();
